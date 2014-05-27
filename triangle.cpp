d110fa8945de130381b65d2a4fef7e5a69cddefb
yu <fengyu225@gmail.com>
2014-05-26 23:41:52 -0700

    triangle

---
+++ triangle.cpp
@@ -0,0 +1,21 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include<vector>
+
+using namespace std;
+
+int minimumTotal(vector<vector<int> >& triangle){
+    return 0; 
+}
+
+int main(){
+    int arr0[] = {2};
+    vector<int> s0(arr0,arr0+sizeof(arr0)/sizeof(int));
+    int arr1[] = {3, 4};
+    vector<int> s1(arr1,arr1+sizeof(arr1)/sizeof(int));
+    int arr2[] = {6, 5, 7};
+    vector<int> s2(arr2,arr2+sizeof(arr2)/sizeof(int));
+    int arr3[] = {4, 1, 8, 3};
+    vector<int> s3(arr3,arr3+sizeof(arr3)/sizeof(int));
+    return 0;
+}
