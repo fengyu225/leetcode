1da996b6a3bd616ba27047988d60c7aba5db5f17
fengyu225@gmail.com <fengyu225@gmail.com>
2014-05-27 19:50:24 -0700

    triangle

--- triangle.cpp
+++ triangle.cpp
@@ -5,8 +5,16 @@
 using namespace std;
 
 int minimumTotal(vector<vector<int> >& triangle){
-
-    return 0; 
+    int m[triangle[triangle.size()-1].size()];
+    for(int i=0; i<triangle[triangle.size()-1].size(); i++)
+        m[i] = triangle[triangle.size()-1][i];
+    for(int i=triangle.size()-2;i>=0;i--){
+        for(int j=0; j<triangle[i].size();j++){
+            m[j] = m[j]<m[j+1]?m[j]:m[j+1];
+            m[j] += triangle[i][j];
+        }
+    }
+    return m[0]; 
 }
 
 int main(){
@@ -18,11 +26,11 @@ int main(){
     vector<int> s2(arr2,arr2+sizeof(arr2)/sizeof(int));
     int arr3[] = {4, 1, 8, 3};
     vector<int> s3(arr3,arr3+sizeof(arr3)/sizeof(int));
-    vector<vector<it> > triangle;
-    triangle.push_back(arr0);
-    triangle.push_back(arr1);
-    triangle.push_back(arr2);
-    triangle.push_back(arr3);
+    vector<vector<int> > triangle;
+    triangle.push_back(s0);
+    triangle.push_back(s1);
+    triangle.push_back(s2);
+    triangle.push_back(s3);
     printf("%d\n", minimumTotal(triangle));
     return 0;
 }
