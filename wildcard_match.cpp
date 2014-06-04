c3d01e771cb8bdba3c5fb59a728aa7fc742d3850
fengyu225@gmail.com <fengyu225@gmail.com>
2014-06-04 15:03:28 -0700

    wildcard match

--- wildcard_match.cpp
+++ wildcard_match.cpp
@@ -2,6 +2,9 @@
 #include<string.h>
 #include<stdio.h>
 #include<stdbool.h>
+#include<queue>
+
+using std::queue;
 
 //bool isMatch(const char* s, const char* p){
 //    if(!*p) return *s=='\0';
@@ -18,14 +21,43 @@ bool isMatch(const char *s, const char *p) {
         if ((*p=='?')||(*p==*s)){s++;p++;continue;}
         if (*p=='*'){star=p++; ss=s;continue;}
         if (star){ p = star+1; s=++ss;continue;}
-        printf("p: %s, s: %s\n", p, s);
-        printf("-------------------\n");
         return false;
     }
     while (*p=='*'){p++;}
     return !*p;
 }
-    
+
+
+//bool isMatch(const char* s, const char* p){
+//    queue<const char*> s_q;
+//    queue<const char*> p_q;
+//    s_q.push(s); p_q.push(p);
+//    while(!s_q.empty() && !p_q.empty()){
+//        const char* s_t = s_q.front();
+//        const char* p_t = p_q.front();
+//        s_q.pop(); p_q.pop();
+//        if(!*p_t){
+//            if(*s_t=='\0') return true;
+//            else continue;
+//        }
+//        if(!*s_t){
+//            if(*p_t=='\0' || *p_t=='*') return true;
+//            else continue;
+//        }
+//        if(*p_t=='?' || *p_t==*s_t){
+//            s_q.push(s_t+1);
+//            p_q.push(p_t+1);
+//            continue;
+//        }
+//        if(*p_t=='*'){
+//            s_q.push(s_t);p_q.push(p_t+1);
+//            s_q.push(s_t+1);p_q.push(p_t+1);
+//            s_q.push(s_t+1);p_q.push(p_t);
+//            continue;
+//        }
+//    }
+//    return false;
+//}
 
 int main(){
 //    printf("%s\n", isMatch("aa","a")?"true":"false");
