0a1fedc8a2ed1fd81a03f56a89f0ec099386c155
fengyu225@gmail.com <fengyu225@gmail.com>
2014-06-04 11:55:56 -0700

    wildcard matching

--- wildcard_match.cpp
+++ wildcard_match.cpp
@@ -3,19 +3,37 @@
 #include<stdio.h>
 #include<stdbool.h>
 
-bool isMatch(const char* s, const char* p){
-    if(!*p) return *s=='\0';
-    if(!*s) return *p=='\0' || *p=='*';
-    return ((*p=='?' || *p==*s) && isMatch(s+1,p+1)) || (*p=='*' && (isMatch(s,p+1) || isMatch(s+1,p+1) || isMatch(s+1,p)));
+//bool isMatch(const char* s, const char* p){
+//    if(!*p) return *s=='\0';
+//    if(!*s) return *p=='\0' || *p=='*';
+//    return ((*p=='?' || *p==*s) && isMatch(s+1,p+1)) || (*p=='*' && (isMatch(s,p+1) || isMatch(s+1,p+1) || isMatch(s+1,p)));
+//}
+
+bool isMatch(const char *s, const char *p) {
+    const char* star=NULL;
+    const char* ss=s; 
+    while (*s){
+        printf("-------------------\n");
+        printf("p: %s, s: %s\n", p, s);
+        if ((*p=='?')||(*p==*s)){s++;p++;continue;}
+        if (*p=='*'){star=p++; ss=s;continue;}
+        if (star){ p = star+1; s=++ss;continue;}
+        printf("p: %s, s: %s\n", p, s);
+        printf("-------------------\n");
+        return false;
+    }
+    while (*p=='*'){p++;}
+    return !*p;
 }
+    
 
 int main(){
-    printf("%s\n", isMatch("aa","a")?"true":"false");
-    printf("%s\n", isMatch("aa","aa")?"true":"false");
-    printf("%s\n", isMatch("aaa","aa")?"true":"false");
-    printf("%s\n", isMatch("aa", "*")?"true":"false");
-    printf("%s\n", isMatch("aa", "a*")?"true":"false");
-    printf("%s\n", isMatch("ab", "?*")?"true":"false");
-    printf("%s\n", isMatch("aab", "c*a*b")?"true":"false");
+//    printf("%s\n", isMatch("aa","a")?"true":"false");
+//    printf("%s\n", isMatch("aa","aa")?"true":"false");
+//    printf("%s\n", isMatch("aaa","aa")?"true":"false");
+//    printf("%s\n", isMatch("aa", "*")?"true":"false");
+//    printf("%s\n", isMatch("aa", "a*")?"true":"false");
+    printf("%s\n", isMatch("cdadadabe", "?*a*?")?"true":"false");
+//    printf("%s\n", isMatch("aab", "c*a*b")?"true":"false");
     return 0;
 }
