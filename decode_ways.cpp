e2bf851067aa47dcb4bd848d9cf1e16ec196880d
fengyu225@gmail.com <fengyu225@gmail.com>
2014-06-05 18:52:49 -0700

    decode ways

--- decode_ways.cpp
+++ decode_ways.cpp
@@ -1,15 +1,24 @@
 #include<stdlib.h>
 #include<stdio.h>
 #include<string>
+#include<stdbool.h>
 
 using std::string;
+
+bool valid(string s){
+    return (s[0]=='1' && s[1]<='9' && s[1]>='0') || (s[0] == '2' && s[1]>=0 && s[1]<='6');
+}
 
 int numDecodings(string s){
     if(s.size() == 0) return 0;
     int arr[s.size()+1];
     arr[0] = 0;
     arr[1] = s[0] == '0'?0:1;
-    for(int i=1; i<s.size(); i++){
+    if(s.size() == 1) return arr[1];
+    arr[2] = 0;
+    if((s[0] == 0 && s[1]!='0') || s[0]>'2' || s[0]=='2' && s[1]>'6') arr[2]+=1;
+    if(valid(s.substr(s,2))) arr[2]+=1;
+    for(int i=2; i<s.size(); i++){
         if(s[i] == '0'){
             arr[i+1]=arr[i];
             continue;
