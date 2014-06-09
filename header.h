b902cee3dbc657e3d9825a6b9005926109757e29
yu <fengyu225@gmail.com>
2014-06-08 21:46:27 -0700

    header file

---
+++ header.h
@@ -0,0 +1,16 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include<stdbool.h>
+#include<vector>
+#include<string>
+#include<queue>
+#include<stack>
+#include<unordered_map>
+#include<unordered_set>
+
+using std::vector;
+using std::string;
+using std::queue;
+using std::stack;
+using std::unordered_map;
+using std::unordered_set;
