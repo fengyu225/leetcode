e64dcf48db65345e5165d9f16fc6b751d2a78abb
yu <fengyu225@gmail.com>
2014-06-09 01:48:52 -0700

    median of two sorted array

--- median_of_two_sorted_arrays.cpp
+++ median_of_two_sorted_arrays.cpp
@@ -19,6 +19,25 @@ int findKSmallest(int A[], int m, int B[], int n, int k){
         return findKSmallest(A, a_m, B+b_m+1, n-b_m-1, k-b_m-1);
 }
 
+double findKSmallest(int a[], int m, int b[], int n, int k)  
+{  
+    //always assume that m is equal or smaller than n  
+    if (m > n)  
+        return findKSmallest(b, n, a, m, k);  
+    if (m == 0)  
+        return b[k - 1];  
+    if (k == 1)  
+        return min(a[0], b[0]);  
+    //divide k into two parts  
+    int pa = min(k / 2, m), pb = k - pa;  
+    if (a[pa - 1] < b[pb - 1])  
+        return findKSmallest(a + pa, m - pa, b, n, k - pa);  
+    else if (a[pa - 1] > b[pb - 1])  
+        return findKSmallest(a, m, b + pb, n - pb, k - pb);  
+    else  
+        return a[pa - 1];  
+}  
+
 double findMedianSortedArrays(int A[], int m, int B[], int n){
     int total = m+n;
     if(total%2==1)
