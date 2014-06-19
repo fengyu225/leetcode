9637b9f93aa4c9169d62d0178c9867952f24adc7
fengyu225@gmail.com <fengyu225@gmail.com>
2014-06-19 15:18:30 -0700

    sudoku_solver.cpp

--- sudoku_solver.cpp
+++ sudoku_solver.cpp
@@ -1,7 +1,58 @@
 #include "header.h"
 
+bool is_valid(vector<vector<char> >& board, int i, int j){
+    //i row, j column
+    int arr[10] = {0,0,0,0,0,0,0,0,0,0};
+    int arr1[10] = {0,0,0,0,0,0,0,0,0,0};
+    int arr2[10] = {0,0,0,0,0,0,0,0,0,0};
+    for(int a=0;a<board[i].size();a++){
+        if(board[i][a] == '.') continue;
+        int x = board[i][a]-'0';
+        if(arr[x] != 0){
+            return false;
+        }
+        arr[x] = 1;
+    }
+    for(int a=0; a<board.size(); a++){
+        if(board[a][j] == '.') continue;
+        int x = board[a][j]-'0';
+        if(arr1[x] != 0){
+            return false;
+        }
+        arr1[x] = 1;
+    }
+    for(int a=i/3*3;a<i/3*3+3;a++){
+        for(int b=j/3*3;b<j/3*3+3;b++){
+            if(board[a][b] == '.') continue;
+            int x = board[a][b]-'0';
+            if(arr2[x] != 0){
+                return false;
+            }
+            arr2[x] = 1;
+        }
+    }
+    return true;
+}
+
+bool solve(vector<vector<char> >& board){
+    for(int i=0; i<board.size(); i++)
+        for(int j=0; j<board[i].size(); j++){
+            if(board[i][j] != '.') continue;
+            for(int k=1; k<10; k++){
+                board[i][j] = (char)(k+'0');
+                if(is_valid(board,i,j) && solve(board)){
+                    return true;
+                }
+                board[i][j] = '.';
+            }
+            return false;
+        }
+    return true;
+}
+
 void solveSudoku(vector<vector<char> > &board) {
-    
+    if(board.size() == 0 || board[0].size() == 0) return;
+    solve(board);
 }
 
 int main(){
@@ -14,15 +65,15 @@ int main(){
     char arr6[] = ".6....28.";
     char arr7[] = "...419..5";
     char arr8[] = "....8..79";
-    vector<char> v0(arr0,arr0+sizeof(arr0)/sizeof(char));
-    vector<char> v1(arr1,arr1+sizeof(arr1)/sizeof(char));
-    vector<char> v2(arr2,arr2+sizeof(arr2)/sizeof(char));
-    vector<char> v3(arr3,arr3+sizeof(arr3)/sizeof(char));
-    vector<char> v4(arr4,arr4+sizeof(arr4)/sizeof(char));
-    vector<char> v5(arr5,arr5+sizeof(arr5)/sizeof(char));
-    vector<char> v6(arr6,arr6+sizeof(arr6)/sizeof(char));
-    vector<char> v7(arr7,arr7+sizeof(arr7)/sizeof(char));
-    vector<char> v8(arr8,arr8+sizeof(arr8)/sizeof(char));
+    vector<char> v0(arr0,arr0+sizeof(arr0)/sizeof(char)-1);
+    vector<char> v1(arr1,arr1+sizeof(arr1)/sizeof(char)-1);
+    vector<char> v2(arr2,arr2+sizeof(arr2)/sizeof(char)-1);
+    vector<char> v3(arr3,arr3+sizeof(arr3)/sizeof(char)-1);
+    vector<char> v4(arr4,arr4+sizeof(arr4)/sizeof(char)-1);
+    vector<char> v5(arr5,arr5+sizeof(arr5)/sizeof(char)-1);
+    vector<char> v6(arr6,arr6+sizeof(arr6)/sizeof(char)-1);
+    vector<char> v7(arr7,arr7+sizeof(arr7)/sizeof(char)-1);
+    vector<char> v8(arr8,arr8+sizeof(arr8)/sizeof(char)-1);
     vector<vector<char> > board;
     board.push_back(v0);
     board.push_back(v1);
@@ -38,6 +89,7 @@ int main(){
             printf("%c ", board[i][j]);
         printf("\n");
     }
+    printf("\n");
     solveSudoku(board);
     for(int i=0; i<board.size(); i++){
         for(int j=0; j<board[i].size(); j++)
