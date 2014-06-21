e0c167e47c61a6f3048ce1f9916ef0b002cd5772
fengyu225@gmail.com <fengyu225@gmail.com>
2014-06-20 18:27:04 -0700

    M

--- largest_rectangle_in_histogram.cpp
+++ largest_rectangle_in_histogram.cpp
@@ -3,24 +3,25 @@
 //http://fisherlei.blogspot.com/2012/12/leetcode-largest-rectangle-in-histogram.html
 //http://www.geeksforgeeks.org/largest-rectangle-under-histogram/
 
-int largestRectangleArea(vector<int> &height) {
-    if(height.size() == 0) return 0;
-    int max = 0;
-    stack<int> s;
-    for(int i=0; i<height.size(); i++){
-        if(s.empty() || s.top()<=height[i]) s.push(height[i]);
-        else{
-            int x = 1;
-            while(!s.empty() && s.top()>height[i]){
-                s.pop();
-                x++;
-            }
-            max = max<height[i]*x?height[i]*x:max;
-            s.push(height[i]);
-        }
-    }
-    return max;
+int max(int a,int b){
+    return a>b?a:b;
 }
+
+int largestRectangleArea(vector<int> &h) {   
+     stack<int> S;  
+     h.push_back(0);  
+     int sum = 0;  
+     for (int i = 0; i < h.size(); i++) {   
+          if (S.empty() || h[i] > h[S.top()]) S.push(i);  
+          else {   
+               int tmp = S.top();  
+               S.pop();  
+                sum = max(sum, h[tmp]*(S.empty()? i : i-S.top()-1));  
+                i--;  
+           }
+     }
+     return sum;  
+}  
 
 int main(){
     int height[] = {2, 1, 5, 6, 2, 3};
