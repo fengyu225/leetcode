7cd88b7a966cf97d1674abf7efab235633c0df68
fengyu225@gmail.com <fengyu225@gmail.com>
2014-06-24 18:42:14 -0700

    M  find_missing_positive.cpp

--- find_missing_positive.cpp
+++ find_missing_positive.cpp
@@ -1,22 +1,23 @@
 #include "header.h"
 
 int firstMissingPositive(int A[], int n) {
-    int min_negative = -1;
+    int min_negative = 0;
     for(int i=0; i<n; i++)
         min_negative = A[i]<min_negative?A[i]:min_negative;
+    min_negative--;
     for(int i=0; i<n;i++){
         if(A[i]>=n) continue;
         int x;
-        if(A[i]<min_negative)
+        if(A[i]<=min_negative)
             x = min_negative-A[i];
-        if(A[i]>0)
+        if(A[i]>=0)
             x = A[i];
         if(x<n && A[x]>=0)
             A[x] = min_negative-A[x];
     }
-    for(int i=0; i<n; i++)
-        printf("%d ", A[i]);
-    printf("\n");
+//    for(int i=0; i<n; i++)
+//        printf("%d ", A[i]);
+//    printf("\n");
     for(int i=1; i<n; i++)
         if(A[i]>min_negative && A[i]<n)
             return i;
@@ -24,7 +25,8 @@ int firstMissingPositive(int A[], int n) {
 }
 
 int main(){
-    int arr0[] = {1, 2, 0}; //3
+    //int arr0[] = {1, 2, 0}; //3
+    int arr0[] = {1000,-1}; //3
     int arr1[] = {3, 4, -1, 1}; //2 
     printf("%d\n", firstMissingPositive(arr0,sizeof(arr0)/sizeof(arr0[0])));
     printf("%d\n", firstMissingPositive(arr1,sizeof(arr1)/sizeof(arr1[0])));
