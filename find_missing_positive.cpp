27b45418a19d31b77edfe55db32d499e0b80ebb9
fengyu225@gmail.com <fengyu225@gmail.com>
2014-06-24 18:35:35 -0700

    M  find_missing_positive.cpp

--- find_missing_positive.cpp
+++ find_missing_positive.cpp
@@ -1,7 +1,26 @@
 #include "header.h"
 
 int firstMissingPositive(int A[], int n) {
-    return 0;
+    int min_negative = -1;
+    for(int i=0; i<n; i++)
+        min_negative = A[i]<min_negative?A[i]:min_negative;
+    for(int i=0; i<n;i++){
+        if(A[i]>=n) continue;
+        int x;
+        if(A[i]<min_negative)
+            x = min_negative-A[i];
+        if(A[i]>0)
+            x = A[i];
+        if(x<n && A[x]>=0)
+            A[x] = min_negative-A[x];
+    }
+    for(int i=0; i<n; i++)
+        printf("%d ", A[i]);
+    printf("\n");
+    for(int i=1; i<n; i++)
+        if(A[i]>min_negative && A[i]<n)
+            return i;
+    return n;
 }
 
 int main(){
