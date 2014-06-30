3da70c9cfd18da6e5f7960cc91317a83000aae21
fengyu225@gmail.com <fengyu225@gmail.com>
2014-06-30 15:50:02 -0700

    M  zigzag_conversion.cpp

--- zigzag_conversion.cpp
+++ zigzag_conversion.cpp
@@ -1,27 +1,27 @@
 #include "header.h"
 
 string convert(string s, int nRows){
-    if(nRows == 1) return s;
-    string before = "";
-    string after = "";
-    for(int i=0; i<nRows/2; i++){
-        for(int j=i;j<s.size();j+=(nRows+1)/2*2)
-            before+=s[j];
-    }
-    if(nRows%2==1){
-        for(int i=nRows/2;i<s.size();i+=(nRows+1)/2)
-            before+=s[i];
-    }
-    for(int i=(nRows+1)/2;i<nRows;i++){
-        for(int j=i;j<s.size();j+=(nRows+1)/2*2)
-            after+=s[j];
-    }
-    return before+after;
+    if(s.length()==0 || nRows <=0)  
+        return "";  
+    if(nRows == 1)  
+        return s;  
+    string res = ""; 
+    int size = 2*nRows-2;  
+    for(int i=0;i<nRows;i++)  
+    {  
+        for(int j=i;j<s.length();j+=size)  
+        {  
+            res += s[j];  
+            if(i!=0 && i!=nRows-1 && j+size-2*i<s.length())  
+                res+=s[j+size-2*i];  
+        }                  
+    }  
+    return res;  
 }
 
 int main(){
-    //string s("PAYPALISHIRING");
-    string s("ABC");
+    string s("PAYPALISHIRING");
+//    string s("ABC");
     string res = convert(s,3);
     printf("%s\n", res.c_str());
     return 0;
