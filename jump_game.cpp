9ba775ec91898b5eeaffec7721f1a3e3128e1a61
fengyu225@gmail.com <fengyu225@gmail.com>
2014-06-30 15:52:31 -0700

    A  jump_game.cpp

---
+++ jump_game.cpp
@@ -0,0 +1,12 @@
+#include "header.h"
+
+int jump(int A[], int n){
+    return 0;
+}
+
+int main(){
+    int A[] = {2, 3, 1, 1, 4};
+    int res = jump(A,sizeof(A)/sizeof(A[0]));
+    printf("%d\n", res);
+    return 0;
+}
