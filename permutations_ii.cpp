117103553571dfbfb7c57ab11e1aad0282947806
fengyu225@gmail.com <fengyu225@gmail.com>
2014-07-03 14:04:42 -0700

    M  permutations_ii.cpp ?? x

--- permutations_ii.cpp
+++ permutations_ii.cpp
@@ -12,7 +12,7 @@ void perm(vector<int>& num, int curr, vector<vector<int> >& res){
         return;
     }
     for(int i=curr; i<num.size(); i++){
-        if(i!=curr && num[curr] == num[i]) continue;
+        if(i!=curr && num[i] == num[i-1] || i!=curr && num[i] == num[curr]) continue;
         swap(num,curr,i);
         perm(num,curr+1,res);
         swap(num,curr,i);
@@ -26,10 +26,48 @@ vector<vector<int> > permuteUnique(vector<int> &num){
     return res;
 }
 
+//bool noswap(int k, int i, const vector<int> num){
+//    for (int j=k;j<i;j++){
+//        if (num[j]==num[i]){
+//            return true;
+//        }
+//    }
+//    return false;
+//}
+//
+//void perm(vector<int> num,int k,int n, vector<vector<int> > &res){
+//    if (k==n){
+//        res.push_back(num);
+//    }else{
+//        for (int i=k;i<=n;i++){
+//             
+//            if (noswap(k,i,num)){continue;}
+//             
+//            int tmp = num[k];
+//            num[k]=num[i];
+//            num[i]=tmp;
+//             
+//            perm(num,k+1,n,res);
+//             
+//            tmp = num[k];
+//            num[k]=num[i];
+//            num[i]=tmp;
+//        }
+//    }
+//}
+//vector<vector<int> > permuteUnique(vector<int> &num) {
+//    // Start typing your C/C++ solution below
+//    // DO NOT write int main() function
+//    vector<vector<int> > res;
+//    perm(num,0,(num.size()-1),res);
+//    return res;
+//}
+
 int main(){
     //int arr[] = {1, 1, 2};
     //int arr[] = {1, 2, 3};
-    int arr[] = {1, 2, 1, 2};
+//    int arr[] = {1, 2, 1, 2};
+    int arr[] = {-1,2,0,-1,1,0,1};
     vector<int> num(arr,arr+sizeof(arr)/sizeof(arr[0]));
     vector<vector<int> > res = permuteUnique(num);
     for(int i=0; i<res.size(); i++){
