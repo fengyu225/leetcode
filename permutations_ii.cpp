da93447b6f2d95a777ee851b2181c345552480d0
fengyu225@gmail.com <fengyu225@gmail.com>
2014-07-02 17:22:12 -0700

    M  permutations_ii.cpp

--- permutations_ii.cpp
+++ permutations_ii.cpp
@@ -1,12 +1,35 @@
 #include "header.h"
 
+void swap(vector<int>& num, int a, int b){
+    int x = num[a];
+    num[a] = num[b];
+    num[b] = x;
+}
+
+void perm(vector<int>& num, int curr, vector<vector<int> >& res){
+    if(curr == num.size()){
+        res.push_back(num);
+        return;
+    }
+    for(int i=curr; i<num.size(); i++){
+        if(i!=curr && num[curr] == num[i]) continue;
+        swap(num,curr,i);
+        perm(num,curr+1,res);
+        swap(num,curr,i);
+    }
+}
+
 vector<vector<int> > permuteUnique(vector<int> &num){
     vector<vector<int> > res;
+    std::sort(num.begin(), num.end());
+    perm(num,0,res);
     return res;
 }
 
 int main(){
-    int arr[] = {1, 1, 2};
+    //int arr[] = {1, 1, 2};
+    //int arr[] = {1, 2, 3};
+    int arr[] = {1, 2, 1, 2};
     vector<int> num(arr,arr+sizeof(arr)/sizeof(arr[0]));
     vector<vector<int> > res = permuteUnique(num);
     for(int i=0; i<res.size(); i++){
