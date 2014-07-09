5890d3b634568cd15ae808553b59ca594e1920fd
fengyu225@gmail.com <fengyu225@gmail.com>
2014-07-09 13:21:04 -0700

    M  unique_binary_search_tree_ii.cpp

--- unique_binary_search_tree_ii.cpp
+++ unique_binary_search_tree_ii.cpp
@@ -7,19 +7,16 @@ struct TreeNode {
     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
 
-vector<TreeNode*> build(int l, int r){
-    vector<TreeNode*> res;
+void build(int l, int r,vector<TreeNode*>& res){
     if(l>r){
         res.push_back(NULL);
-        return res;
-    }
-    if(l == r){
-        res.push_back(new TreeNode(l));
-        return res;
+        return;
     }
     for(int i=l;i<=r;i++){
-        vector<TreeNode*> left_v = build(l,i-1);
-        vector<TreeNode*> right_v = build(i+1,r);
+        vector<TreeNode*> left_v;
+        vector<TreeNode*> right_v;
+        build(l,i-1,left_v);
+        build(i+1,r,right_v);
         for(int j=0;j<left_v.size();j++)
             for(int k=0;k<right_v.size();k++){
                 TreeNode* cr = new TreeNode(i);
@@ -28,11 +25,11 @@ vector<TreeNode*> build(int l, int r){
                 res.push_back(cr);
             }
     }
-    return res;
 }
 
 vector<TreeNode*> generateTrees(int n) {
-    vector<TreeNode*> res = build(1,n);
+    vector<TreeNode*> res;
+    build(1,n,res);
     return res;
 }
 
