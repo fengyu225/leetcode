b3fbda5afc70afe5d5350374a7eaac0f844973d0
fengyu225@gmail.com <fengyu225@gmail.com>
2014-07-11 14:32:38 -0700

    M  longest_common_prefix.cpp

--- longest_common_prefix.cpp
+++ longest_common_prefix.cpp
@@ -1,7 +1,23 @@
 #include "header.h"
 
 string longestCommonPrefix(vector<string> &strs) {
+    string res;
+    if(strs.size()==0) return res;
+    if(strs.size()==1) return strs[0];
 
 }
 
-
+int main(){
+    string s0("abce");
+    string s1("abcd");
+    string s2("abde");
+    string s3("abcf");
+    vector<string> v;
+    v.push_back(s0);
+    v.push_back(s1);
+    v.push_back(s2);
+    v.push_back(s3);
+    string res = longestCommonPrefix(v);
+    printf("%s\n", res.c_str());
+    return 0;
+}
