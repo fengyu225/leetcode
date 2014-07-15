94ae0db58e15189932f7e98ae757331e6d49ca75
fengyu225@gmail.com <fengyu225@gmail.com>
2014-07-14 20:07:14 -0700

    M  palindrome_number.cpp

--- palindrome_number.cpp
+++ palindrome_number.cpp
@@ -1,5 +1,27 @@
 #include "header.h"
 
 bool isPalindrome(int x) {
+    if(x<0) return false;
+    int m=10,n=1;
+    int temp=x;
+    while(temp>=10){
+        temp/=10;
+        n*=10;
+    }
+    while(m<=n){
+        int l=x-x/m*m;
+        int r=x/n % 10;
+        if(l!=r) return false;
+        n/=10;m*=10;
+    }
+    return true;
+}
 
+int main(){
+    //printf("%d\n", isPalindrome(10000021));
+    printf("%d\n", isPalindrome(1001));
+    printf("%d\n", isPalindrome(1));
+    printf("%d\n", isPalindrome(-1));
+    printf("%d\n", isPalindrome(INT_MAX));
+    return 0;
 }
