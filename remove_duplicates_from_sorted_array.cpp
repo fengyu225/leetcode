73bf266e412c2880bc32407dd411c942b9833c6d
fengyu225@gmail.com <fengyu225@gmail.com>
2014-07-15 11:08:14 -0700

    M  remove_duplicates_from_sorted_array.cpp

--- remove_duplicates_from_sorted_array.cpp
+++ remove_duplicates_from_sorted_array.cpp
@@ -1,11 +1,25 @@
 #include "header.h"
 
 int removeDuplicates(int A[], int n) {
-
+    if(n==0 || n==1) return n;
+    int p=0,q=0;
+    while(q<n){
+        if(A[p] == A[q]){
+            q++;
+            continue;
+        }
+        A[p+1] = A[q];
+        p+=1;
+    }
+    return p+1;
 }
 
 int main(){
     int A[] = {1, 1, 2};
-    printf("%d\n", removeDuplicates(A,sizeof(A)/sizeof(A[0])));
+    int res = removeDuplicates(A,sizeof(A)/sizeof(A[0]));
+    printf("%d\n", res);
+    for(int i=0; i<res; i++)
+        printf("%d ", A[i]);
+    printf("\n");
     return 0;
 }
