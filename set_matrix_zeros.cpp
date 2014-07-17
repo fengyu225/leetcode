0dd83db052703c48d1ba1cae812acb3d1f75fb5d
fengyu225@gmail.com <fengyu225@gmail.com>
2014-07-16 17:15:42 -0700

    M  set_matrix_zeros.cpp

--- set_matrix_zeros.cpp
+++ set_matrix_zeros.cpp
@@ -1,7 +1,52 @@
 #include "header.h"
 
 void setZeroes(vector<vector<int> > &matrix) {
-    
+    bool row_sz = false;
+    bool col_sz = false;
+    for(int i=0; i<matrix.size(); i++)
+        if(matrix[i][0] == 0){
+            col_sz = true;
+            break;
+        }
+    for(int i=0; i<matrix[0].size(); i++)
+        if(matrix[0][i] == 0){
+            row_sz = true;
+            break;
+        }
+    for(int row=1; row<matrix.size(); row++){
+        for(int col=0; col<matrix[row].size(); col++){
+            if(matrix[row][col] == 0){
+                matrix[row][0] = 0;
+                continue;
+            }
+        }
+    }
+    for(int col=1; col<matrix[0].size(); col++){
+        for(int row=0; row<matrix.size(); row++){
+            if(matrix[row][col] == 0){
+                matrix[0][col] = 0;
+                continue;
+            }
+        }
+    }
+    for(int row=1; row<matrix.size(); row++)
+        if(matrix[row][0] == 0){
+            for(int col=1; col<matrix[row].size(); col++)
+                matrix[row][col]=0;
+        }
+    for(int col=1; col<matrix[0].size(); col++)
+        if(matrix[0][col] == 0){
+            for(int row=1;row<matrix.size();row++)
+                matrix[row][col]=0;
+        }
+    if(row_sz){
+        for(int i=0; i<matrix[0].size(); i++)
+            matrix[0][i] = 0;
+    }
+    if(col_sz){
+        for(int i=0; i<matrix.size(); i++)
+            matrix[i][0] = 0;
+    }
 }
 
 int main(){
