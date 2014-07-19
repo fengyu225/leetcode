813359a5f3806e2ecce29435ed77eb49bf644381
fengyu225@gmail.com <fengyu225@gmail.com>
2014-07-18 18:49:03 -0700

    M  swap_nodes_in_pairs.cpp

--- swap_nodes_in_pairs.cpp
+++ swap_nodes_in_pairs.cpp
@@ -22,14 +22,32 @@ ListNode* create_list(int arr[], int n){
 }
 
 ListNode* swapPairs(ListNode *head) {
-    if(!head || !head->next) return head;
     //1 2 3 4 5 6 7
-    //7 6 5 4 3 2 1
     //2 1 4 3 6 5 7
+    if(!head || !head->next) return head;
+    ListNode* curr = head;
+    ListNode* res = NULL;
+    ListNode* last_tail = NULL;
+    while(1){
+        if(!curr) return res;
+        ListNode* a = curr;
+        if(!curr->next){
+            if(last_tail) last_tail->next = curr;
+            return res;
+        }
+        ListNode* b = curr->next;
+        curr = b->next;
+        b->next = a;
+        if(!res) res = b;
+        else last_tail->next = b;
+        last_tail = a;
+        last_tail->next = NULL;
+    }
+    return res;
 }
 
 int main(){
-    int arr[] = {1, 2, 3, 4};
+    int arr[] = {1, 2, 3, 4, 5};
     ListNode* l = create_list(arr, sizeof(arr)/sizeof(arr[0]));
     ListNode* r = swapPairs(l);
     while(r){
