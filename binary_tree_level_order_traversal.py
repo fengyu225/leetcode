bfda87e66c54e6c65265a1b4ce76afd6837f9537
yufeng <fengyu225@gmail.com>
2014-10-06 00:33:55 -0700

    M  binary_tree_level_order_traversal.py

--- binary_tree_level_order_traversal.py
+++ binary_tree_level_order_traversal.py
@@ -10,6 +10,25 @@ class Solution:
     # @param root, a tree node
     # @return a list of lists of integers
     def levelOrder(self, root):
+        res = []
+        if root is None:
+            return res
+        q = [[],[]]
+        temp = [root.val]
+        curr = 0
+        q[curr].append(root)
+        while len(q[curr%2])>0:
+            n = q[curr%2][0]
+            q[curr%2] = q[curr%2][1:]
+            if n.left is not None:
+                q[(curr+1)%2].append(n.left)
+            if n.right is not None:
+                q[(curr+1)%2].append(n.right)
+            if len(q[curr%2]) == 0:
+                res.append(temp)
+                temp = []
+                curr += 1
+        return res
 
 
 if __name__ == "__main__":
@@ -19,7 +38,7 @@ if __name__ == "__main__":
     n15 = Node(15)
     n7 = Node(7)
     n3.left = n9
-    n2.right = n20
+    n3.right = n20
     n20.left = n15
     n20.right = n7
     print Solution().levelOrder(n3)
