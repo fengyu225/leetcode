1c2d50e21c8a16cd81c5e8e8bcc93561c9b33e98
yufeng <fengyu225@gmail.com>
2014-10-07 01:46:43 -0700

    M  copy_list_with_random_ptr.py

--- copy_list_with_random_ptr.py
+++ copy_list_with_random_ptr.py
@@ -9,12 +9,30 @@ class Solution:
     # @param head, a RandomListNode
     # @return a RandomListNode
     def copyRandomList(self, head):
+        res = None
+
 
 def print_list(root):
     while root:
-        print "root val: {0}, next val: {1}, rand val: {2}".format(root.val, "None" if root.next is None else root.next.val, "None" if root.random is None else root.random.val)
+        print "root val: {0}, next val: {1}, rand val: {2}".format(root.label, "None" if root.next is None else root.next.label, "None" if root.random is None else root.random.label)
         root = root.next
 
 
 if __name__ == "__main__":
-
+    n0 = RandomListNode(0)
+    n1 = RandomListNode(1)
+    n2 = RandomListNode(2)
+    n3 = RandomListNode(3)
+    n4 = RandomListNode(4)
+    n5 = RandomListNode(5)
+    n0.next = n1
+    n1.next = n2
+    n2.next = n3
+    n3.next = n4
+    n4.next = n5
+    n0.random = n3
+    n1.random = n1
+    n2.random = n4
+    n3.random = n5
+    n4.random = n2
+    print_list(n0)
