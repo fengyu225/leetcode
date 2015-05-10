3eb929067367d08a2e15412bfe15bc0397ed9ac6
Yu Feng <yfeng@boingo.com>
2015-05-10 11:10:50 -0700

    M  5/longest_substring_without_repeating_char.cpp

--- 5/longest_substring_without_repeating_char.cpp
+++ 5/longest_substring_without_repeating_char.cpp
@@ -4,10 +4,25 @@ int lengthOfLongestSubstring(string s) {
 	if(s.length()<2) return s.length();
 	int sz = s.length();
 	int map[127] = {0};
-	int l = 0, r=0;
+	int l = 0, r=1;
+	map[s[l]] = 1;
+	int res = 1;
 	while(r<sz){
-		
+		if(map[s[r]]==0){
+			map[s[r]]++;
+			r++;
+			res = max(res,r-l);
+			continue;
+		}
+		while(map[s[r]]>0){
+			map[s[l]]--;
+			l++;
+		}
+		map[s[r]]=1;
+		res = max(res,r-l+1);
+		r++;
 	}
+	return res;
 }	
 
 int main(){
