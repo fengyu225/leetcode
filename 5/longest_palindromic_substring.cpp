8ed1fba073831b03cdf005c07e9b1d5ba85daa06
Yu Feng <yfeng@boingo.com>
2015-05-15 15:23:21 -0700

    M  5/longest_palindromic_substring.cpp ?? 5/-std=c++11

--- 5/longest_palindromic_substring.cpp
+++ 5/longest_palindromic_substring.cpp
@@ -5,11 +5,11 @@ string longestPalindrome(string s) {
     int sz = s.length();
     int arr[1000][1000];
     int res = 1, res_l = 0;
-    for(int i=0;i<1000;i++){for(int j=0;j<1000;j++) arr[i][j]=1;}
+    for(int i=0;i<1000;i++){arr[i][i]=1;}
     for(int l=2;l<=sz;l++){
         for(int i=0; i+l<=sz; i++){
             int temp = l==2?0:arr[i+1][i+l-2];
-            if(s[i]==s[i+l-1] && temp!=0){
+            if(s[i]==s[i+l-1] && (temp!=0||l==2)){
                 arr[i][i+l-1] = temp+2;
                 if(arr[i][i+l-1]>res){
                     res = arr[i][i+l-1];
