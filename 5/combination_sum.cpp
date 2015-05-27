89b85f9581b92723c7456d186ba0d27cb3f12f4d
Yu Feng <yfeng@boingo.com>
2015-05-26 22:59:13 -0700

    M  5/combination_sum.cpp A  5/combination_sum_ii.cpp

--- 5/combination_sum.cpp
+++ 5/combination_sum.cpp
@@ -1,35 +1,33 @@
+/*
+Given a set of candidate numbers (C) and a target number (T), find all unique combinations in C where the candidate numbers sums to T.
+
+The same repeated number may be chosen from C unlimited number of times.
+
+Note:
+All numbers (including target) will be positive integers.
+Elements in a combination (a1, a2, … , ak) must be in non-descending order. (ie, a1 ≤ a2 ≤ … ≤ ak).
+The solution set must not contain duplicate combinations.
+For example, given candidate set 2,3,6,7 and target 7, 
+A solution set is: 
+[7] 
+[2, 2, 3] 
+*/
+
 #include "header.h"
 
-void search(vector<int>& candidates, int curr_idx, int curr_sum, int target, vector<int>& temp, vector<vector<int> >& res){
-    if(curr_sum == target){
-        res.push_back(temp);
-        return;
-    }
-    if(curr_sum > target)
-        return;
-    if(curr_idx >= candidates.size())
-        return;
-    temp.push_back(candidates[curr_idx]);
-    search(candidates,curr_idx,curr_sum+candidates[curr_idx],target,temp,res);
-    temp.pop_back();
-    int i = curr_idx+1;
-    while(i<candidates.size() && candidates[i] == candidates[i-1]) i++;
-    search(candidates,i,curr_sum,target,temp,res);
-}
-
-vector<vector<int> > combinationSum(vector<int> &candidates, int target) {
+vector<vector<int> > combinationSum2(vector<int>& candidates, int target){
     vector<vector<int> > res;
-    if(candidates.size() == 0) return res;
-    std::sort(candidates.begin(), candidates.end());
-    vector<int> temp;
-    search(candidates, 0, 0, target, temp, res);
+    int sz = candidates.size();
+    if(sz == 0) return res;
+    vector<int> s;
+    sort(candidates.begin(), candidates.end());
     return res;
 }
 
 int main(){
-    int arr[] = {2, 2, 3, 6, 6, 7};
+    int arr[] = {2, 3, 6, 7};
     vector<int> candidates(arr,arr+sizeof(arr)/sizeof(arr[0]));
-    vector<vector<int> > res = combinationSum(candidates,7);
+    vector<vector<int> > res = combinationSum2(candidates,7);
     for(int i=0; i<res.size(); i++){
         for(int j=0; j<res[i].size(); j++)
             printf("%d ", res[i][j]);
