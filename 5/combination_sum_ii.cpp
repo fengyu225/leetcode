10a24e41e561c29720e6011a39a531363cfef5c3
Yu Feng <yfeng@boingo.com>
2015-05-27 00:17:17 -0700

    M  5/combination_sum_ii.cpp

--- 5/combination_sum_ii.cpp
+++ 5/combination_sum_ii.cpp
@@ -22,34 +22,35 @@ void search(vector<int>& num, int curr_idx, int curr_sum, int target, vector<int
         res.push_back(temp);
         return;
     }
-    if(curr_sum > target)
-        return;
-    if(curr_idx >= num.size())
-        return;
-    temp.push_back(num[curr_idx]);
-    search(num,curr_idx+1,curr_sum+num[curr_idx],target,temp,res);
-    temp.pop_back();
-    int i = curr_idx+1;
-    while(i<num.size() && num[i] == num[i-1]) i++;
-    search(num,i,curr_sum,target,temp,res);
+    if(curr_sum > target || curr_idx > num.size()) return;
+    int n = curr_idx+1;
+    while(n<num.size() && num[n] == num[n-1]) n++;
+    vector<int> x = temp;
+    for(int i=curr_idx; i<n; i++){
+        temp.push_back(num[curr_idx]);
+        search(num, n, curr_sum+(i-curr_idx+1)*num[curr_idx], target, temp, res);
+    }
+    temp = x;
+    search(num, n, curr_sum, target, temp, res);
 }
 
-vector<vector<int> > combinationSum2(vector<int> &num, int target) {
+vector<vector<int> > combinationSum2(vector<int>& candidates, int target) {
     vector<vector<int> > res;
-    if(num.size() == 0) return res;
-    std::sort(num.begin(), num.end());
+    if(candidates.size() == 0) return res;
+    std::sort(candidates.begin(), candidates.end());
     vector<int> temp;
-    search(num,0,0,target,temp,res);
+    search(candidates,0,0,target,temp,res);
     return res;
 }
 
 int main(){
     //int arr[] = {10,1,2,7,6,1,5};
+    int arr[] = {2,1,3,1,4};
     //int arr[] = {1,1,2,5,6,7,10};
-    int arr[] = {1,1};
+    //int arr[] = {1,1};
     vector<int> candidates(arr,arr+sizeof(arr)/sizeof(arr[0]));
-    //vector<vector<int> > res = combinationSum2(candidates,8);
-    vector<vector<int> > res = combinationSum2(candidates,1);
+    vector<vector<int> > res = combinationSum2(candidates,10);
+//    vector<vector<int> > res = combinationSum2(candidates,1);
     for(int i=0; i<res.size(); i++){
         for(int j=0; j<res[i].size(); j++)
             printf("%d ", res[i][j]);
