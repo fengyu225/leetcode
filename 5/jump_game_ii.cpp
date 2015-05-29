9ead018ff6335c18c9665e5dfa359405eb21e4e7
Yu Feng <yfeng@boingo.com>
2015-05-29 16:53:00 -0700

    A  5/jump_game_ii.cpp  M 5/revisit ?? 5/-std=c++11

---
+++ 5/jump_game_ii.cpp
@@ -0,0 +1,26 @@
+/*
+Given an array of non-negative integers, you are initially positioned at the first index of the array.
+
+Each element in the array represents your maximum jump length at that position.
+
+Your goal is to reach the last index in the minimum number of jumps.
+
+For example:
+Given array A = [2,3,1,1,4]
+
+The minimum number of jumps to reach the last index is 2. (Jump 1 step from index 0 to 1, then 3 steps to the last index.)
+*/
+
+#include "header.h"
+
+int jump(vector<int>& nums){
+    return 0;
+}
+
+int main(){
+    int A[] = {2, 3, 1, 1, 4};
+    vector<int> arr(A, A+sizeof(A)/sizeof(A[0]));
+    int res = jump(arr);
+    printf("%d\n", res);
+    return 0;
+}
