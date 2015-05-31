5864af2b02ca4dde684a96bcb9bfb3375b471fc1
Yu Feng <yfeng@boingo.com>
2015-05-31 08:46:37 -0700

    A  5/n_queens.cpp

---
+++ 5/n_queens.cpp
@@ -0,0 +1,56 @@
+/*
+The n-queens puzzle is the problem of placing n queens on an n×n chessboard such that no two queens attack each other.
+
+
+
+Given an integer n, return all distinct solutions to the n-queens puzzle.
+
+Each solution contains a distinct board configuration of the n-queens' placement, where 'Q' and '.' both indicate a queen and an empty space respectively.
+
+For example,
+There exist two distinct solutions to the 4-queens puzzle:
+
+[
+ [".Q..",  // Solution 1
+  "...Q",
+  "Q...",
+  "..Q."],
+
+ ["..Q.",  // Solution 2
+  "Q...",
+  "...Q",
+  ".Q.."]
+]
+*/
+
+#include "header.h"
+
+bool isValid(vector<int> A, int r){
+    for (int i=0;i<r;i++){
+        if ( (A[i]==A[r])||(abs(A[i]-A[r])==(r-i))){
+            return false;
+        }
+    }
+    return true;
+}
+
+void solve(vector<int>& A, int curr, int n, vector<vector<string> >& res){
+
+}
+
+vector<vector<string> > solveNQueens(int n) {
+    vector<vector<string> > res;
+    vector<int> A(n, -1);
+    solve(A, 0, n, res);
+    return res;
+}
+
+int main(){
+    vector<vector<string> > res = solveNQueens(8);
+    for(int i=0; i<res.size(); i++){
+        for(int j=0; j<res[i].size(); j++)
+            printf("%s \n", res[i][j].c_str());
+        printf("\n");
+    }
+    return 0;
+}
