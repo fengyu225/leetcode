3e3a45bb418c0c7e44f882abf0ed61f07fdcc681
Yu Feng <yfeng@boingo.com>
2015-06-01 16:13:29 -0700

    A  5/length_of_last_word.cpp

---
+++ 5/length_of_last_word.cpp
@@ -0,0 +1,26 @@
+/*
+Given a string s consists of upper/lower-case alphabets and empty space characters ' ', return the length of last word in the string.
+
+If the last word does not exist, return 0.
+
+Note: A word is defined as a character sequence consists of non-space characters only.
+
+For example, 
+Given s = "Hello World",
+return 5.
+*/
+
+#include "header.h"
+
+int lengthOfLastWord(string s) {
+        int i;int size = s.length();
+        while(s[size-1]==' ')   s.resize(--size);
+        i = s.find_last_of(' ');
+        return (i==-1)?size:size-i-1;
+}
+
+int main(){
+    char s[] = "Hello World s ";
+    printf("%d\n", lengthOfLastWord(s));
+    return 0;
+}
