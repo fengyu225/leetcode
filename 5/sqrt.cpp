04c1a056bf443f16e0525a724fb976f93fd873ce
Yu Feng <yfeng@boingo.com>
2015-06-02 11:28:18 -0700

    M  5/sqrt.cpp

--- 5/sqrt.cpp
+++ 5/sqrt.cpp
@@ -13,25 +13,36 @@ int mySqrt(int x){
     return l;
 }
 
+double mySqrt_f(double x, double f){
+    if(x == 0.0 || x==1.1) return x;
+    double curr = x;
+    double next = x;
+    do{
+        curr = next;
+        next = 0.5*(curr+x/curr);
+    }while(curr<next?next-curr>=f:curr-next>=f);
+    return curr;
+}
+
 int main(){
-//    printf("%.10f\n", mySqrt_f(4,0.00000000001f));
-//    printf("%.10f\n", mySqrt_f(5,0.00000000001f));
+    printf("%.10f\n", mySqrt_f(4,0.00000000001f));
+    printf("%.10f\n", mySqrt_f(5,0.00000000001f));
 //    printf("%d\n", mySqrt(5));
 //    printf("%d\n", mySqrt(2));
 //    printf("%d\n", mySqrt(0));
 //    printf("%d\n", mySqrt(1));
 //    printf("%d\n", mySqrt(16));
 //    printf("%d\n", mySqrt(2147395599));
-    printf("%d\n", mySqrt(0));
-    printf("%d\n", mySqrt(1));
-    printf("%d\n", mySqrt(2));
-    printf("%d\n", mySqrt(3));
-    printf("%d\n", mySqrt(4));
-    printf("%d\n", mySqrt(5));
-    printf("%d\n", mySqrt(6));
-    printf("%d\n", mySqrt(7));
-    printf("%d\n", mySqrt(8));
-    printf("%d\n", mySqrt(9));
-    printf("%d\n", mySqrt(10));
+//    printf("%d\n", mySqrt(0));
+//    printf("%d\n", mySqrt(1));
+//    printf("%d\n", mySqrt(2));
+//    printf("%d\n", mySqrt(3));
+//    printf("%d\n", mySqrt(4));
+//    printf("%d\n", mySqrt(5));
+//    printf("%d\n", mySqrt(6));
+//    printf("%d\n", mySqrt(7));
+//    printf("%d\n", mySqrt(8));
+//    printf("%d\n", mySqrt(9));
+//    printf("%d\n", mySqrt(10));
     return 0;
 }
