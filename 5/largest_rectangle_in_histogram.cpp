ff6776fd5ed94bdb867daca835e50ff18f754777
Yu Feng <yfeng@boingo.com>
2015-06-04 15:06:07 -0700

    A  2/lib.cpp A  2/lib.h M  5/largest_rectangle_in_histogram.cpp A  5/max_rectangle.cpp

--- 5/largest_rectangle_in_histogram.cpp
+++ 5/largest_rectangle_in_histogram.cpp
@@ -3,19 +3,42 @@
 //http://fisherlei.blogspot.com/2012/12/leetcode-largest-rectangle-in-histogram.html
 //http://www.geeksforgeeks.org/largest-rectangle-under-histogram/
 
-void find_rec(vector<int>& height, vector<int>& v){
-
+void find_left(vector<int>& height, vector<int>& v){
+    if(height.size() == 0) return;
+    stack<int> s;
+    height.push_back(0);
+    for(int i=0; i<height.size(); i++){
+        if(s.empty() || height[s.top()]<height[i]){
+            s.push(i);
+            continue;
+        }
+        while(!s.empty() && height[s.top()]>=height[i]){
+            int x = s.top();
+            s.pop();
+            v[x] = s.empty()?height[x]*(x+1):height[x]*(x-s.top());
+        }
+        s.push(i);
+    }
+    height.pop_back();
 }
 
 int largestRectangleArea(vector<int>& height){
-    vector<int> right;
-    find_rec(height, right);
-    return 0;
+    vector<int> left(height.size(), 0);
+    vector<int> right(height.size(), 0);
+    find_left(height, left);
+    reverse(height.begin(), height.end());
+    find_left(height, right);
+    reverse(right.begin(), right.end());
+    reverse(height.begin(), height.end());
+    int res = 0;
+    for(int i=0; i<left.size(); i++)
+        res = max(res, left[i]+right[i]-height[i]);
+    return res;
 }
 
 int main(){
-    int height[] = {6, 2, 5, 4, 5, 1, 6};
-//    int height[] = {2,4};
+//    int height[] = {6, 2, 5, 4, 5, 1, 6};
+    int height[] = {2,4};
     vector<int> h_v(height,height+sizeof(height)/sizeof(height[0]));
     printf("%d\n", largestRectangleArea(h_v));
     return 0;
---
+++ 5/max_rectangle.cpp
@@ -0,0 +1,75 @@
+/*
+Given a 2D binary matrix filled with 0's and 1's, find the largest rectangle containing all ones and return its area.
+*/
+
+#include "header.h"
+
+void find_left(vector<int>& h, vector<int>& left){
+    stack<int> stack;
+    h.push_back(0);
+    for(int i=0; i<h.size(); i++){
+        if(stack.empty() || h[stack.top()]<h[i]){
+            stack.push(i);
+            continue;
+        }
+        while(!stack.empty() && h[stack.top()]>=h[i]){
+            int x = stack.top();
+            stack.pop();
+            left[x] = stack.empty()?h[x]*(x+1):h[x]*(x-stack.top());
+        }
+        stack.push(i);
+    }
+    h.pop_back();
+}
+
+int largestRectangleArea(vector<int> &h) {   
+    vector<int> left(h.size(),0);
+    vector<int> right(h.size(),0);
+    find_left(h,left);
+    std::reverse(h.begin(), h.end());
+    find_left(h,right);
+    std::reverse(right.begin(), right.end());
+    std::reverse(h.begin(), h.end());
+    int res = 0;
+    for(int i=0; i<h.size(); i++)
+        res = std::max(res,(left[i]+right[i]-h[i]));
+    return res;
+}
+
+int maximalRectangle(vector<vector<char> > &matrix) {
+    vector<vector<int> > vs;
+    for(int i=0; i<matrix.size(); i++){
+        vector<int> v;
+        for(int j=0; j<matrix[i].size(); j++){
+            if(vs.empty())
+                v.push_back(matrix[i][j]-'0');
+            else{
+                v.push_back(matrix[i][j]=='0'?0:vs.back()[j]+1);
+            }
+        }
+        vs.push_back(v);
+    }
+    int res = 0;
+    for(int i=0; i<vs.size(); i++)
+        res = std::max(largestRectangleArea(vs[i]), res);
+    return res;
+}
+
+int main(){
+    char arr0[] = "10100";
+    char arr1[] = "01110";
+    char arr2[] = "01110";
+    char arr3[] = "01110";
+    vector<char> v0(arr0,arr0+sizeof(arr0)/sizeof(arr0[0])-1);
+    vector<char> v1(arr1,arr1+sizeof(arr1)/sizeof(arr1[1])-1);
+    vector<char> v2(arr2,arr2+sizeof(arr2)/sizeof(arr2[2])-1);
+    vector<char> v3(arr3,arr3+sizeof(arr3)/sizeof(arr3[3])-1);
+    vector<vector<char> > matrix;
+    matrix.push_back(v0);
+    matrix.push_back(v1);
+    matrix.push_back(v2);
+    matrix.push_back(v3);
+    int res = maximalRectangle(matrix);
+    printf("%d\n", res);
+    return 0;
+}
