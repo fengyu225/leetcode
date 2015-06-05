257c150951fcad09397a66ac3435b0e42f5be4eb
Yu Feng <yfeng@boingo.com>
2015-06-04 18:44:00 -0700

    M  5/scramble_string.cpp

--- 5/scramble_string.cpp
+++ 5/scramble_string.cpp
@@ -37,14 +37,43 @@ Given two strings s1 and s2 of the same length, determine if s2 is a scrambled s
 #include "header.h"
 
 bool isScramble(string s1, string s2){
+    int s1_len = s1.length(), s2_len = s2.length();
+    if(s1_len != s2_len) return false;
+    if(s1_len == 1) return s1 == s2;
+    bool arr[s1_len][s2_len][s1_len+1];
+    memset(arr, 0, sizeof(arr));
+    for(int len=1; len<=s1_len; len++){
+        for(int i=0; i+len<=s1_len; i++){
+            for(int j=0; j+len<=s2_len; j++){
+                if(len == 1) arr[i][j][1] = s1[i]==s2[j]?true:false;
+                else{
+                    for(int p=1;p<len;p++)
+                        arr[i][j][len] |= (
+                                (arr[i][j][p] && arr[i+p][j+p][len-p]) ||
+                                (arr[i][len+j-p][p] && arr[i+p][j][len-p])
+                                );
+                }
+            }
+        }
+    }
+    for(int len=1; len<=s1_len; len++){
+        cout<<"==================== "<<len<<endl;
+        for(int i=0; i<s1_len; i++){
+            for(int j=0; j<s1_len; j++)
+                cout<<arr[i][j][len]<<" ";
+            cout<<endl;
+        }
+    }
+    return arr[0][0][s1_len];
 }
 
 int main(){
     printf("%d\n", isScramble("great", "rgtae"));
-    printf("%d\n", isScramble("great", "great"));
-    printf("%d\n", isScramble("abc", "bca"));
-    printf("%d\n", isScramble("abc", "cab"));
-    printf("%d\n", isScramble("abcd", "bcad"));
-    printf("%d\n", isScramble("abcd", "cabd"));
+//      printf("%d\n", isScramble("eat", "tae"));
+//    printf("%d\n", isScramble("great", "great"));
+//    printf("%d\n", isScramble("abc", "bca"));
+//    printf("%d\n", isScramble("abc", "cab"));
+//    printf("%d\n", isScramble("abcd", "bcad"));
+//    printf("%d\n", isScramble("abcd", "cabd"));
     return 0;
 }
