955e5aa59c4161b1c26ee5b2987d4294fc74d3ee
Yu Feng <yfeng@boingo.com>
2015-06-04 22:52:57 -0700

    M  5/reverse_linked_list_ii.cpp

--- 5/reverse_linked_list_ii.cpp
+++ 5/reverse_linked_list_ii.cpp
@@ -13,14 +13,52 @@ Given m, n satisfy the following condition:
 
 #include "header.h"
 
+void reverse(ListNode* head, ListNode* tail){
+    if(head->next == NULL || head == tail) return;
+    ListNode* curr=head, *next = curr->next;
+    curr->next = NULL;
+    while(next){
+        ListNode* temp = next->next;
+        next->next = curr;
+        curr = next;
+        next = temp;
+    }
+}
+
 ListNode* reverseBetween(ListNode* head, int m, int n) {
-    return head;
+    ListNode* front_head = NULL, *front_tail = NULL;
+    ListNode* middle_head = NULL, *middle_tail = NULL;
+    ListNode* last_head = NULL;
+    ListNode* curr = head;
+    int s = 1;
+    while(curr){
+        if(s == m-1){
+            front_head = head;
+            front_tail = curr;
+        }
+        if(s == m) middle_head = curr;
+        if(s == n) middle_tail = curr;
+        if(s == n+1) last_head = curr;
+        s++;
+        curr = curr->next;
+    }
+    middle_tail->next = NULL;
+    reverse(middle_head, middle_tail);
+    ListNode* res = NULL;
+    if(front_head){
+        front_tail->next = middle_tail;
+        res = front_head;
+    }
+    else res = middle_tail;
+    middle_head->next = last_head;
+    return res;
 }
 
 int main(){
-    int arr[] = {1, 2, 3, 4, 5};
+    //int arr[] = {1, 2, 3, 4, 5};
+    int arr[] = {5};
     ListNode* l = create_list(arr,sizeof(arr)/sizeof(arr[0]));
-    l = reverseBetween(l,1,5);
+    l = reverseBetween(l,1,1);
     while(l){
         printf("%d ", l->val);
         l = l->next;
