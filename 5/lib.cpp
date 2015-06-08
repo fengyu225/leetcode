b86d6892224f0408a9b93e71df1f8a376575995b
Yu Feng <yfeng@boingo.com>
2015-06-08 16:52:14 -0700

    M  5/lib.cpp M  5/symmetric_tree.cpp

--- 5/lib.cpp
+++ 5/lib.cpp
@@ -1,10 +1,12 @@
 #include "lib.h"
 #include <string>
 #include <queue>
+#include <iostream>
 
 using namespace std;
 
 ListNode* create_list(int arr[], int n){
+    cout<<"======================"<<endl;
     ListNode* result = NULL;
     ListNode* curr = NULL;
     for(int i=0; i<n; i++){
@@ -17,26 +19,35 @@ ListNode* create_list(int arr[], int n){
         }
     }
     return result;
+    cout<<"======================"<<endl;
 }
 
 TreeNode* create_tree(string v){
+    cout<<"======================"<<endl;
     if(v[0] == '#') return NULL;
     queue<TreeNode*> q;
     TreeNode* root = new TreeNode(v[0]-'0');
     TreeNode* curr_n = root;
     q.push(curr_n);
     int curr = 1;
-    while(!q.empty() && curr_n){
+    while(!q.empty()){
+        curr_n = q.front();
         TreeNode* l = curr<v.length() && v[curr] != '#'?new TreeNode(v[curr]-'0'):NULL;
         TreeNode* r = curr+1<v.length() && v[curr+1] != '#'?new TreeNode(v[curr+1]-'0'):NULL;
-        q.push(l);
-        q.push(r);
+        cout<<curr<<" "<<curr_n->val<<" ";
+        if(l) cout<<l->val<<" "; 
+        else cout<<"NULL"<<" ";
+        if(r) cout<<r->val<<" ";
+        else cout<<"NULL"<<" ";
+        cout<<endl;
+        if(l) q.push(l);
+        if(r) q.push(r);
         curr_n->left = l;
         curr_n->right = r;
         q.pop();
         curr += 2;
-        curr_n = l?l:r; 
     }
+    cout<<"======================"<<endl;
     return root;
 }
 
@@ -47,16 +58,22 @@ TreeNode* create_tree(vector<string> v){
     TreeNode* curr_n = root;
     q.push(curr_n);
     int curr = 1;
-    while(!q.empty() && curr_n){
+    while(!q.empty()){
+        curr_n = q.front();
         TreeNode* l = curr<v.size() && v[curr] != "#"?new TreeNode(stoi(v[curr])):NULL;
         TreeNode* r = curr+1<v.size() && v[curr+1] != "#"?new TreeNode(stoi(v[curr+1])):NULL;
-        q.push(l);
-        q.push(r);
+        cout<<curr<<" "<<curr_n->val<<" ";
+        if(l) cout<<l->val<<" "; 
+        else cout<<"NULL"<<" ";
+        if(r) cout<<r->val<<" ";
+        else cout<<"NULL"<<" ";
+        cout<<endl;
+        if(l) q.push(l);
+        if(r) q.push(r);
         curr_n->left = l;
         curr_n->right = r;
         q.pop();
         curr += 2;
-        curr_n = l?l:r; 
     }
     return root;
 }
--- 5/symmetric_tree.cpp
+++ 5/symmetric_tree.cpp
@@ -7,6 +7,7 @@ For example, this binary tree is symmetric:
   2   2
  / \ / \
 3  4 4  3
+
     1
    / \
   2   2
@@ -17,7 +18,57 @@ Bonus points if you could solve it both recursively and iteratively.
 
 #include "header.h"
 
+/* using recursion
+bool check(TreeNode* left, TreeNode* right){
+    if(!left && !right) return true;
+    if(left && right)
+        return left->val==right->val && check(left->left, right->right) && check(left->right, right->left);
+    return false;
+}
+
 bool isSymmetric(TreeNode* root){
+    if(!root) return true;
+    return check(root->left, root->right);
+}
+*/
+
+bool check(vector<string>& v){
+    int l=0, r=v.size()-1;
+    while(l<r){
+        if(v[l] != v[r]) return false;
+        l++;
+        r--;
+    }
+    return true;
+}
+
+bool isSymmetric(TreeNode* root){
+    if(!root) return true;
+    queue<TreeNode*> curr_q;
+    queue<TreeNode*> next_q;
+    curr_q.push(root);
+    queue<TreeNode*> qs[2] = {curr_q, next_q};
+    vector<string> v;
+    int x = 0;
+    while(!qs[x%2].empty()){
+        TreeNode* temp = qs[x%2].front();
+        qs[x%2].pop();
+        if(temp->left){
+            qs[(x+1)%2].push(temp->left);
+            v.push_back(to_string(temp->left->val));
+        }
+        else v.push_back("#");
+        if(temp->right){
+            qs[(x+1)%2].push(temp->right);
+            v.push_back(to_string(temp->right->val));
+        }
+        else v.push_back("#");
+        if(qs[x%2].empty()){
+            if(!check(v)) return false;
+            v.clear();
+            x++;
+        }
+    }
     return true;
 }
 
@@ -26,7 +77,9 @@ int main(){
     vector<string> v_1(arr_1, arr_1+sizeof(arr_1)/sizeof(arr_1[0]));
     string arr_2[] = {"1","2","2","#","3","#","3"};
     vector<string> v_2(arr_2, arr_2+sizeof(arr_2)/sizeof(arr_2[0]));
-    TreeNode* t1 = create_tree(v_1);
-    TreeNode* t2 = create_tree(v_2);
+    TreeNode* t1 = create_tree("1223443");
+    TreeNode* t2 = create_tree("122#3#3");
+    cout<<isSymmetric(t1)<<endl;
+    cout<<isSymmetric(t2)<<endl;
     return 0;
 }
