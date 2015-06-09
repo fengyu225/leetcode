d9421dee2ac2b9f73e26a281c80959c7a0ed570c
Yu Feng <yfeng@boingo.com>
2015-06-09 00:01:45 -0700

    M  5/minimum_depth_of_binary_tree.cpp ?? 5/a..dSYM/

--- 5/minimum_depth_of_binary_tree.cpp
+++ 5/minimum_depth_of_binary_tree.cpp
@@ -7,7 +7,12 @@ The minimum depth is the number of nodes along the shortest path from the root n
 #include "header.h"
 
 int minDepth(TreeNode *root){
-    return 0;
+    if(!root) return 0;
+    if(!root->left && !root->right) return 1;
+    int l=INT_MAX, r=INT_MAX;
+    if(root->left) l = minDepth(root->left);
+    if(root->right) r = minDepth(root->right);
+    return min(l,r)+1;
 }
 
 int main(){
