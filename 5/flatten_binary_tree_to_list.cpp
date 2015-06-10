45f311780183f4f702d1016de73365726ec3b3a1
Yu Feng <yfeng@boingo.com>
2015-06-09 17:30:04 -0700

    M  5/flatten_binary_tree_to_list.cpp

--- 5/flatten_binary_tree_to_list.cpp
+++ 5/flatten_binary_tree_to_list.cpp
@@ -24,14 +24,31 @@ The flattened tree should look like:
 
 #include "header.h"
 
+void flatten(TreeNode* root, TreeNode*& head, TreeNode*& tail){
+    if(!root){
+        head = tail = NULL;
+        return;
+    }
+    TreeNode* l_head = NULL, *l_tail=NULL, *r_head=NULL, *r_tail=NULL;
+    flatten(root->left, l_head, l_tail);
+    flatten(root->right, r_head, r_tail);
+    root->left = NULL;
+    root->right = l_head?l_head:r_head;
+    if(l_head) l_tail->right=r_head;
+    head=root;
+    tail=r_tail?r_tail:l_tail?l_tail:head;
+    tail->right = NULL;
+}
+
 void flatten(TreeNode* root){
-    
+    TreeNode* head = NULL, *tail=NULL;
+    flatten(root, head, tail);
 }
 
 int main(){
     TreeNode* root = create_tree("12534#6");
     flatten(root);
-    TreeNode* curr = &n1;
+    TreeNode* curr = root;
     while(curr){
         printf("%d ", curr->val);
         curr = curr->right;
