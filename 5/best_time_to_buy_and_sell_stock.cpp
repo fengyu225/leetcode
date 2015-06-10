2f9e2a74f8bcb90b1e27d7e6159a506b1259f1af
Yu Feng <yfeng@boingo.com>
2015-06-09 20:09:44 -0700

    M  5/best_time_to_buy_and_sell_stock.cpp

--- 5/best_time_to_buy_and_sell_stock.cpp
+++ 5/best_time_to_buy_and_sell_stock.cpp
@@ -7,12 +7,21 @@ If you were only permitted to complete at most one transaction (ie, buy one and 
 #include "header.h"
 
 int maxProfit(vector<int>& prices){
-
+    int sz = prices.size();
+    if(sz == 1) return 0;
+    int arr[sz], res=0;
+    arr[0] = INT_MAX;
+    for(int i=1; i<sz; i++){
+        arr[i] = min(arr[i-1],prices[i-1]);
+        cout<<prices[i]<<" "<<arr[i]<<endl;
+        res = max(res,prices[i]-arr[i]);
+    }
+    return res;
 }
 
 int main(){
     //int prices_arr[] = {0, 1, 2, 3};
-    int prices_arr[] = {2,1};
+    int prices_arr[] = {1,2,4};
     vector<int> prices(prices_arr,prices_arr+sizeof(prices_arr)/sizeof(prices_arr[0]));
     printf("%d\n", maxProfit(prices));
     return 0;
