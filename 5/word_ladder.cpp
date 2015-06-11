4d7a7e37a00a8cc102635dfc13467bdb4bcafd06
Yu Feng <yfeng@boingo.com>
2015-06-10 18:45:14 -0700

    A  5/word_ladder.cpp

---
+++ 5/word_ladder.cpp
@@ -0,0 +1,61 @@
+/*
+Given two words (beginWord and endWord), and a dictionary, find the length of shortest transformation sequence from beginWord to endWord, such that:
+
+Only one letter can be changed at a time
+Each intermediate word must exist in the dictionary
+For example,
+
+Given:
+start = "hit"
+end = "cog"
+dict = ["hot","dot","dog","lot","log"]
+As one shortest transformation is "hit" -> "hot" -> "dot" -> "dog" -> "cog",
+return its length 5.
+
+Note:
+Return 0 if there is no such transformation sequence.
+All words have the same length.
+All words contain only lowercase alphabetic characters.
+*/
+
+#include "header.h"
+
+int ladderLength(string beginWord, string endWord, unordered_set<string>& wordDict){
+    int res = 1;
+    if(beginWord == endWord) return res;
+    queue<string> curr;
+    queue<string> next;
+    curr.push(beginWord);
+    queue<string> arr[2] = {curr,next};
+    unordered_set<string> has_seen;
+    has_seen.insert(beginWord);
+    int x = 0;
+    while(!arr[x%2].empty()){
+        string temp = arr[x%2].front();
+        arr[x%2].pop();
+        if(temp == endWord) return res;
+        for(int i=0; i<temp.length(); i++)
+            for(int j='a'; j<='z'; j++){
+                char c = temp[i];
+                temp[i] = j;
+                if(wordDict.find(temp) != wordDict.end() && has_seen.find(temp) == has_seen.end()){
+                    arr[(x+1)%2].push(temp);
+                    has_seen.insert(temp);
+                }
+                temp[i] = c;
+            }
+        if(arr[x%2].empty()){
+            res++;
+            x++;
+        }
+    }
+    return 0;
+}
+
+int main(){
+    string start("hit");
+    string end("cog");
+    unordered_set<string> dict({"hot","dot","dog","lot","log"});
+    printf("%d\n", ladderLength(start,end,dict));
+    return 0;
+}
