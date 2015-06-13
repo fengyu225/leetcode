25e2b9c99c846237f36f8713a9e97ed87891a94e
Yu Feng <yfeng@boingo.com>
2015-06-13 14:17:10 -0700

    M  5/word_break.cpp

--- 5/word_break.cpp
+++ 5/word_break.cpp
@@ -10,15 +10,28 @@ Return true because "leetcode" can be segmented as "leet code".
 
 #include "header.h"
 
+bool search(string& s, int curr, unordered_set<string>& wordDict){
+    if(curr==s.length()) return true;
+    bool res = false;
+    for(int i=curr; i<s.length(); i++){
+        string temp = s.substr(curr, i-curr+1);
+        if(wordDict.find(temp) == wordDict.end()) continue;
+        res |= search(s,i+1,wordDict);
+    } 
+    return res;
+}
+
 bool wordBreak(string s, unordered_set<string>& wordDict) {
-    return false;
+    int sz = s.length();
+    if(sz == 0) return 0;
+    return search(s,0,wordDict);
 }
 
 int main(){
     unordered_set<string> dict;
     dict.insert("leet");
     dict.insert("code");
-    string s("leetcodeleetleet");
+    string s("leetcodeletleet");
     printf("%d\n", wordBreak(s,dict));
     return 0;
 }
