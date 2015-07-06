d8fb0727524aad3ba65c29c91571e5a9ee5c4f18
Yu Feng <yfeng@boingo.com>
2015-07-06 16:41:56 -0700

    M  5/repeated_dna_seq.cpp

--- 5/repeated_dna_seq.cpp
+++ 5/repeated_dna_seq.cpp
@@ -13,9 +13,39 @@ Return:
 #include "header.h"
 
 vector<string> findRepeatedDnaSequences(string s){
-
+    vector<string> res;
+    if(s.length()<=10) return res; 
+    unordered_set<int> set;
+    unordered_set<string> res_set;
+    unordered_map<int,int> m;
+    m['A'] = 0;
+    m['C'] = 1;
+    m['G'] = 2;
+    m['T'] = 3;
+    int num = 0;
+    int x = 0;
+    for(int i=0; i<20; i++) x |= (1<<i);
+    int curr = 0;
+    while(curr<10){
+        num <<= 2;
+        num |= m[s[curr++]];
+    }
+    set.insert(num);
+    while(curr<s.length()){
+        num <<= 2;
+        num |= m[s[curr++]];
+        num &= x;
+        if(set.find(num) != set.end()) res_set.insert(s.substr(curr-10, 10));
+        else set.insert(num);
+    }
+    for(auto i = res_set.begin(); i!=res_set.end(); i++) res.push_back(*i);
+    return res;
 }
 
 int main(){
+    string s = "AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT";
+    vector<string> res = findRepeatedDnaSequences(s);
+    for(int i=0; i<res.size(); i++) cout<<res[i]<<" ";
+    cout<<endl;
     return 0;
 }
