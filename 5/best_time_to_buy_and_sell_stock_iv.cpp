59f0e7c3027f6e44d7d77ef96bc4ab33209dda8e
Yu Feng <yfeng@boingo.com>
2015-07-06 16:53:22 -0700

    A  5/best_time_to_buy_and_sell_stock_iv.cpp

---
+++ 5/best_time_to_buy_and_sell_stock_iv.cpp
@@ -0,0 +1,18 @@
+/*
+Say you have an array for which the ith element is the price of a given stock on day i.
+
+Design an algorithm to find the maximum profit. You may complete at most k transactions.
+
+Note:
+You may not engage in multiple transactions at the same time (ie, you must sell the stock before you buy again).
+*/
+
+#include "header.h"
+
+int maxProfit(int k, vector<int>& prices){
+
+}
+
+int main(){
+    return 0;
+}
