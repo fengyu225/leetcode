84c6e5db39ba158887a77d66aaa1e7ec637ffc53
Yu Feng <yfeng@boingo.com>
2015-07-06 12:18:35 -0700

    A  5/excel_sheet_col_num.cpp M  5/factorial_trailing_zero.cpp

---
+++ 5/excel_sheet_col_num.cpp
@@ -0,0 +1,29 @@
+/*
+Related to question Excel Sheet Column Title
+
+Given a column title as appear in an Excel sheet, return its corresponding column number.
+
+For example:
+    A -> 1
+    B -> 2
+    C -> 3
+    ...
+    Z -> 26
+    AA -> 27
+    AB -> 28 
+*/
+
+#include "header.h"
+
+int titleToNumber(string s){
+    int res = 0;
+    for(int i=0; i<s.length(); i++){
+        res = res*26+(s[i]-'A')+1;
+    }
+    return res;
+}
+
+int main(){
+    cout<<titleToNumber("AB")<<endl;
+    return 0;
+}
--- 5/factorial_trailing_zero.cpp
+++ 5/factorial_trailing_zero.cpp
@@ -11,14 +11,6 @@ Special thanks to @ts for adding this problem and creating all test cases.
 
 int count(int s, int n){
     //count number of appearence of n in s!
-    if(s<n) return 0;
-    int curr = s;
-    int res = 0;
-    while(curr>=n){
-        res += curr/n;
-        curr = curr/n;
-    }
-    return res;
 }
 
 int trailingZeroes(int n) {
