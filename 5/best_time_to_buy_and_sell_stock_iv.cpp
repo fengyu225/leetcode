b4b18482f7114ed08a6bc9574eef607c8255b142
Yu Feng <yfeng@boingo.com>
2015-07-06 19:40:17 -0700

    M  5/best_time_to_buy_and_sell_stock_iv.cpp

--- 5/best_time_to_buy_and_sell_stock_iv.cpp
+++ 5/best_time_to_buy_and_sell_stock_iv.cpp
@@ -10,7 +10,27 @@ You may not engage in multiple transactions at the same time (ie, you must sell 
 #include "header.h"
 
 int maxProfit(int k, vector<int>& prices){
-
+    if(k == 0) return 0;
+    int sz = prices.size();
+    if(sz < 2) return 0;
+    int local[k+1][sz];
+    int global[k+1][sz];
+    memset(local, 0, sizeof(local));
+    memset(global, 0, sizeof(global));
+    for(int i=1; i<sz; i++){
+        int diff = prices[i] - prices[i-1];
+        for(int j = 1; j<=k; j++){
+            local[j][i] = max(
+                    global[j-1][i-1]+max(diff, 0),
+                    local[j][i-1]+diff
+                    );
+            global[j][i] = max(
+                    global[j][i-1],
+                    local[j][i]
+                    );
+        }
+    }
+    return global[k][sz-1];
 }
 
 int main(){
