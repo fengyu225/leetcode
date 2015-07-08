6e5bd5c34c360a456892130c95d9ae502ba8d535
Yu Feng <yfeng@boingo.com>
2015-07-07 18:32:48 -0700

    M  5/divide_int_without_operator.cpp

--- 5/divide_int_without_operator.cpp
+++ 5/divide_int_without_operator.cpp
@@ -14,6 +14,7 @@ int divide(int dividend, int divisor) {
     if(divisor == 0) return flag?INT_MAX:INT_MIN;
     unsigned int dd = dividend>=0?dividend:-1*dividend;
     unsigned int ds = divisor>=0?divisor:-1*divisor;
+    unsigned int orig_dd = dd, orig_ds = ds;
     if(dd<ds) return 0;
     unsigned int res = 0;
     for(int i=31; i>=0; i--){
@@ -22,6 +23,8 @@ int divide(int dividend, int divisor) {
             dd -= (ds<<i);
         }
     }
+    if(res > INT_MAX) cout<<"remainder: "<<0<<endl;
+    else cout<<"remainder: "<<orig_dd-orig_ds*res<<endl;
     if(res > INT_MAX) return flag?INT_MAX:INT_MIN;
     else return flag?res:-1*res;
 }
@@ -29,10 +32,9 @@ int divide(int dividend, int divisor) {
 int main(){
 //    printf("%d\n", divide(-999511578, -2147483648));
 //    printf("%d\n", divide(100579234, -555806774));
-    cout<<sizeof(long)<<" "<<sizeof(int)<<endl;
     printf("%d\n", divide(-2147483648, -1));
     printf("%d\n", divide(-2147483648, 1));
-//    printf("%d\n", divide(5, 2));
-//    printf("%d\n", divide(1, -1));
+    printf("%d\n", divide(5, 2));
+    printf("%d\n", divide(1, -1));
     return 0;
 }
