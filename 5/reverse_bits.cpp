20f0ceea264bfced411bbdfe5553aa3a8567fa44
Yu Feng <yfeng@boingo.com>
2015-07-10 14:27:33 -0700

    M  5/reverse_bits.cpp

--- 5/reverse_bits.cpp
+++ 5/reverse_bits.cpp
@@ -12,9 +12,26 @@ Related problem: Reverse Integer
 #include "header.h"
 
 uint32_t reverseBits(uint32_t n){
+    uint32_t res = 0;
+    int l = 31, r = 0;
+    while(r<l){
+        res |= ((n>>l)&1)<<(31-l);
+        res |= ((n>>r)&1)<<(31-r);
+        l--;
+        r++;
+    }
+    return res;
 
+//    uint32_t x = n;
+//    x = (((x & 0xaaaaaaaa) >> 1) | ((x & 0x55555555) << 1));
+//    x = (((x & 0xcccccccc) >> 2) | ((x & 0x33333333) << 2));
+//    x = (((x & 0xf0f0f0f0) >> 4) | ((x & 0x0f0f0f0f) << 4));
+//    x = (((x & 0xff00ff00) >> 8) | ((x & 0x00ff00ff) << 8));
+//    return((x >> 16) | (x << 16));
 }
 
 int main(){
+    cout<<reverseBits(43261596)<<endl;
+    //cout<<reverseBits(1)<<endl;
     return 0;
 }
