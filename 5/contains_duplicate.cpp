08e1da851849f089cddf48510b79a9dc3aceabaf
Yu Feng <yfeng@boingo.com>
2015-07-17 11:17:35 -0700

    M  5/contains_duplicate.cpp A  5/contains_duplicate_ii.cpp

--- 5/contains_duplicate.cpp
+++ 5/contains_duplicate.cpp
@@ -5,9 +5,19 @@ Given an array of integers, find if the array contains any duplicates. Your func
 #include "header.h"
 
 bool containsDuplicate(vector<int>& nums) {
-
+    int sz = nums.size();
+    if(sz<2) return false;
+    unordered_set<int> set;
+    for(int i=0; i<nums.size(); i++){
+        if(set.find(nums[i]) != set.end()) return true;
+        set.insert(nums[i]);
+    }
+    return false;
 }
 
 int main(){
+    int arr[] = {0, 1, 1, 2, 3};
+    vector<int> v_arr(arr, arr+sizeof(arr)/sizeof(arr[0]));
+    cout<<containsDuplicate(v_arr)<<endl;
     return 0;
 }
---
+++ 5/contains_duplicate_ii.cpp
@@ -0,0 +1,14 @@
+/*
+Given an array of integers and an integer k, find out whether there there are two distinct indices i and j in the array such that nums[i] = nums[j] and the difference between i and j is at most k.
+*/
+
+#include "header.h"
+
+bool containsNearbyDuplicate(vector<int>& nums, int k){
+
+}
+
+int main(){
+
+    return 0;
+}
