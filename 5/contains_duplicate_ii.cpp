564915abcd6d8029c0331862405447a295db12c7
Yu Feng <yfeng@boingo.com>
2015-07-17 12:44:06 -0700

    M  5/contains_duplicate_ii.cpp A  5/contains_duplicate_iii.cpp

--- 5/contains_duplicate_ii.cpp
+++ 5/contains_duplicate_ii.cpp
@@ -26,9 +26,9 @@ bool containsNearbyDuplicate(vector<int>& nums, int k){
 }
 
 int main(){
-    //int arr[] = {3, 1, 2, 1, 2, 3};
-    int arr[] = {99,99};
+    int arr[] = {1,2,3,1,3};
+//    int arr[] = {99,99};
     vector<int> v_arr(arr, arr+sizeof(arr)/sizeof(arr[0]));
-    cout<<containsNearbyDuplicate(v_arr, 2)<<endl;
+    cout<<containsNearbyDuplicate(v_arr, 3)<<endl;
     return 0;
 }
---
+++ 5/contains_duplicate_iii.cpp
@@ -0,0 +1,14 @@
+/*
+Given an array of integers, find out whether there are two distinct indices i and j in the array such that the difference between nums[i] and nums[j] is at most t and the difference between i and j is at most k.
+*/
+
+#include "header.h"
+
+bool containsNearbyAlmostDuplicate(vector<int>& nums, int k, int t){
+
+}
+
+int main(){
+
+    return 0;
+}
