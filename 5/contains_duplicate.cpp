21ef035da38f070a29b517fa3cf56c41abab2677
Yu Feng <yfeng@boingo.com>
2015-07-17 11:10:13 -0700

    A  5/contains_duplicate.cpp

---
+++ 5/contains_duplicate.cpp
@@ -0,0 +1,13 @@
+/*
+Given an array of integers, find if the array contains any duplicates. Your function should return true if any value appears at least twice in the array, and it should return false if every element is distinct.
+*/
+
+#include "header.h"
+
+bool containsDuplicate(vector<int>& nums) {
+
+}
+
+int main(){
+    return 0;
+}
