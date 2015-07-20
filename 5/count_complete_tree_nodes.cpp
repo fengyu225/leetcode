e4331acc0a869a837435c6274e17cdfcd1e02d81
Yu Feng <yfeng@boingo.com>
2015-07-20 11:18:41 -0700

    M  5/count_complete_tree_nodes.cpp A  5/rectangle_area.cpp

--- 5/count_complete_tree_nodes.cpp
+++ 5/count_complete_tree_nodes.cpp
@@ -17,28 +17,45 @@ int getHeight(TreeNode* root){
 }
 
 int countNodes(TreeNode* root) {
-    //iterative
     if(!root) return 0;
-    if(!root->left && !root->right) return 1;
-    int leaves = 0, level=1;
+    int res = 1;
     TreeNode* curr = root;
-    while(curr->left){
-        int l_h = getHeight(curr->left);
-        int r_h = getHeight(curr->right);
-        level = max(level, l_h+1);
-        if(!curr->right){
-            leaves++;
-            break;
-        }
-        if(l_h == r_h){
-            leaves += (1<<(l_h-1));
-            if(curr->right->left == NULL) leaves++;
+    int height = getHeight(root);
+    while(--height){
+        res <<= 1;
+        int r_height = getHeight(curr->right);
+        if(height == r_height){
+            res |= 1;
             curr = curr->right;
         }
         else curr=curr->left;
     }
-    return (1<<(level-1))-1+leaves;
+    return res;
 }
+
+//int countNodes(TreeNode* root) {
+//    //iterative
+//    if(!root) return 0;
+//    if(!root->left && !root->right) return 1;
+//    int leaves = 0, level=1;
+//    TreeNode* curr = root;
+//    while(curr->left){
+//        int l_h = getHeight(curr->left);
+//        int r_h = getHeight(curr->right);
+//        level = max(level, l_h+1);
+//        if(!curr->right){
+//            leaves++;
+//            break;
+//        }
+//        if(l_h == r_h){
+//            leaves += (1<<(l_h-1));
+//            if(l_h == 1) leaves++;
+//            curr = curr->right;
+//        }
+//        else curr=curr->left;
+//    }
+//    return (1<<(level-1))-1+leaves;
+//}
 
 //int getLeftHeight(TreeNode* root){
 //    int res = 0;
@@ -58,8 +75,8 @@ int countNodes(TreeNode* root) {
 //}
 
 int main(){
-//    TreeNode* root = create_tree("1234567898989");
-    TreeNode* root = create_tree("1234");
+    TreeNode* root = create_tree("1234567898989");
+//    TreeNode* root = create_tree("1234");
     //vector<string> v(arr, arr+sizeof(arr)/sizeof(arr[0]));
     //TreeNode* root = create_tree("123456789");
     //TreeNode* root = create_tree(v);
