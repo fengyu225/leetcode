9190e83946ffffb4f0e8e1a2341b0f0fcccf6b44
Yu Feng <yfeng@boingo.com>
2015-07-31 16:09:48 -0700

    M  5/strstr.cpp

--- 5/strstr.cpp
+++ 5/strstr.cpp
@@ -30,43 +30,47 @@ The signature of the function had been updated to return the index instead of th
 
 void create_next(string& needle, vector<int>& next){
     int n_sz = needle.length();
-    for(int i=1; i<next.size(); i++){
-        int last = next[i-1];
-        if(last>=0 && needle[last]==needle[i]) next[i]=last+1;
-        else next[i] = 0;
+    int k = -1, curr = 0;
+    next[0] = -1;
+    while(curr<n_sz){
+        if(k == -1 || needle[k] == needle[curr]){
+            k++;
+            curr++;
+            next[curr] = k;
+        }
+        else k=next[k];
     }
-    for(int i=0; i<next.size(); i++) cout<<next[i]<<" ";
-    cout<<endl;
 }
 
 // kmp
 int strStr(string haystack, string needle){
-    int h_sz = haystack.length(), n_sz = needle.length();
+    int h_sz = haystack.length();
+    int n_sz = needle.length();
+    if(n_sz>h_sz) return -1;
     if(n_sz == 0) return 0;
-    vector<int> next(n_sz, -1);
-    create_next(needle, next);
-    int h_start = 0, curr = 0;
-    while(h_start<=h_sz-n_sz){
-        if(haystack[h_start+curr] == needle[curr]){
-            if(curr == n_sz-1) return h_start;
-            curr++;
-            continue;
+    vector<int> next(n_sz+1, -1);
+    create_next(needle, next); 
+    int h_curr = 0, n_curr = 0;
+    while(h_curr<h_sz){
+        if(haystack[h_curr] == needle[n_curr]){
+            h_curr++;
+            n_curr++;
+            if(n_curr == n_sz) return h_curr-n_curr;
         }
-        if(curr == 0) h_start++;
-        else{
-            int m = next[curr-1]==-1?curr-1:next[curr-1];
-            h_start+=curr-m;
-            curr = 0;
+        else if(next[n_curr] == -1){
+            h_curr++;
+            n_curr = 0;
         }
+        else n_curr = next[n_curr];
     }
     return -1;
 }
 
 int main(){
-    //string haystack = "BBC ABCDAB ABCDABCDABDE";
-    //string needle = "ABCDABD";
-    string haystack = "babbbbbabb"; 
-    string needle = "bbab";
+    string haystack = "BBC ABCDAB ABCDABCDABDE";
+    string needle = "ABCDABD";
+//    string haystack = "babbbbbabb"; 
+//    string needle = "bbab";
     int res = strStr(haystack,needle);
     if(res >= 0)
         cout<<haystack.substr(res, needle.length())<<endl;
