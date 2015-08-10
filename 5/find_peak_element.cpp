9827c47d01c46c2d70e6893799516590a4653431
Yu Feng <yfeng@boingo.com>
2015-08-10 00:17:08 -0700

    M  5/find_peak_element.cpp M  5/longest_substr_with_at_most_two_distinct_chars.cpp

--- 5/find_peak_element.cpp
+++ 5/find_peak_element.cpp
@@ -13,26 +13,41 @@ For example, in array [1, 2, 3, 1], 3 is a peak element and your function should
 #include "header.h"
 
 int findPeakElement(vector<int>& nums){
+    //log(n) solution
     int sz = nums.size();
-    if(sz == 1) return nums[0];
-    vector<bool> arr(sz, false);
-    for(int i=0; i<sz; i++) if(i == 0 || nums[i]>nums[i-1]) arr[i] = true;
-    for(int i=sz-1; i>=0; i--){
-        if(i == sz-1 || nums[i]>nums[i+1]) arr[i] = arr[i]&true;
-        else arr[i] = false;
+    if(sz == 1) return 0;
+    int l = 0, r = sz-1, m=(l+r)/2;
+    while(l<r){
+        m = (l+r)/2;
+        if((m == 0 || nums[m-1]<nums[m]) && (m==sz-1 || nums[m+1]<nums[m])) return m;
+        else if(nums[m]<nums[m+1]) l = m+1;
+        else r=m-1;
     }
-    int res = 0;
-    for(int i=0; i<sz; i++){
-        if(arr[i]){
-            res = i;
-            break;
-        }
-    }
-    return res;
+    return l;
 }
 
+//int findPeakElement(vector<int>& nums){
+//    int sz = nums.size();
+//    if(sz == 1) return nums[0];
+//    vector<bool> arr(sz, false);
+//    for(int i=0; i<sz; i++) if(i == 0 || nums[i]>nums[i-1]) arr[i] = true;
+//    for(int i=sz-1; i>=0; i--){
+//        if(i == sz-1 || nums[i]>nums[i+1]) arr[i] = arr[i]&true;
+//        else arr[i] = false;
+//    }
+//    int res = 0;
+//    for(int i=0; i<sz; i++){
+//        if(arr[i]){
+//            res = i;
+//            break;
+//        }
+//    }
+//    return res;
+//}
+
 int main(){
-    int arr[] = {1, 2, 3, 1};
+    int arr[] = {1, 2, 3, 4};
+//    int arr[] = {2, 1};
     vector<int> v(arr,arr+sizeof(arr)/sizeof(arr[0]));
     cout<<findPeakElement(v)<<endl;
     return 0;
--- 5/longest_substr_with_at_most_two_distinct_chars.cpp
+++ 5/longest_substr_with_at_most_two_distinct_chars.cpp
@@ -9,9 +9,27 @@ T is "ece" which its length is 3.
 #include "header.h"
 
 int lengthOfLongestSubstringTwoDistinct(string s){
-
+    int count = 0, l=0, res = 0, sz=s.length();
+    int map[256] = {0};
+    for(int i=0; i<sz; i++){
+        if(map[s[i]] == 0) count++;
+        map[s[i]]++;
+        if(count<=2) res=max(res, i-l+1);
+        else{
+            while(count>2){
+                if(map[s[l]] == 1) count--;
+                map[s[l++]]--;
+            }
+        }
+    }
+    return res;
 }
 
 int main(){
+    cout<<lengthOfLongestSubstringTwoDistinct("")<<endl;
+    cout<<lengthOfLongestSubstringTwoDistinct("e")<<endl;
+    cout<<lengthOfLongestSubstringTwoDistinct("eeeeeeeee")<<endl;
+    cout<<lengthOfLongestSubstringTwoDistinct("abcdefg")<<endl;
+    cout<<lengthOfLongestSubstringTwoDistinct("eceba")<<endl;
     return 0;
 }
