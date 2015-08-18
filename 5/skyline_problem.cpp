6f173f96b98d824e619bec4b1b9917b78bfead7d
Yu Feng <yfeng@boingo.com>
2015-08-18 00:06:25 -0700

    M  5/skyline_problem.cpp

--- 5/skyline_problem.cpp
+++ 5/skyline_problem.cpp
@@ -20,32 +20,90 @@ There must be no consecutive horizontal lines of equal height in the output skyl
 
 #include "header.h"
 
+class q_cmp{
+    public:
+        bool operator() (pair<int,int> l, pair<int,int> r){
+            return l.second<r.second;
+        }
+};
+
 class cmp{
     public:
         bool operator() (pair<int,int> l, pair<int,int> r){
-            return r.second<l.second;
+            return abs(l.second)<abs(r.second);
         }
 };
 
 vector<pair<int, int> > getSkyline(vector<vector<int> >& buildings){
     vector<pair<int, int> > res;
-    priority_queue<pair<int,int>, vector<pair<int,int> >, cmp> bound_q; 
-    priority_queue<pair<int,int>, vector<pair<int,int> >, cmp> height_q; 
-    int last_h = 0;
+    priority_queue<pair<int,int>, vector<pair<int,int> >, q_cmp> height_q; 
+    vector<pair<int,int> > points;
+    for(int i=0; i<buildings.size(); i++){
+        if(buildings.size()>=2 && i<buildings.size()-1 && buildings[i][0] == buildings[i+1][0] && buildings[i][1] == buildings[i+1][1]){
+            buildings[i+1][2] = max(buildings[i][2], buildings[i+1][2]);
+            buildings.erase(buildings.begin()+i);
+            i--;
+        }
+    }
     int sz = buildings.size();
     for(int i=0; i<sz; i++){
+        points.push_back(make_pair(i, buildings[i][0]));
+        points.push_back(make_pair(i, -1*buildings[i][1]));
+    }
+    sort(points.begin(), points.end(), cmp());
+    for(int i=0; i<points.size(); i++){
+        int h;
+        if(points[i].second>=0){
+            h = buildings[points[i].first][2];
+            height_q.push(make_pair(points[i].first, h)); 
+            while(!height_q.empty() && buildings[height_q.top().first][1]<points[i].second)
+                height_q.pop();
+            h = height_q.top().second;
+            if(res.empty() || res.back().second<h) res.push_back(make_pair(points[i].second, h));
+        }
+        else{
+            while(!height_q.empty() && buildings[height_q.top().first][1]<-1*points[i].second)
+                height_q.pop();
+            if(!height_q.empty() && height_q.top().first == points[i].first){
+                height_q.pop();
+                while(!height_q.empty() && buildings[height_q.top().first][1]<-1*points[i].second)
+                    height_q.pop();
+                if(height_q.empty()){
+                    res.push_back(make_pair(-1*points[i].second, 0));
+                }
+                else{
+                    if(res.empty() || res.back().second>height_q.top().second)
+                        res.push_back(make_pair(-1*points[i].second, height_q.top().second));
+                }
+            } 
+        }
+    }
+    for(int i=0; i<res.size(); i++){
+        if(i-2>=0 && res[i].first == res[i-1].first && res[i].second == res[i-2].second){
+            res.erase(res.begin()+(i-1), res.begin()+i+1);
+            i-=2;
+        }
     }
     return res;
 }
 
 int main(){
+//    vector<vector<int> > buildings = {
+//        {2, 9, 10},
+//        {3, 7, 15},
+//        {5, 12, 12},
+//        {15, 20, 10},
+//        {19, 24, 8}
+//    };    
     vector<vector<int> > buildings = {
-        {2, 9, 10},
-        {3, 7, 15},
-        {5, 12, 12},
-        {15, 20, 10},
-        {19, 24, 8}
+        {4, 10, 10},
+        {5, 10, 9},
+        {6, 10, 8},
+        {7, 10, 7},
+        {8, 10, 6},
+        {9, 10, 5}
     };
+
     vector<pair<int,int> > res = getSkyline(buildings);
     for(int i=0; i<res.size(); i++)
         cout<<res[i].first<<" "<<res[i].second<<", ";
