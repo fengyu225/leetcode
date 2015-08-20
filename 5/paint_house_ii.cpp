0028119d4b9c7ce9dad610a375125c2854cee232
Yu Feng <yfeng@boingo.com>
2015-08-20 14:06:58 -0700

    M  5/paint_house_ii.cpp

--- 5/paint_house_ii.cpp
+++ 5/paint_house_ii.cpp
@@ -13,37 +13,79 @@ Could you solve it in O(nk) runtime?
 #include "header.h"
 
 int minCostII(vector<vector<int> >& costs){
-    //O(n*k*lg(k))
     int n = costs.size();
     if(n == 0) return 0;
     int k = costs[0].size();
     vector<vector<int> > arr(n, vector<int>(k, INT_MAX));
+    pair<int,int> min_arr = make_pair(-1, -1); 
     for(int i=0; i<n; i++){
-        multiset<int> q;
-        if(i != 0){
-            for(int j=0; j<k; j++) q.insert(arr[i-1][j]);
-        }
         for(int j=0; j<k; j++){
-            if(i == 0) arr[i][j] = costs[i][j];
+            if(i == 0) arr[i][j] = costs[i][j]; 
+            else
+                arr[i][j] = costs[i][j]+(j==min_arr.first?arr[i-1][min_arr.second]:arr[i-1][min_arr.first]);
+        } 
+        min_arr.first = min_arr.second = -1;
+        for(int j=0; j<k; j++){
+            if(min_arr.first==-1) min_arr.first = j;
+            else if(min_arr.second == -1){
+                if(arr[i][j]<arr[i][min_arr.first]){
+                    min_arr.second = min_arr.first;
+                    min_arr.first = j;
+                }
+                else min_arr.second = j;
+            }
             else{
-                q.erase(q.find(arr[i-1][j]));
-                arr[i][j] = *q.begin()+costs[i][j];
-                q.insert(arr[i-1][j]);
+                if(arr[i][j]<arr[i][min_arr.first]){
+                    min_arr.second = min_arr.first;
+                    min_arr.first = j;
+                }
+                else if(arr[i][j]<arr[i][min_arr.second]) min_arr.second = j;
             }
         }
     }
-    int res = INT_MAX;
-    for(int i=0; i<k; i++)
-        res = min(res, arr[n-1][i]);
-    return res;
+    return arr[n-1][min_arr.first]; 
 }
 
+//int minCostII(vector<vector<int> >& costs){
+//    //O(n*k*lg(k))
+//    int n = costs.size();
+//    if(n == 0) return 0;
+//    int k = costs[0].size();
+//    vector<vector<int> > arr(n, vector<int>(k, INT_MAX));
+//    for(int i=0; i<n; i++){
+//        multiset<int> q;
+//        if(i != 0){
+//            for(int j=0; j<k; j++) q.insert(arr[i-1][j]);
+//        }
+//        for(int j=0; j<k; j++){
+//            if(i == 0) arr[i][j] = costs[i][j];
+//            else{
+//                q.erase(q.find(arr[i-1][j]));
+//                arr[i][j] = *q.begin()+costs[i][j];
+//                q.insert(arr[i-1][j]);
+//            }
+//        }
+//    }
+//    int res = INT_MAX;
+//    for(int i=0; i<k; i++)
+//        res = min(res, arr[n-1][i]);
+//    return res;
+//}
+
 int main(){
+
     vector<vector<int> > costs = {
-        {1, 2, 3},
-        {4, 5, 6},
-        {7, 8, 9}
+        {3,14,12,2,20,16,12,2},
+        {9,6,9,8,2,9,20,18},
+        {20,2,20,4,5,12,11,11},
+        {16,3,7,5,15,2,2,4},
+        {17,3,11,1,9,5,7,11}
     };
+//    vector<vector<int> > costs = {
+//        {1, 2, 3},
+//        {4, 5, 6},
+//        {7, 8, 9}
+//    };
     cout<<minCostII(costs)<<endl;
     return 0;
 }
