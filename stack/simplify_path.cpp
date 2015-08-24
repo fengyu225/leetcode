3ca68b155444ef33cfc653fe322c4bab6992e4e0
Yu Feng <yfeng@boingo.com>
2015-08-24 00:36:07 -0700

    A  stack/simplify_path.cpp

---
+++ stack/simplify_path.cpp
@@ -0,0 +1,44 @@
+/*
+Given an absolute path for a file (Unix-style), simplify it.
+
+For example,
+path = "/home/", => "/home"
+path = "/a/./b/../../c/", => "/c"
+*/
+
+#include "header.h"
+
+string simplifyPath(string path) {
+    int sz = path.length();
+    string res = "";
+    stack<string> s;
+    int curr = 0;
+    string temp = "";
+    while(curr<sz){
+        if(path[curr] == '/' && temp.length()>0){
+            if(temp != "." && temp != "..") s.push(temp);
+            else if(temp == ".."){
+                if(!s.empty()) s.pop();
+            }
+            temp = "";
+        }
+        else if(path[curr] != '/'){
+            temp += path[curr];
+        }
+        curr++;
+    }
+    if(s.empty()) return "/";
+    while(!s.empty()){
+        string temp = s.top();
+        res += "/"+temp+res;
+        s.pop();
+    }
+    return res;
+}
+
+int main(){
+    cout<<simplifyPath("/a/./b/../../c/")<<endl;
+    cout<<simplifyPath("/")<<endl;
+    cout<<simplifyPath("/a//b/../../c/")<<endl;
+    return 0;
+}
