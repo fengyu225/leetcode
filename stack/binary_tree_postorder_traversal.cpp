ef7d313b0a056b0e31fdce7be6882e7e936716bc
Yu Feng <yfeng@boingo.com>
2015-08-28 19:35:01 -0700

    M  stack/binary_tree_postorder_traversal.cpp

--- stack/binary_tree_postorder_traversal.cpp
+++ stack/binary_tree_postorder_traversal.cpp
@@ -18,11 +18,41 @@ Note: Recursive solution is trivial, could you do it iteratively?
 vector<int> postorderTraversal(TreeNode* root) {
     vector<int> res;
     if(!root) return res;
-    
+    TreeNode* curr_level = root;
+    stack<TreeNode*> st;
+    st.push(root);
+    while(!st.empty()){
+        TreeNode* curr = st.top();
+        if(curr_level == curr){
+            if(curr->left || curr->right){
+                TreeNode* n = curr->left?curr->left:curr->right;
+                st.push(n);
+                curr_level = n;
+            }
+            else{
+                res.push_back(curr->val);
+                st.pop();
+                curr_level = curr;
+            }
+        }
+        else{
+            if(curr_level == curr->left && curr->right){
+                st.push(curr->right);
+                curr_level = curr->right;
+            }
+            else{
+                res.push_back(curr->val);
+                st.pop();
+                curr_level = curr;
+            }
+        }
+    }
+    return res;
 }
 
 int main(){
-    TreeNode* root = create_tree("1#23");
+    //TreeNode* root = create_tree("1#23");
+    TreeNode* root = create_tree("312");
     vector<int> res = postorderTraversal(root);
     print_vector<int>(res);
     return 0;
