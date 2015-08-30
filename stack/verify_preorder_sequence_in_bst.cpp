e4cb9649d79329ee510be0fcbdd37e4fcb218609
Yu Feng <yfeng@boingo.com>
2015-08-29 17:04:25 -0700

    M  stack/verify_preorder_sequence_in_bst.cpp

--- stack/verify_preorder_sequence_in_bst.cpp
+++ stack/verify_preorder_sequence_in_bst.cpp
@@ -10,9 +10,27 @@ Could you do it using only constant space complexity?
 #include "header.h"
 
 bool verifyPreorder(vector<int>& preorder) {
-
+    int sz = preorder.size();
+    stack<int> st;
+    int curr_p = INT_MIN;
+    for(int i=0; i<sz; i++){
+        if(st.empty() || st.top()>preorder[i]){
+            if(preorder[i]<curr_p) return false;
+            st.push(preorder[i]);
+        }
+        else{
+            while(!st.empty() && st.top()<preorder[i]){
+                curr_p = st.top();
+                st.pop();
+            }
+            st.push(preorder[i]);
+        }
+    }
+    return true;
 }
 
 int main(){
-    vector<
+    vector<int> preorder = {6, 3, 1, 5, 4, 7, 9, 8};
+    cout<<verifyPreorder(preorder)<<endl;
+    return 0;
 }
