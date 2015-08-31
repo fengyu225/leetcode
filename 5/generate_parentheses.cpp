cb5c4de111d30307590847102ea26b38768c851b
Yu Feng <yfeng@boingo.com>
2015-08-31 14:18:04 -0700

    M  5/generate_parentheses.cpp ?? 5/generate_parentheses.cpp_bk

--- 5/generate_parentheses.cpp
+++ 5/generate_parentheses.cpp
@@ -8,7 +8,28 @@ For example, given n = 3, a solution set is:
 
 #include "header.h"
 
+void search(int n, int curr_open, int balanced, string& temp, vector<string>& res){
+    if(curr_open == 0 && balanced == n){
+        res.push_back(temp);
+        return;
+    }
+    if(curr_open+balanced>n) return;
+    string orig = temp;
+    if(curr_open>0){
+        temp += ")";
+        search(n, curr_open-1, balanced+1, temp, res);
+        temp = orig;
+    }
+    temp += "(";
+    search(n, curr_open+1, balanced, temp, res);
+    temp = orig;
+}
+
 vector<string> generateParenthesis(int n) {
+    vector<string> res;
+    string temp = "";
+    search(n, 0, 0, temp, res);
+    return res;
 }
 
 int main(){
