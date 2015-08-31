5685246a0b24d3e5564ed800f2b64d8b1f79cb0e
Yu Feng <yfeng@boingo.com>
2015-08-31 15:52:26 -0700

    M  5/different_ways_to_add_parentheses.cpp ?? 5/generate_parentheses.cpp_bk

--- 5/different_ways_to_add_parentheses.cpp
+++ 5/different_ways_to_add_parentheses.cpp
@@ -38,25 +38,6 @@ vector<int> search(vector<string>& input, int start, int end){
     return res;
 }
 
-vector<int> diffwaystocompute(string input){
-    vector<int> res;
-    int sz = input.length();
-    if(sz == 0) return res;
-    vector<string> input_arr;
-    int curr = 1, head = 0;
-    while(curr<sz){
-        if(input[curr] == '+' || input[curr] == '-' || input[curr] == '*'){
-            input_arr.push_back(input.substr(head, curr-head));
-            input_arr.push_back(input.substr(curr, 1));
-            curr++;
-            head = curr;
-        }
-        else curr++;
-    }
-    input_arr.push_back(input.substr(head, curr-head));
-    return search(input_arr, 0, input_arr.size()-1);
-}
-
 vector<int> diffWaysToCompute(string input){
     vector<int> res;
     int sz = input.length();
@@ -73,33 +54,54 @@ vector<int> diffWaysToCompute(string input){
         else curr++;
     }
     input_arr.push_back(input.substr(head, curr-head));
-    sz = input_arr.size();
-    vector<vector<vector<int> > > table(sz, vector<vector<int> >(sz, vector<int>()));
-    for(int len=1; len<=sz; len+=2){
-        for(int start = 0; start+len-1<sz; start+=2){
-            vector<int> temp;
-            if(len == 1){
-                temp.push_back(stoi(input_arr[start]));
-                table[start][start+len-1] = temp;
-            }
-            else{
-                for(int l_len = 1; l_len<len; l_len+=2){
-                    vector<int> left = table[start][start+l_len-1];
-                    vector<int> right = table[start+l_len+1][start+len-1];
-                    for(int l_idx = 0; l_idx<left.size(); l_idx++){
-                        for(int r_idx=0; r_idx<right.size(); r_idx++){
-                            if(input_arr[start+l_len] == "+") temp.push_back(left[l_idx]+right[r_idx]);
-                            if(input_arr[start+l_len] == "-") temp.push_back(left[l_idx]-right[r_idx]);
-                            if(input_arr[start+l_len] == "*") temp.push_back(left[l_idx]*right[r_idx]);
-                        }
-                    }
-                } 
-            }
-            table[start][start+len-1] = temp;
-        }   
-    }
-    return table[0][input_arr.size()-1];
+    for(string x:input_arr) cout<<x<<", ";
+    cout<<endl;
+    return search(input_arr, 0, input_arr.size()-1);
 }
+
+//vector<int> diffWaysToCompute(string input){
+//    vector<int> res;
+//    int sz = input.length();
+//    if(sz == 0) return res;
+//    vector<string> input_arr;
+//    int curr = 1, head = 0;
+//    while(curr<sz){
+//        if(input[curr] == '+' || input[curr] == '-' || input[curr] == '*'){
+//            input_arr.push_back(input.substr(head, curr-head));
+//            input_arr.push_back(input.substr(curr, 1));
+//            curr++;
+//            head = curr;
+//        }
+//        else curr++;
+//    }
+//    input_arr.push_back(input.substr(head, curr-head));
+//    sz = input_arr.size();
+//    vector<vector<vector<int> > > table(sz, vector<vector<int> >(sz, vector<int>()));
+//    for(int len=1; len<=sz; len+=2){
+//        for(int start = 0; start+len-1<sz; start+=2){
+//            vector<int> temp;
+//            if(len == 1){
+//                temp.push_back(stoi(input_arr[start]));
+//                table[start][start+len-1] = temp;
+//            }
+//            else{
+//                for(int l_len = 1; l_len<len; l_len+=2){
+//                    vector<int> left = table[start][start+l_len-1];
+//                    vector<int> right = table[start+l_len+1][start+len-1];
+//                    for(int l_idx = 0; l_idx<left.size(); l_idx++){
+//                        for(int r_idx=0; r_idx<right.size(); r_idx++){
+//                            if(input_arr[start+l_len] == "+") temp.push_back(left[l_idx]+right[r_idx]);
+//                            if(input_arr[start+l_len] == "-") temp.push_back(left[l_idx]-right[r_idx]);
+//                            if(input_arr[start+l_len] == "*") temp.push_back(left[l_idx]*right[r_idx]);
+//                        }
+//                    }
+//                } 
+//            }
+//            table[start][start+len-1] = temp;
+//        }   
+//    }
+//    return table[0][input_arr.size()-1];
+//}
 
 int main(){
     vector<int> res = diffWaysToCompute("2*3-4*5");
