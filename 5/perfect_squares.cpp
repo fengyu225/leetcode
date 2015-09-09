c11007bc87f9dc7a7b51c35ff5543f180a5b1685
Yu Feng <yfeng@boingo.com>
2015-09-09 15:18:11 -0700

    M  5/perfect_squares.cpp ?? 5/generate_parentheses.cpp_bk

--- 5/perfect_squares.cpp
+++ 5/perfect_squares.cpp
@@ -7,10 +7,25 @@ For example, given n = 12, return 3 because 12 = 4 + 4 + 4; given n = 13, return
 #include "header.h"
 
 int numSquares(int n) {
-
+    vector<int> arr(n+1, 0);
+    for(int i=1; i<=n; i++){
+        arr[i] = i;
+        for(int j = 1; j*j<=i; j++){
+            if(j*j == i) {
+                arr[i] = 1;
+                break;
+            }
+            arr[i] = min(arr[i], arr[i-j*j]+1); 
+        }
+    }
+    return arr[n];
 }
 
 int main(){
-
+    cout<<numSquares(12)<<endl;
+    cout<<numSquares(INT_MAX-2)<<endl;
+    cout<<numSquares(13)<<endl;
+    cout<<numSquares(1)<<endl;
+    cout<<numSquares(4)<<endl;
     return 0;
 }
