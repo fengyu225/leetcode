96c107920bb6f0473800e9d15cab16fbc16583f6
Yu Feng <yfeng@boingo.com>
2015-09-15 17:29:32 -0700

    M  array/longest_consecutive_sequence.cpp

--- array/longest_consecutive_sequence.cpp
+++ array/longest_consecutive_sequence.cpp
@@ -12,13 +12,32 @@ Your algorithm should run in O(n) complexity.
 
 int longestConsecutive(vector<int>& nums) {
     int sz = nums.size();
-    unordered_map<int,int> m;
-     
+    unordered_map<int,pair<int,int> > m;
+    int res = 1;
+    for(int i=0; i<sz; i++){
+        if(m.find(nums[i]) != m.end()) continue;
+        m[nums[i]] = make_pair(0, 0);
+        if(m.find(nums[i]-1) != m.end()){
+            m[nums[i]].first = m[nums[i]-1].first+1;
+            m[nums[i]].second = max(0, m[nums[i]-1].second-1);
+            if(m[nums[i]].first != 0)
+                m[nums[i]-m[nums[i]].first].second = m[nums[i]].first;
+        }
+        if(m.find(nums[i]+1) != m.end()){
+            m[nums[i]].second = max(m[nums[i]].second, m[nums[i]+1].second+1);
+            if(m[nums[i]].second != 0) 
+                m[nums[i]+m[nums[i]].second].first = m[nums[i]].second+m[nums[i]].first;
+            if(m[nums[i]].first != 0) 
+                m[nums[i]-m[nums[i]].first].second = m[nums[i]].first+m[nums[i]].second;
+        }
+        res = max(res, m[nums[i]].second+m[nums[i]].first+1);
+    }
+    return res;
 }
 
 int main(){
-    int arr[] = {100, 4, 200, 1, 3, 2};
-    vector<int> num(arr,arr+sizeof(arr)/sizeof(arr[0]));
+    //vector<int> num = {100, 4, 200, 1, 3, 2};
+    vector<int> num = {1, 3, 5, 2, 4};
     printf("%d\n", longestConsecutive(num));
     return 0;
 }
