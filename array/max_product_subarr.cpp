4e0ebe11526aa3545b19d340cbe6d909015d099d
Yu Feng <yfeng@boingo.com>
2015-09-15 20:00:32 -0700

    M  array/max_product_subarr.cpp

--- array/max_product_subarr.cpp
+++ array/max_product_subarr.cpp
@@ -7,6 +7,7 @@ the contiguous subarray [2,3] has the largest product = 6.
 
 #include "header.h"
 
+/*
 int maxProduct(vector<int>& nums) {
     int sz = nums.size();
     pair<int,int>* arr = new pair<int,int>[sz];
@@ -27,10 +28,25 @@ int maxProduct(vector<int>& nums) {
     }
     return res;
 }
+*/
+
+int maxProduct(vector<int>& nums) {
+    int sz = nums.size();
+    if(sz  == 0) return 0;
+    int res = nums[0], curr_max = nums[0], curr_min = nums[0];
+    for(int i=1; i<sz; i++){
+        int temp = curr_max;
+        curr_max = max(nums[i], max(curr_max*nums[i], curr_min*nums[i]));
+        curr_min = min(nums[i], min(temp*nums[i], curr_min*nums[i]));
+        res = max(res, curr_max);
+    }
+    return res;
+}
 
 int main(){
 //    int arr[] = {2, 3, -2, 4};
-    int arr[] = {-2, 0, -1};
+//    int arr[] = {-2, 0, -1};
+    int arr[] = {-4, -3, -2};
 //    int arr[] = {0,2};
     vector<int> v(arr, arr+sizeof(arr)/sizeof(arr[0]));
     cout<<maxProduct(v)<<endl;
