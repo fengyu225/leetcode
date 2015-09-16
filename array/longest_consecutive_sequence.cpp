5fb92bd155270bb143dbb3185561230b65805081
Yu Feng <yfeng@boingo.com>
2015-09-15 18:48:46 -0700

    M  array/longest_consecutive_sequence.cpp

--- array/longest_consecutive_sequence.cpp
+++ array/longest_consecutive_sequence.cpp
@@ -28,34 +28,52 @@ Let's walk through this example: [1, 3, 5, 2, 4]. We traverse the array:
 The following is code in c++:
 */
 
+//int longestConsecutive(vector<int>& nums) {
+//    int sz = nums.size();
+//    unordered_map<int,pair<int,int> > m;
+//    int res = 1;
+//    for(int i=0; i<sz; i++){
+//        if(m.find(nums[i]) != m.end()) continue;
+//        m[nums[i]] = make_pair(0, 0);
+//        if(m.find(nums[i]-1) != m.end()){
+//            m[nums[i]].first = m[nums[i]-1].first+1;
+//            m[nums[i]].second = max(0, m[nums[i]-1].second-1);
+//            if(m[nums[i]].first != 0)
+//                m[nums[i]-m[nums[i]].first].second = m[nums[i]].first;
+//        }
+//        if(m.find(nums[i]+1) != m.end()){
+//            m[nums[i]].second = max(m[nums[i]].second, m[nums[i]+1].second+1);
+//            if(m[nums[i]].second != 0) 
+//                m[nums[i]+m[nums[i]].second].first = m[nums[i]].second+m[nums[i]].first;
+//            if(m[nums[i]].first != 0) 
+//                m[nums[i]-m[nums[i]].first].second = m[nums[i]].first+m[nums[i]].second;
+//        }
+//        res = max(res, m[nums[i]].second+m[nums[i]].first+1);
+//    }
+//    return res;
+//}
+
 int longestConsecutive(vector<int>& nums) {
-    int sz = nums.size();
-    unordered_map<int,pair<int,int> > m;
-    int res = 1;
+    int sz = nums.size(), res = 1;
+    unordered_set<int> s;
+    for(int i=0; i<sz; i++) s.insert(nums[i]);
     for(int i=0; i<sz; i++){
-        if(m.find(nums[i]) != m.end()) continue;
-        m[nums[i]] = make_pair(0, 0);
-        if(m.find(nums[i]-1) != m.end()){
-            m[nums[i]].first = m[nums[i]-1].first+1;
-            m[nums[i]].second = max(0, m[nums[i]-1].second-1);
-            if(m[nums[i]].first != 0)
-                m[nums[i]-m[nums[i]].first].second = m[nums[i]].first;
+        if(s.find(nums[i]) == s.end()) continue;
+        int curr = nums[i]-1, temp = 0;
+        while(s.find(curr) != s.end()){
+            s.erase(s.find(curr--));
+            temp++;
         }
-        if(m.find(nums[i]+1) != m.end()){
-            m[nums[i]].second = max(m[nums[i]].second, m[nums[i]+1].second+1);
-            if(m[nums[i]].second != 0) 
-                m[nums[i]+m[nums[i]].second].first = m[nums[i]].second+m[nums[i]].first;
-            if(m[nums[i]].first != 0) 
-                m[nums[i]-m[nums[i]].first].second = m[nums[i]].first+m[nums[i]].second;
+        curr = nums[i]+1;
+        while(s.find(curr) != s.end()){
+            s.erase(s.find(curr++));
+            temp++;
         }
-        res = max(res, m[nums[i]].second+m[nums[i]].first+1);
+        s.erase(s.find(nums[i]));
+        res = max(res, temp+1);
     }
     return res;
 }
-
-//int longestConsecutive(vector<int>& nums) {
-//
-//}
 
 int main(){
     //vector<int> num = {100, 4, 200, 1, 3, 2};
