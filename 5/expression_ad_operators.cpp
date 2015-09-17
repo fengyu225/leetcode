1fb6fedabc72e877e7274775a7285cdc5d39a94f
Yu Feng <yfeng@boingo.com>
2015-09-16 17:42:28 -0700

    M  5/expression_ad_operators.cpp

--- 5/expression_ad_operators.cpp
+++ 5/expression_ad_operators.cpp
@@ -11,17 +11,40 @@ Examples:
 // 3:24
 #include "header.h"
 
-vector<string> addOperators(string num, int target) {
-    int sz = num.length();
-    if(sz == 1) return num[0]-'0' == target?vector<string>(1, num):vector<string>(); 
-    vector<vector<vector<int>* > > arr(sz-1, vector<vector<int>* >(3, NULL));
-    for(int i=0; i<sz-1; i++){
-         
+void search(string& num, int l, int r, unordered_map<string,int>& m){
+    if(l>r) return;
+    if(l == r){
+        m[num.substr(l, 1)] = num[l]-'0';
+        return;
+    }
+    for(int i=l; i<r; i++){
+        unordered_map<string,int> l_m;
+        unordered_map<string,int> r_m;
+        search(num, l, i, l_m);
+        search(num, i+1, r, r_m);
+        for(auto l_i:l_m){
+            for(auto r_i:r_m){
+                m[l_i.first+"+"+r_i.first] = l_i.second+r_i.second;
+                m[l_i.first+"-"+r_i.first] = l_i.second-r_i.second;
+                m[l_i.first+"*"+r_i.first] = l_i.second*r_i.second;
+            }
+        }
     }
 }
 
+vector<string> addOperators(string num, int target) {
+    int sz = num.length();
+    if(sz == 1) return num[0]-'0' == target?vector<string>(1, num):vector<string>(); 
+    unordered_map<string,int> m;
+    search(num, 0, sz-1, m);
+    vector<string> res;
+    for(auto i:m) if(i.second == target) res.push_back(i.first);
+    return res;
+}
+
 int main(){
-    vector<string> res = addOperators("123", 6);
+    //vector<string> res = addOperators("123", 6);
+    vector<string> res = addOperators("00", 0);
     print_vector<string>(res);
     return 0;
 }
