b7826af237bca9e20c3ac3b3c7544115e02693c1
Yu Feng <yfeng@boingo.com>
2015-09-17 20:45:05 -0700

    M  array/minimum_size_subarray_sum.cpp

--- array/minimum_size_subarray_sum.cpp
+++ array/minimum_size_subarray_sum.cpp
@@ -12,12 +12,23 @@ If you have figured out the O(n) solution, try coding another solution of which 
 
 #include "header.h"
 
+// O(n) solution
 int minSubArrayLen(int s, vector<int>& nums) {
-    
+    int sz = nums.size(); 
+    int l = 0, r = 0, res = sz+1, curr_sum = 0;
+    for(;r<sz;r++){
+        curr_sum += nums[r];
+        if(curr_sum<s) continue;
+        while(curr_sum>=s) curr_sum -= nums[l++];
+        res = min(res, r-l+2);
+    }
+    return res == sz+1?0:res;
 }
+
 int main(){
     int arr[] = {2, 3, 1, 2, 4, 3};
     vector<int> nums(arr,arr+sizeof(arr)/sizeof(arr[0]));
-    cout<<minSubArrayLen(INT_MAX,nums)<<endl;
+    //cout<<minSubArrayLen(INT_MAX,nums)<<endl;
+    cout<<minSubArrayLen(7,nums)<<endl;
     return 0;
 }
