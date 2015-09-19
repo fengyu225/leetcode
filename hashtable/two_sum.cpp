edf4822e49bd05c2ecd7d6f248905bd01cc81fc5
Yu Feng <yfeng@boingo.com>
2015-09-19 13:13:59 -0700

    A  hashtable/header.h A  hashtable/lib.cpp A  hashtable/lib.h M  hashtable/two_sum.cpp

--- hashtable/two_sum.cpp
+++ hashtable/two_sum.cpp
@@ -1,29 +1,36 @@
+/*
+Given an array of integers, find two numbers such that they add up to a specific target number.
+
+The function twoSum should return indices of the two numbers such that they add up to the target, where index1 must be less than index2. Please note that your returned answers (both index1 and index2) are not zero-based.
+
+You may assume that each input would have exactly one solution.
+
+Input: numbers={2, 7, 11, 15}, target=9
+Output: index1=1, index2=2
+*/
+
 #include "header.h"
 
-vector<int> twoSum(vector<int> &numbers, int target){
-	vector<int> res;
-	if(numbers.size()<2) return res;
-	unordered_map<int,int> m;
-	for(int i=0; i<numbers.size(); i++){
-		if(m.find(target-numbers[i]) != m.end()){
-			res.push_back(m[target-numbers[i]]);
-			res.push_back(i+1);
-			return res;
-		}
-		m[numbers[i]]=i+1;
-	}
-	return res;
+vector<int> twoSum(vector<int>& nums, int target) {
+    vector<int> res;
+    unordered_map<int,int> m;
+    int sz = nums.size();
+    for(int i=0; i<sz; i++){
+        if(m.find(target-nums[i]) != m.end()){
+            res.push_back(m[target-nums[i]]);
+            res.push_back(i+1);
+            break;
+        }
+        m[nums[i]] = i+1;
+    }
+    return res;
 }
 
 int main(){
 //    int arr[] = {2, 7, 11, 15};
 //    int target = 9;
-    int arr[] = {3, 2, 4};
-    int target = 6;
-    vector<int> numbers(arr, arr+sizeof(arr)/sizeof(arr[0]));
-    vector<int> r = twoSum(numbers, target);
-    for(int i=0; i<r.size(); i++)
-        printf("%d ", r[i]);
-    printf("\n");
+    vector<int> numbers = {0, 4, 3, 0};
+    vector<int> r = twoSum(numbers, 0);
+    print_vector<int>(r);
     return 0;
 }
