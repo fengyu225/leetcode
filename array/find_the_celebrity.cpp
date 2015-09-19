ad9f87774889912cfd2ee1e8afb77f752db2390a
Yu Feng <yfeng@boingo.com>
2015-09-19 12:43:57 -0700

    M  array/find_the_celebrity.cpp

--- array/find_the_celebrity.cpp
+++ array/find_the_celebrity.cpp
@@ -22,7 +22,11 @@ bool knows(int a, int b){
 class Solution {
 public:
     int findCelebrity(int n) {
-        
+        if(n<2) return n?0:-1
+        int candidate = 0;
+        for(int i=1; i<n; i++) if(knows(candidate, i)) candidate = i;
+        for(int i=0; i<n; i++) if(i!=candidate && knows(candidate, i) || !knows(i,candidate)) return -1;
+        return candidate;
     }
 };
 
