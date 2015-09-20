5c9bdd889254bdde93a50a34e6f1e227acb36d49
Yu Feng <yfeng@boingo.com>
2015-09-20 14:54:24 -0700

    M  hashtable/longest_substring_with_at_most_two_distinct_characters.cpp

--- hashtable/longest_substring_with_at_most_two_distinct_characters.cpp
+++ hashtable/longest_substring_with_at_most_two_distinct_characters.cpp
@@ -10,6 +10,22 @@ T is "ece" which its length is 3.
 
 int lengthOfLongestSubstringTwoDistinct(string s) {
     int s_len = s.length();    
+    if(s_len < 2) return s_len;
+    int has_found[256] = {0};
+    int l = 0, r = 0, res=0, count = 0;
+    for(;r<s_len;r++){
+        has_found[s[r]]++;
+        if(has_found[s[r]] == 1) count++;
+        if(count <= 2) res = max(res, r-l+1);
+        if(count == 3){
+            while(l<r && count == 3){
+                has_found[s[l]]--;
+                if(has_found[s[l]] == 0) count--;
+                l++;
+            }
+        }
+    }
+    return res;
 }
 
 int main(){
