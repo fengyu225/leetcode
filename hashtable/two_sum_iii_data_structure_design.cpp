4a5d70de43e7fd2f1a3c50378e0693d40c8d1012
Yu Feng <yfeng@boingo.com>
2015-09-20 22:10:31 -0700

    M  hashtable/two_sum_iii_data_structure_design.cpp

--- hashtable/two_sum_iii_data_structure_design.cpp
+++ hashtable/two_sum_iii_data_structure_design.cpp
@@ -12,21 +12,54 @@ find(7) -> false
 
 #include "header.h"
 
+//class TwoSum {
+//    //TLE
+//public:
+//    void add(int number) {
+//        for(int i=0; i<v.size(); i++){
+//            int temp = v[i]+number;
+//            if(s.find(temp) == s.end()) s.insert(temp);
+//        }
+//        v.push_back(number);
+//    }
+//
+//    bool find(int value) {
+//        return s.find(value) != s.end();
+//    }
+//private:
+//    vector<int> v;
+//    unordered_set<int> s;
+//};
+
 class TwoSum {
+    unordered_map<int,int> m;
 public:
+    // Add the number to an internal data structure.
     void add(int number) {
-        m[number]++;    
+        m[number]++; 
     }
 
+    // Find if there exists any pair of numbers which sum is equal to the value.
+//    bool find(int value) {
+//        for(auto x:m){
+//            if(m.find(value-x) != m.end() && (value-x.first != x.first || x.second>1)) return true;
+//        }
+//        return false;
+//    }
     bool find(int value) {
+        // faster than above
         for(auto x:m){
-            if(m.find(value-x.first) != m.end() && (value-x.first != x.first || x.second>1)) return true;
-        } 
+            if(x.first<value-x.first && m.find(value-x.first) != m.end() || x.first == value-x.first && x.second >=2) return true;
+        }
         return false;
     }
-private:
-    unordered_map<int,int> m;
 };
+
+
+// Your TwoSum object will be instantiated and called as such:
+// TwoSum twoSum;
+// twoSum.add(number);
+// twoSum.find(value);
 
 int main(){
     TwoSum ts;
