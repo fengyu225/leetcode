f4d9f328a725338a17fea96bacd8b03bb2cb9caa
Yu Feng <yfeng@boingo.com>
2015-09-20 21:38:09 -0700

    M  hashtable/two_sum_iii_data_structure_design.cpp

--- hashtable/two_sum_iii_data_structure_design.cpp
+++ hashtable/two_sum_iii_data_structure_design.cpp
@@ -14,22 +14,19 @@ find(7) -> false
 
 class TwoSum {
 public:
-
-    // Add the number to an internal data structure.
     void add(int number) {
-        
+        m[number]++;    
     }
 
-    // Find if there exists any pair of numbers which sum is equal to the value.
     bool find(int value) {
-        
+        for(auto x:m){
+            if(m.find(value-x.first) != m.end() && (value-x.first != x.first || x.second>1)) return true;
+        } 
+        return false;
     }
+private:
+    unordered_map<int,int> m;
 };
-
-// Your TwoSum object will be instantiated and called as such:
-// TwoSum twoSum;
-// twoSum.add(number);
-// twoSum.find(value);
 
 int main(){
     TwoSum ts;
