c970b563e0edd16ce4d353c3aa4fd26bde5208fb
Yu Feng <yfeng@boingo.com>
2015-09-20 19:38:24 -0700

    M  hashtable/max_points_on_line.cpp

--- hashtable/max_points_on_line.cpp
+++ hashtable/max_points_on_line.cpp
@@ -11,27 +11,57 @@ struct Point {
     Point(int a, int b) : x(a), y(b) {}
 };
 
+int gcd(int a, int b){
+    if(a == b || b == 0) return a;
+    if(abs(a)<abs(b)) return gcd(b, a);
+    return gcd(b, a%b);
+}
+
+/* pair<int,int> as key */
 int maxPoints(vector<Point> &points) {
-    unordered_map<double,int> m;
-    int res = 0;
-    for(int i=0; i<points.size(); i++){
+    map<pair<int,int>,int> m;
+    int res = 0, sz = points.size();
+    for(int i=0; i<sz; i++){
         m.clear();
         int same = 1, temp = 0;
-        for(int j=i+1;j<points.size(); j++){
+        for(int j=i+1;j<sz; j++){
             if(points[i].x==points[j].x && points[i].y == points[j].y){
                 same++;
                 continue;
             }
-            double slp = points[i].x != points[j].x?
-                (double)((double)(points[i].y-points[j].y)/(double)(points[i].x-points[j].x)):
-                numeric_limits<double>::infinity();
-            m[slp]++;
+            int a = points[i].y-points[j].y, b =points[i].x-points[j].x; 
+            int g = gcd(a,b);
+            pair<int,int> k = make_pair(a/g, b/g);
+            m[k]++;
         }
         for(auto x:m) temp = max(temp, x.second);
         res = max(res,temp+same);
     }
     return res;
 }
+
+/* double as key */
+//int maxPoints(vector<Point> &points) {
+//    unordered_map<double,int> m;
+//    int res = 0;
+//    for(int i=0; i<points.size(); i++){
+//        m.clear();
+//        int same = 1, temp = 0;
+//        for(int j=i+1;j<points.size(); j++){
+//            if(points[i].x==points[j].x && points[i].y == points[j].y){
+//                same++;
+//                continue;
+//            }
+//            double slp = points[i].x != points[j].x?
+//                (double)((double)(points[i].y-points[j].y)/(double)(points[i].x-points[j].x)):
+//                numeric_limits<double>::infinity();
+//            m[slp]++;
+//        }
+//        for(auto x:m) temp = max(temp, x.second);
+//        res = max(res,temp+same);
+//    }
+//    return res;
+//}
 
 int main(){
     vector<Point> points = {{84,250},{0,0},{1,0},{0,-70},{0,-70},{1,-1},{21,10},{42,90},{-42,-230}}; 
