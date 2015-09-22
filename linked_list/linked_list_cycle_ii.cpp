977e26894a0be438033d7c71e0e86417cf5ec765
Yu Feng <yfeng@boingo.com>
2015-09-21 20:56:44 -0700

    M  linked_list/linked_list_cycle_ii.cpp

--- linked_list/linked_list_cycle_ii.cpp
+++ linked_list/linked_list_cycle_ii.cpp
@@ -9,13 +9,28 @@ Can you solve it without using extra space?
 
 ListNode *detectCycle(ListNode *head) {
     if(!head) return NULL;
+    ListNode* slow = head, *fast = head;
+    while(fast){
+        fast = fast->next;
+        if(!fast) return NULL;
+        fast = fast->next;
+        slow = slow->next;
+        if(slow == fast) break;
+    }
+    if(!fast) return NULL;
+    ListNode* x = head;
+    while(x != slow){
+        x = x->next;
+        slow = slow->next;
+    }
+    return x;
 }
 
 
 int main(){
    // int arr[] = {0, 1, 2, 3};
     int arr[] = {1,2};
-    ListNode* l = create_list(arr, sizeof(arr)/sizeof(arr[0]), -1);
+    ListNode* l = create_list(arr, sizeof(arr)/sizeof(arr[0]), 1);
     ListNode* r = detectCycle(l);
     if(r)
         printf("%d\n", r->val);
