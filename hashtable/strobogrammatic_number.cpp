4f2f5404e93189d6028d3704934034ab6704ced0
Yu Feng <yfeng@boingo.com>
2015-09-21 17:15:33 -0700

    A  hashtable/strobogrammatic_number.cpp

---
+++ hashtable/strobogrammatic_number.cpp
@@ -0,0 +1,22 @@
+/*
+A strobogrammatic number is a number that looks the same when rotated 180 degrees (looked at upside down).
+
+Write a function to determine if a number is strobogrammatic. The number is represented as a string.
+
+For example, the numbers "69", "88", and "818" are all strobogrammatic.
+*/
+
+#include "header.h"
+
+bool isStrobogrammatic(string num) {
+    int sz = num.length();
+}
+
+int main(){
+    cout<<isStrobogrammatic("60809")<<endl;
+    //cout<<isStrobogrammatic("101")<<endl;
+    //cout<<isStrobogrammatic("33")<<endl;
+    //cout<<isStrobogrammatic("69")<<endl;
+    //cout<<isStrobogrammatic("818")<<endl;
+    return 0;
+}
