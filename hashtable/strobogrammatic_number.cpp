2a17b515b75c5adbeb0e9af9bb276f598eaedd64
Yu Feng <yfeng@boingo.com>
2015-09-21 17:22:02 -0700

    M  hashtable/strobogrammatic_number.cpp

--- hashtable/strobogrammatic_number.cpp
+++ hashtable/strobogrammatic_number.cpp
@@ -10,6 +10,19 @@ For example, the numbers "69", "88", and "818" are all strobogrammatic.
 
 bool isStrobogrammatic(string num) {
     int sz = num.length();
+    if(sz == 0) return true;
+    if(sz == 1) return num[0] == '0' || num[0] == '1' || num[0] == '8';
+    unordered_map<char,char> m = {
+        {'0','0'},
+        {'1','1'},
+        {'6','9'},
+        {'9','6'},
+        {'8','8'}
+    };
+    for(int l = 0, r = sz-1; l<=r; l++, r--){
+        if(m.find(num[l]) == m.end() || m[num[l]] != num[r]) return false;
+    }
+    return true;
 }
 
 int main(){
