d90864dc6b1ab94370571542d025b1d6dd7e3aa3
Yu Feng <yfeng@boingo.com>
2015-09-22 21:22:40 -0700

    A  linked_list/remove_duplicates_from_sorted_list_ii.cpp

---
+++ linked_list/remove_duplicates_from_sorted_list_ii.cpp
@@ -0,0 +1,31 @@
+/*
+Given a sorted linked list, delete all nodes that have duplicate numbers, leaving only distinct numbers from the original list.
+
+For example,
+Given 1->2->3->3->4->4->5, return 1->2->5.
+Given 1->1->1->2->3, return 2->3.
+*/
+
+#include "header.h"
+
+ListNode* deleteDuplicates(ListNode* head) {
+    if(!head || head && !head->next) return head;
+    ListNode* res = NULL, *res_tail = NULL;
+    ListNode* pre = head, *curr = pre->next;
+    while(curr){
+        
+    }
+}
+
+int main(){
+//    int arr[] = {1, 1, 2, 2, 3, 3, 4, 4, 5};
+    int arr[] = {1, 2, 2};
+    ListNode* l = create_list(arr, sizeof(arr)/sizeof(arr[0]));
+    l = deleteDuplicates(l);
+    while(l){
+        printf("%d ", l->val);
+        l = l->next;
+    }
+    printf("\n");
+    return 0;
+}
