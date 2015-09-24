485c7a77d76fb40a04f1095dff3a16c3f59518c5
Yu Feng <yfeng@boingo.com>
2015-09-23 21:33:32 -0700

    M  tree/valid_bst.cpp

--- tree/valid_bst.cpp
+++ tree/valid_bst.cpp
@@ -10,20 +10,49 @@ Both the left and right subtrees must also be binary search trees.
 
 #include "header.h"
 
-bool isValidBST(TreeNode* root, long long l_bound, long long r_bound){
-    if(!root) return true;
-    int l_res = isValidBST(root->left, l_bound, root->val);
-    int r_res = isValidBST(root->right, root->val, r_bound);
-    return l_res && r_res && root->val<r_bound && root->val>l_bound;
-}
+//bool isValidBST(TreeNode* root, long long l_bound, long long r_bound){
+//    if(!root) return true;
+//    int l_res = isValidBST(root->left, l_bound, root->val);
+//    int r_res = isValidBST(root->right, root->val, r_bound);
+//    return l_res && r_res && root->val<r_bound && root->val>l_bound;
+//}
+//
+//bool isValidBST(TreeNode* root) {
+//    if(!root) return true;
+//    return isValidBST(root, (double)(INT_MIN)-1, (double)(INT_MAX)+1);
+//}
 
 bool isValidBST(TreeNode* root) {
+    //Morris travesal
     if(!root) return true;
-    return isValidBST(root, (double)(INT_MIN)-1, (double)(INT_MAX)+1);
-}
+    TreeNode* prev = NULL, *curr = root;
+    while(curr){
+        if(curr->left){
+            TreeNode* temp = curr->left;
+            while(temp->right && temp->right != curr) temp = temp->right;
+            if(temp->right == curr){
+                temp->right = NULL;
+                if(prev && prev->val>=curr->val) return false;
+                prev = curr;
+                curr = curr->right;
+            }
+            else{
+                temp->right = curr;
+                curr = curr->left;
+            }
+        }
+        else{
+            if(prev && prev->val>=curr->val) return false;
+            prev = curr;
+            curr = curr->right;
+        }
+    }
+    return true;
+}   
 
 int main(){
-    TreeNode* root = create_tree("21");
+    //TreeNode* root = create_tree("21");
+    TreeNode* root = create_tree("11");
     bool res = isValidBST(root);
     printf("%s\n", res?"true":"false");
     return 0;
