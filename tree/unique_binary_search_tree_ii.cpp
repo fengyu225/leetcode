dfbaa8b73c652c613b4e05ef9603d9559f80538c
Yu Feng <yfeng@boingo.com>
2015-09-23 20:42:05 -0700

    M  tree/unique_binary_search_tree_ii.cpp

--- tree/unique_binary_search_tree_ii.cpp
+++ tree/unique_binary_search_tree_ii.cpp
@@ -11,9 +11,32 @@ Given n = 3, your program should return all 5 unique BST's shown below.
 */
 #include "header.h"
 
+void generate(vector<TreeNode*>& res, int l, int r){
+    if(l >= r){
+        res.push_back(l == r?new TreeNode(l):NULL);
+        return;
+    }
+    for(int i=l; i<=r; i++){
+        vector<TreeNode*> l_res;
+        vector<TreeNode*> r_res;
+        generate(l_res, l, i-1);
+        generate(r_res, i+1, r);
+        for(TreeNode* l_c:l_res){
+            for(TreeNode* r_c:r_res){
+                TreeNode* root = new TreeNode(i);
+                root->left = l_c;
+                root->right = r_c;
+                res.push_back(root);
+            }
+        }
+    }
+}
+
 vector<TreeNode*> generateTrees(int n) {
+    if(n == 0) return vector<TreeNode*>(1, NULL);
     vector<TreeNode*> res;
-    if(n == 0) return res;
+    generate(res, 1, n);
+    return res;
 }
 
 int main(){
