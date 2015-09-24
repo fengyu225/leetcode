31f36f45dfab6c4caa20c88cf61fe72496009e91
Yu Feng <yfeng@boingo.com>
2015-09-23 19:17:54 -0700

    M  tree/binary_tree_postorder_traversal_iterative.cpp

--- tree/binary_tree_postorder_traversal_iterative.cpp
+++ tree/binary_tree_postorder_traversal_iterative.cpp
@@ -15,10 +15,44 @@ Note: Recursive solution is trivial, could you do it iteratively?
 
 #include "header.h"
 
+void push_nodes(TreeNode* from, TreeNode* to, vector<int>& res){
+    stack<TreeNode*> st;
+    TreeNode* temp = from;
+    while(temp != to){
+        st.push(temp);
+        temp = temp->right;
+    }
+    for(;!st.empty();res.push_back(st.top()->val), st.pop());
+}
+
 vector<int> postorderTraversal(TreeNode* root) {
     vector<int> res;
     if(!root) return res;
-
+    TreeNode* from = NULL, *to = NULL;
+    TreeNode* new_root = new TreeNode(0);
+    new_root->left = root;
+    TreeNode* curr = new_root;
+    while(curr){
+        if(curr->left){
+            TreeNode* temp = curr->left;
+            from = temp;
+            while(temp->right && temp->right != curr) temp = temp->right;
+            to = temp->right;
+            if(temp->right == curr){
+                push_nodes(from, to, res);
+                temp->right = NULL;
+                curr = curr->right;
+            }
+            else{
+                temp->right = curr;
+                curr = curr->left;
+            }
+        }
+        else{
+            curr = curr->right;
+        }
+    }
+    return res;
 }
 
 int main(){
