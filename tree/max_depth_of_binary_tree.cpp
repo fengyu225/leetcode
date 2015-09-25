72c6237b8a980fa7ff863f4e83444cc364cfb30c
Yu Feng <yfeng@boingo.com>
2015-09-24 19:39:12 -0700

    A  tree/max_depth_of_binary_tree.cpp

---
+++ tree/max_depth_of_binary_tree.cpp
@@ -0,0 +1,33 @@
+/*
+Given a binary tree, find its maximum depth.
+
+The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+*/
+
+#include "header.h"
+
+void search(TreeNode* root, int& res, int curr){
+    if(!root->left && !root->right){
+        res = max(res, curr+1);
+        return;
+    }
+    if(root->left) search(root->left, res, curr+1);
+    if(root->right) search(root->right, res, curr+1);
+}
+
+int maxDepth(TreeNode* root) {
+    if(!root) return 0;
+    int res = 0;
+    search(root, res, 0);
+    return res;
+}
+
+int main(){
+    TreeNode* root = create_tree("1#2");
+//    string arr[] = {"0","2","4","1","#","3","-1","5","1","#","6","#","8"};
+//    vector<string> v(arr,arr+sizeof(arr)/sizeof(arr[0]));
+//    root = create_tree(v);
+    int res = maxDepth(root);
+    printf("%d\n", res);
+    return 0;
+}
