c8a8c63a97dc93cc2b9aa732ecb158ac55d11a87
Yu Feng <yfeng@boingo.com>
2015-09-25 15:01:55 -0700

    A  tree/binary_tree_upside_down.cpp

---
+++ tree/binary_tree_upside_down.cpp
@@ -0,0 +1,33 @@
+/*
+Given a binary tree where all the right nodes are either leaf nodes with a sibling (a left node that shares the same parent node) or empty, flip it upside down and turn it into a tree where the original right nodes turned into left leaf nodes. Return the new root.
+For example:
+Given a binary tree {1,2,3,4,5},
+    1
+   / \
+  2   3
+ / \
+4   5
+return the root of the binary tree [4,5,2,#,#,3,1].
+   4
+  / \
+ 5   2
+    / \
+   3   1  
+
+*/
+
+#include "header.h"
+
+TreeNode* upsideDownBinaryTree(TreeNode* root) {
+
+}
+
+int main(){
+    TreeNode* root = create_tree("12#45");
+    TreeNode* new_root = upsideDownBinaryTree(root);
+    pre_order(new_root);
+    cout<<endl;
+    in_order(new_root);
+    cout<<endl;
+    return 0;
+}
