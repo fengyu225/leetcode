229eaffd57cbfa01befd091c420e61632c090fc6
Yu Feng <yfeng@boingo.com>
2015-09-27 15:42:42 -0700

    M  dp/ugly_number.cpp

--- dp/ugly_number.cpp
+++ dp/ugly_number.cpp
@@ -8,28 +8,14 @@ Note that 1 is typically treated as an ugly number.
 
 #include "header.h"
 
-//bool isUgly(int num) {
-//    //slow ...
-//    long n = num>=0?(long)num:(long)-1*(long)num;
-//    vector<bool> checks(n+1, true);
-//    for(long i=2; i<=sqrt(n); i++){
-//        for(long x=2; x*i<=sqrt(n); x++)
-//            checks[x*i] = false;
-//    }
-//    for(long i=6; i<=sqrt(n); i++){
-//        if(checks[i] && num%i == 0) return false;
-//    }
-//    return true;
-//}
-
 bool isUgly(int num) {
-    if(num<0) return false;
-    long n = num>=0?(long)num:(long)-1*(long)num;
-    if(n == 1) return true;
-    while(n>=2 && n%2 == 0) n=n/2;
-    while(n>= 5 && n%5 == 0) n=n/5;
-    while(n>=3 && n%3 == 0) n=n/3;
-    return n == 1;
+    if(num<=0) return false;
+    if(num == 1) return true;
+    num = (long)abs(num);
+    for(;num >=2 && num%2 == 0; num /=2);
+    for(;num >=3 && num%3 == 0; num /=3);
+    for(;num >=5 && num%5 == 0; num /=5);
+    return num == 1;
 }
 
 int main(){
