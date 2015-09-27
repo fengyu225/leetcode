3fe7dc39ebff03fee994ba8490239490e969f92c
Yu Feng <yfeng@boingo.com>
2015-09-26 21:34:21 -0700

    M  tree/count_univalue_subtrees.cpp ?? 5/walls_and_gates.cpp_ ?? tree/a..dSYM/

--- tree/count_univalue_subtrees.cpp
+++ tree/count_univalue_subtrees.cpp
@@ -15,6 +15,25 @@ return 4.
 
 #include "header.h"
 
+bool countUnivalSubtrees(TreeNode* root, int& count){
+    if(!root) return true;
+    bool l_res = countUnivalSubtrees(root->left, count);
+    bool r_res = countUnivalSubtrees(root->right, count);
+    int l_v = root->left?root->left->val:root->val;
+    int r_v = root->right?root->right->val:root->val;
+    if(l_res && r_res && l_v == root->val && r_v == root->val){
+        count++;
+        return true;
+    }
+    else return false;
+}
+
+int countUnivalSubtrees(TreeNode* root) {
+    if(!root) return 0;
+    int count = 0;
+    countUnivalSubtrees(root, count);
+    return count;
+}
 
 int main(){
     TreeNode* root = create_tree("51555#5");
