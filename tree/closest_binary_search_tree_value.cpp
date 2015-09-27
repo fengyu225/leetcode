601dfc18e6ab0cfccf17bc6510561f32745a3f0c
Yu Feng <yfeng@boingo.com>
2015-09-26 23:47:16 -0700

    M  tree/closest_binary_search_tree_value.cpp ?? 5/walls_and_gates.cpp_ ?? tree/a..dSYM/

--- tree/closest_binary_search_tree_value.cpp
+++ tree/closest_binary_search_tree_value.cpp
@@ -8,21 +8,8 @@ You are guaranteed to have only one unique value in the BST that is closest to t
 
 #include "header.h"
 
-void search(TreeNode* root, double target, TreeNode*& curr, double& curr_diff){
-    double d = abs(target-(double)root->val);
-    if(d<curr_diff){
-        curr = root;
-        curr_diff = d;
-    }
-    if(root->left && target<root->val) search(root->left, target, curr, curr_diff);
-    if(root->right && target>root->val) search(root->right, target, curr, curr_diff);
-}
-
 int closestValue(TreeNode* root, double target) {
-    TreeNode* curr = root;
-    double curr_diff = abs(target-(double)root->val);
-    search(root, target, curr, curr_diff);
-    return curr->val;
+
 }
 
 int main(){
