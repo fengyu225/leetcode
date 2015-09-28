0b1f7ac298911ee6d6a2a1475bde87fc40ea6462
Yu Feng <yfeng@boingo.com>
2015-09-27 21:11:44 -0700

    M  5/find_the_duplicate_number.cpp

--- 5/find_the_duplicate_number.cpp
+++ 5/find_the_duplicate_number.cpp
@@ -9,17 +9,42 @@ Your runtime complexity should be less than O(n2).
 
 #include "header.h"
 
-int findDuplicate(vector<int>& nums) {
-    sort(nums.begin(), nums.end());
-    int sz = nums.size();
-    for(int i=1; i<sz; i++){
-        if(nums[i] == nums[i-1]) return nums[i];
+//int findDuplicate(vector<int>& nums) {
+//    sort(nums.begin(), nums.end());
+//    int sz = nums.size();
+//    for(int i=1; i<sz; i++){
+//        if(nums[i] == nums[i-1]) return nums[i];
+//    }
+//}
+
+int search(vector<int>& nums, int l, int r){
+    if(l == r) return nums[l];
+    int tail = l, curr = l+1;
+    int min_v = nums[l], max_v = nums[l];
+    while(curr<=r){
+        if(nums[curr]<min_v) min_v = nums[curr];
+        if(nums[curr]>max_v) max_v = nums[curr];
+        if(nums[curr] == nums[l] || curr != r && nums[curr] == nums[r]) return nums[curr];
+        if(nums[curr]<nums[l]) swap(nums[++tail], nums[curr]);
+        curr++;
     }
+    swap(nums[tail], nums[l]);
+    if(tail == l) return search(nums, l+1, r);
+    if(tail == r) return search(nums, l, r-1);
+    if(nums[tail]-min_v<tail-l) return search(nums, l, tail);
+    else return search(nums, tail, r);
+}
+
+int findDuplicate(vector<int>& nums) {
+    int sz = nums.size();
+    int l = 0, r = sz-1;
+    return search(nums, l, r);
 }
 
 int main(){
 //    vector<int> nums = {1, 2, 3, 4, 5, 3};
-    vector<int> nums = {[2,2,2,2,2]};
+//    vector<int> nums = {2,2,2,2,2};
+    vector<int> nums = {14,16,12,1,16,17,6,8,5,19,16,13,16,3,11,16,4,16,9,7};
     cout<<findDuplicate(nums)<<endl;
     return 0;
 }
