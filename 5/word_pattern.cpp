681ee47bd796771d7e93b93494d826dcd8f7d186
Yu Feng <yfeng@boingo.com>
2015-10-05 13:21:53 -0700

    M  5/word_pattern.cpp

--- 5/word_pattern.cpp
+++ 5/word_pattern.cpp
@@ -16,7 +16,26 @@ Each letter in pattern must map to a word with length that is at least 1.
 #include "header.h"
 
 bool wordPattern(string pattern, string str) {
-    return false;
+    int p_sz = pattern.size(), s_sz = str.size();
+    if(p_sz == 0) return s_sz == 0;
+    stringstream ss(str);
+    string curr;
+    int i = 0;
+    unordered_map<char,string> from;
+    unordered_map<string,char> to;
+    while(getline(ss, curr, ' ')){
+//        cout<<curr<<endl;
+        if(from.find(pattern[i]) == from.end() && to.find(curr) == to.end()){
+            from[pattern[i]] = curr;
+            to[curr] = pattern[i];
+        }
+        else if(from.find(pattern[i]) != from.end() && to.find(curr) != to.end()){
+            if(from[pattern[i]] != curr || to[curr] != pattern[i]) return false;
+        }
+        else return false;
+        i++;
+    }
+    return i == p_sz;
 }
 
 int main(){
