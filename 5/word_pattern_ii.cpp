bbed2bf30306281f76201dcad12116bf89a59d85
Yu Feng <yfeng@boingo.com>
2015-10-14 18:43:24 -0700

    252 ms

--- 5/word_pattern_ii.cpp
+++ 5/word_pattern_ii.cpp
@@ -21,7 +21,7 @@ bool check(string& str, int i, int a, int b){
     return true;
 }
 
-bool search(string& pattern, string& str, int p_sz, int s_sz, int p_curr, int s_curr, unordered_map<char,pair<int,int> >& m, unordered_map<string, char>& n){
+bool search(string& pattern, string& str, int p_sz, int s_sz, int p_curr, int s_curr, unordered_map<char,pair<int,int> >& m, unordered_map<string, char>& n, char& cause){
     //cout<<p_curr<<" "<<s_curr<<endl;
     if(s_curr == s_sz){
         if(p_curr == p_sz) return true;
@@ -32,21 +32,30 @@ bool search(string& pattern, string& str, int p_sz, int s_sz, int p_curr, int s_
         pair<int,int> temp = m[pattern[p_curr]];
         if(check(str, s_curr, temp.first, temp.second)){
             //cout<<"checking return true"<<endl;
-            bool res = search(pattern, str, p_sz, s_sz, p_curr+1, s_curr+(temp.second-temp.first)+1, m, n);
+            bool res = search(pattern, str, p_sz, s_sz, p_curr+1, s_curr+(temp.second-temp.first)+1, m, n, cause);
             if(res) return true;
         }
-        else return false;
+        else{
+            cause = pattern[p_curr];
+            return false;
+        };
     }
     else{
         for(int i=s_curr; i<s_sz; i++){
             string temp = str.substr(s_curr, i-s_curr+1);
+            //cout<<pattern[p_curr]<<" "<<temp<<endl;
             if(n.find(temp) != n.end() && n[temp] != pattern[p_curr]) continue;
             m[pattern[p_curr]] = make_pair(s_curr, i);
             n[temp] = pattern[p_curr];
-            bool res = search(pattern, str, p_sz, s_sz, p_curr+1, i+1, m, n);
+            char cause = ' ';
+            bool res = search(pattern, str, p_sz, s_sz, p_curr+1, i+1, m, n, cause);
             if(res) return true;
             m.erase(m.find(pattern[p_curr]));
             n.erase(n.find(temp));
+            if(cause == ' ' || cause == pattern[p_curr]) continue;
+            else{
+                break;
+            }
         } 
     }
     return false;
@@ -56,13 +65,14 @@ bool wordPatternMatch(string pattern, string str) {
     int p_sz = pattern.length(), s_sz = str.length();
     unordered_map<char,pair<int,int> > m;
     unordered_map<string,char> n;
-    return search(pattern, str, p_sz, s_sz, 0, 0, m, n);
+    char cause = ' ';
+    return search(pattern, str, p_sz, s_sz, 0, 0, m, n, cause);
 }
 
 int main(){
-    cout<<wordPatternMatch("ab", "aa")<<endl;
-//    cout<<wordPatternMatch("abab", "redblueredblue")<<endl;
-//    cout<<wordPatternMatch("aaaa", "asdasdasdasd")<<endl;
-//    cout<<wordPatternMatch("aabb", "xyzabcxzyabc")<<endl;
+    cout<<wordPatternMatch("abba", "dogcatcatdog")<<endl;
+    cout<<wordPatternMatch("abab", "redblueredblue")<<endl;
+    cout<<wordPatternMatch("aaaa", "asdasdasdasd")<<endl;
+    cout<<wordPatternMatch("aabb", "xyzabcxzyabc")<<endl;
     return 0;
 }
