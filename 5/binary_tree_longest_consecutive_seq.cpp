511eb8577581a9fdd8d7959f4f27d9f1d4d9a6a1
Yu Feng <yfeng@boingo.com>
2015-10-29 18:16:23 -0700

    A  5/binary_tree_longest_consecutive_seq.cpp  D 5/test.cpp

---
+++ 5/binary_tree_longest_consecutive_seq.cpp
@@ -0,0 +1,35 @@
+/*
+Given a binary tree, find the length of the longest consecutive sequence path.
+
+The path refers to any sequence of nodes from some starting node to any node in the tree along the parent-child connections. The longest consecutive path need to be from parent to child (cannot be the reverse).
+
+For example,
+   1
+    \
+     3
+    / \
+   2   4
+        \
+         5
+Longest consecutive sequence path is 3-4-5, so return 3.
+   2
+    \
+     3
+    / 
+   2    
+  / 
+ 1
+Longest consecutive sequence path is 2-3,not3-2-1, so return 2.
+*/
+
+#include "header.h"
+
+int longestConsecutive(TreeNode* root) {
+    return 0;
+}
+
+int main(){
+    TreeNode* root = create_tree("1#324###5");
+    cout<<longestConsecutive(root)<<endl;
+    return 0;
+}
