b041eee47440d0b86636274c76e9c9e02e303c11
Yu Feng <yfeng@boingo.com>
2015-11-29 17:21:34 -0800

    M  5/binary_tree_longest_consecutive_seq.cpp  D 5/test.cpp

--- 5/binary_tree_longest_consecutive_seq.cpp
+++ 5/binary_tree_longest_consecutive_seq.cpp
@@ -24,12 +24,27 @@ Longest consecutive sequence path is 2-3,not3-2-1, so return 2.
 
 #include "header.h"
 
+int find_longest_consecutive(TreeNode* root, int& res){
+    if(!root) return 0;
+    int l = find_longest_consecutive(root->left, res);
+    int r = find_longest_consecutive(root->right, res);
+    l = root->left && root->val+1 == root->left->val?l:0;
+    r = root->right && root->val+1 == root->right->val?r:0;
+    int temp = max(l, r)+1;
+    res = max(res, temp);
+    return temp;
+}
+
 int longestConsecutive(TreeNode* root) {
-    return 0;
+    if(!root) return 0;
+    int res = 0;
+    find_longest_consecutive(root, res);
+    return res;
 }
 
 int main(){
-    TreeNode* root = create_tree("1#324###5");
+    //TreeNode* root = create_tree("1#324###5");
+    TreeNode* root = create_tree("2#32#1");
     cout<<longestConsecutive(root)<<endl;
     return 0;
 }
