b8d7f1300d9a8cd5a67f3fa1f7ece5af58f2003f
Yu Feng <yfeng@boingo.com>
2015-12-11 18:39:24 -0800

    M  5/remove_invalid_parentheses.cpp

--- 5/remove_invalid_parentheses.cpp
+++ 5/remove_invalid_parentheses.cpp
@@ -13,7 +13,7 @@ Examples:
 #include "header.h"
 
 //void search(string& s, unordered_set<string>& res_set, string& temp, int curr, int open, int s_sz, int& max_valid_len){
-//    //dfs
+//    //naive dfs
 //    if(s_sz-curr<open) return;
 //    if(curr == s_sz){
 //        if(temp.length()>=max_valid_len && res_set.find(temp) == res_set.end()){
@@ -44,35 +44,68 @@ Examples:
 //    }
 //}
 //
-//vector<string> removeInvalidParentheses(string s) {
-//    vector<string> res;
-//    int sz = s.length();
-//    unordered_set<string> res_set;
-//    string temp = "";
-//    int max_valid_len = 0;
-//    search(s, res_set, temp, 0, 0, sz, max_valid_len);
-//    for(string a:res_set) res.push_back(a);
-//    return res;
-//}
 
-bool is_valid(string& s){
-    int open = 0;
-    for(int i=0; i<s.length(); i++){
-        if(open<0) return false;
-        if(s[i] == '(') open++;
-        else if(s[i] == ')') open--;
+void search(string& s, unordered_set<string>& res_set, string& temp, int curr, int open, int s_sz, int& max_valid_len){
+    // dfs
+    if(s_sz-curr<open) return;
+    if(curr == s_sz){
+        if(temp.length()>=max_valid_len && res_set.find(temp) == res_set.end()){
+            max_valid_len = temp.length();
+            res_set.insert(temp);
+        }
+        return;
+    }    
+    if(s[curr] != '(' && s[curr] != ')'){
+        temp += s[curr];
+        search(s, res_set, temp, curr+1, open, s_sz, max_valid_len);
+        temp.pop_back();
+        return;
     }
-    return open == 0;
+    if(s[curr] == '('){
+        temp += "(";
+        search(s, res_set, temp, curr+1, open+1, s_sz, max_valid_len);
+        temp.pop_back();
+        search(s, res_set, temp, curr+1, open, s_sz, max_valid_len);
+    }
+    else{
+        if(!(s[curr] == ')' && open==0)){
+            temp += ')';
+            search(s, res_set, temp, curr+1, open-1, s_sz, max_valid_len);
+            temp.pop_back();
+        }
+        search(s, res_set, temp, curr+1, open, s_sz, max_valid_len);
+    }
 }
 
-class Node{
-    public:
-        Node(string s, int last_index):s(s),last_index(last_index),met_left(false){}
-        Node(string s, int last_index, bool met_left):s(s),last_index(last_index),met_left(met_left){}
-        string s;
-        int last_index;
-        bool met_left;
-};
+vector<string> removeInvalidParentheses(string s) {
+    vector<string> res;
+    int sz = s.length();
+    unordered_set<string> res_set;
+    string temp = "";
+    int max_valid_len = 0;
+    search(s, res_set, temp, 0, 0, sz, max_valid_len);
+    for(string a:res_set) res.push_back(a);
+    return res;
+}
+
+//bool is_valid(string& s){
+//    int open = 0;
+//    for(int i=0; i<s.length(); i++){
+//        if(open<0) return false;
+//        if(s[i] == '(') open++;
+//        else if(s[i] == ')') open--;
+//    }
+//    return open == 0;
+//}
+//
+//class Node{
+//    public:
+//        Node(string s, int last_index):s(s),last_index(last_index),met_left(false){}
+//        Node(string s, int last_index, bool met_left):s(s),last_index(last_index),met_left(met_left){}
+//        string s;
+//        int last_index;
+//        bool met_left;
+//};
 
 /*
 The naive BFS solution is quite simple to implement. To speed up we can use a Set to record all the strings generated and avoid revisit. But a better and faster solution is to avoid generate duplicate strings all together.
@@ -98,37 +131,38 @@ By applying these restrictions, we can avoid generate duplicate strings and the 
 Ultimately we can further improve the algorithm to eliminate isValid calls. To do this we need to remove and only remove those characters that would lead us to valid strings. This needs some preprocess and can reduce the time to around 3ms.
 */
 
-vector<string> removeInvalidParentheses(string s) {
-    vector<string> res;
-    queue<Node*> q_curr, q_next;
-    queue<Node*> qs[2] = {q_curr, q_next};
-    int curr = 0;
-    bool found = false;
-    qs[curr%2].push(new Node(s, 0));
-    unordered_set<string> visited;
-    while(!qs[curr%2].empty()){
-        Node* temp = qs[curr%2].front();
-        qs[curr%2].pop();
-        if(is_valid(temp->s)){
-            res.push_back(temp->s);
-            found = true;
-        }
-        if(found) continue;
-        int temp_sz = temp->s.length();
-        for(int i=temp->last_index; i<temp_sz; i++){
-            if(temp->s[i] != '(' && temp->s[i] != ')') continue;
-            if(temp->met_left && temp->s[i] == ')') continue;
-            if(!(i>temp->last_index && temp->s[i] == temp->s[i-1])){
-                string temp_n = temp->s.substr(0, i) + temp->s.substr(i+1, temp_sz-i-1);
-                qs[(curr+1)%2].push(new Node(temp_n, i, temp->s[i] == '('));
-            }
-        }
-        if(qs[curr%2].empty()) curr++;
-    }
-    return res;
-}
+//vector<string> removeInvalidParentheses(string s) {
+//    vector<string> res;
+//    queue<Node*> q_curr, q_next;
+//    queue<Node*> qs[2] = {q_curr, q_next};
+//    int curr = 0;
+//    bool found = false;
+//    qs[curr%2].push(new Node(s, 0));
+//    unordered_set<string> visited;
+//    while(!qs[curr%2].empty()){
+//        Node* temp = qs[curr%2].front();
+//        qs[curr%2].pop();
+//        if(is_valid(temp->s)){
+//            res.push_back(temp->s);
+//            found = true;
+//        }
+//        if(found) continue;
+//        int temp_sz = temp->s.length();
+//        for(int i=temp->last_index; i<temp_sz; i++){
+//            if(temp->s[i] != '(' && temp->s[i] != ')') continue;
+//            if(temp->met_left && temp->s[i] == ')') continue;
+//            if(!(i>temp->last_index && temp->s[i] == temp->s[i-1])){
+//                string temp_n = temp->s.substr(0, i) + temp->s.substr(i+1, temp_sz-i-1);
+//                qs[(curr+1)%2].push(new Node(temp_n, i, temp->s[i] == '('));
+//            }
+//        }
+//        if(qs[curr%2].empty()) curr++;
+//    }
+//    return res;
+//}
 
 //vector<string> removeInvalidParentheses(string s) {
+//    //naive bfs
 //    vector<string> res;
 //    queue<string> q;
 //    unordered_set<string> visited;
