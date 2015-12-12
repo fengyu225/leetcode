d6e3965aab91b51297906daa0f2f5e6e311c1ad8
Yu Feng <yfeng@boingo.com>
2015-12-11 17:40:35 -0800

    M  5/remove_invalid_parentheses.cpp

--- 5/remove_invalid_parentheses.cpp
+++ 5/remove_invalid_parentheses.cpp
@@ -55,18 +55,6 @@ Examples:
 //    return res;
 //}
 
-long get_time(){
-    namespace sc = std::chrono;
-    auto time = sc::system_clock::now(); // get the current time
-    auto since_epoch = time.time_since_epoch(); // get the duration since epoch
-    // I don't know what system_clock returns
-    // I think it's uint64_t nanoseconds since epoch
-    // Either way this duration_cast will do the right thing
-    auto millis = sc::duration_cast<sc::milliseconds>(since_epoch);
-    long now = millis.count(); // just like java (new Date()).getTime();
-    return now;
-}
-
 bool is_valid(string& s){
     int open = 0;
     for(int i=0; i<s.length(); i++){
@@ -77,40 +65,100 @@ bool is_valid(string& s){
     return open == 0;
 }
 
+class Node{
+    public:
+        Node(string s, int last_index):s(s),last_index(last_index){}
+        string s;
+        int last_index;
+};
+
+/*
+The naive BFS solution is quite simple to implement. To speed up we can use a Set to record all the strings generated and avoid revisit. But a better and faster solution is to avoid generate duplicate strings all together.
+
+The first observation is when we want to remove a ')' or '(' from several consecutive ones we only remove the first one, because remove any one the result will be the same. For example
+
+"())" ---> "()"
+only remove the first one of '))'
+The second observation is when we remove a character it must behind it's parent removal position. For example
+
+we need remove 2 from "(())(("
+we want to remove positions combination i,j with no duplicate
+so we let i < j then it will not generate duplicate combinations
+in practice, we record the position i and put it in to queue
+which is then polled out and used as the starting point of the next removal
+A third observation is if the previous step we removed a "(", we should never remove a ")" in the following steps. This is obvious since otherwise we could just save these two removals and still be valid with less removals. With this observation all the possible removals will be something like this
+
+")))))))))((((((((("
+All the removed characters forming a string with consecutive left bracket followed by consecutive right bracket.
+
+By applying these restrictions, we can avoid generate duplicate strings and the need of a set which saves a lot of space.
+
+Ultimately we can further improve the algorithm to eliminate isValid calls. To do this we need to remove and only remove those characters that would lead us to valid strings. This needs some preprocess and can reduce the time to around 3ms.
+*/
+
 vector<string> removeInvalidParentheses(string s) {
     vector<string> res;
-    queue<string> q;
-    unordered_set<string> visited;
-    q.push(s);
+    queue<Node*> q_curr, q_next;
+    queue<Node*> qs[2] = {q_curr, q_next};
+    int curr = 0;
     bool found = false;
-    while(!q.empty()){
-        string temp = q.front();
-        q.pop();
-        if(is_valid(temp)){
-            res.push_back(temp);
+    qs[curr%2].push(new Node(s, 0));
+    unordered_set<string> visited;
+    while(!qs[curr%2].empty()){
+        Node* temp = qs[curr%2].front();
+        qs[curr%2].pop();
+        if(is_valid(temp->s)){
+            res.push_back(temp->s);
             found = true;
         }
         if(found) continue;
-        int open = 0;
-        for(int i=0; i<temp.length(); i++){
-            if(temp[i] != '(' && temp[i] != ')') continue;
-            if(i>0 && temp[i]==temp[i-1]){
-                if(temp[i] == '(') open++;
-                else open--;
-                continue;
+        int temp_sz = temp->s.length();
+        for(int i=temp->last_index; i<temp_sz; i++){
+            if(temp->s[i] != '(' && temp->s[i] != ')') continue;
+            if(!(i>temp->last_index && temp->s[i] == temp->s[i-1])){
+                string temp_n = temp->s.substr(0, i) + temp->s.substr(i+1, temp_sz-i-1);
+                qs[(curr+1)%2].push(new Node(temp_n, i));
             }
-            string temp_n = temp.substr(0, i) + temp.substr(i+1, temp.length()-i-1);
-            if(visited.find(temp_n) == visited.end()){
-                q.push(temp_n);
-                visited.insert(temp_n);
-                if(open ==0 && temp[i]==')') break;
-            }
-            if(temp[i] == '(') open++;
-            else open--;
         }
+        if(qs[curr%2].empty()) curr++;
     }
     return res;
 }
+
+//vector<string> removeInvalidParentheses(string s) {
+//    vector<string> res;
+//    queue<string> q;
+//    unordered_set<string> visited;
+//    q.push(s);
+//    bool found = false;
+//    while(!q.empty()){
+//        string temp = q.front();
+//        q.pop();
+//        if(is_valid(temp)){
+//            res.push_back(temp);
+//            found = true;
+//        }
+//        if(found) continue;
+//        int open = 0;
+//        for(int i=0; i<temp.length(); i++){
+//            if(temp[i] != '(' && temp[i] != ')') continue;
+//            if(i>0 && temp[i]==temp[i-1]){
+//                if(temp[i] == '(') open++;
+//                else open--;
+//                continue;
+//            }
+//            string temp_n = temp.substr(0, i) + temp.substr(i+1, temp.length()-i-1);
+//            if(visited.find(temp_n) == visited.end()){
+//                q.push(temp_n);
+//                visited.insert(temp_n);
+//                if(open ==0 && temp[i]==')') break;
+//            }
+//            if(temp[i] == '(') open++;
+//            else open--;
+//        }
+//    }
+//    return res;
+//}
 
 int main(){
 //    vector<string> res = removeInvalidParentheses("()())()");
