778b2007d9ef4bff42e087056893b4143f8a9a9c
Yu Feng <yfeng@boingo.com>
2015-12-13 11:10:24 -0800

    M  5/remove_invalid_parentheses.cpp

--- 5/remove_invalid_parentheses.cpp
+++ 5/remove_invalid_parentheses.cpp
@@ -45,7 +45,8 @@ Examples:
 //}
 //
 
-void search(string& s, unordered_set<string>& res_set, string& temp, int curr, int open, int s_sz, int& max_valid_len){
+void search(string& s, unordered_set<string>& res_set, string& temp, int curr, int open, int s_sz, int& max_valid_len, bool left_deleted){
+    //(()()))q(l)()o)(z)
     // dfs
     if(s_sz-curr<open) return;
     if(curr == s_sz){
@@ -57,23 +58,26 @@ void search(string& s, unordered_set<string>& res_set, string& temp, int curr, i
     }    
     if(s[curr] != '(' && s[curr] != ')'){
         temp += s[curr];
-        search(s, res_set, temp, curr+1, open, s_sz, max_valid_len);
+        search(s, res_set, temp, curr+1, open, s_sz, max_valid_len, left_deleted);
         temp.pop_back();
         return;
     }
     if(s[curr] == '('){
         temp += "(";
-        search(s, res_set, temp, curr+1, open+1, s_sz, max_valid_len);
+        search(s, res_set, temp, curr+1, open+1, s_sz, max_valid_len, left_deleted);
         temp.pop_back();
-        search(s, res_set, temp, curr+1, open, s_sz, max_valid_len);
+        search(s, res_set, temp, curr+1, open, s_sz, max_valid_len, true);
     }
     else{
         if(!(s[curr] == ')' && open==0)){
             temp += ')';
-            search(s, res_set, temp, curr+1, open-1, s_sz, max_valid_len);
+            search(s, res_set, temp, curr+1, open-1, s_sz, max_valid_len, left_deleted);
             temp.pop_back();
         }
-        search(s, res_set, temp, curr+1, open, s_sz, max_valid_len);
+        if(!left_deleted){
+            search(s, res_set, temp, curr+1, open, s_sz, max_valid_len, left_deleted);
+        }
+        //cout<<temp<<" "<<left_deleted<<endl;
     }
 }
 
@@ -83,7 +87,7 @@ vector<string> removeInvalidParentheses(string s) {
     unordered_set<string> res_set;
     string temp = "";
     int max_valid_len = 0;
-    search(s, res_set, temp, 0, 0, sz, max_valid_len);
+    search(s, res_set, temp, 0, 0, sz, max_valid_len, false);
     for(string a:res_set) res.push_back(a);
     return res;
 }
