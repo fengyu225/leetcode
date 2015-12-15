19e0b0b2ff4f7dc750984a56829b897c688e2d5a
Yu Feng <yfeng@boingo.com>
2015-12-14 16:47:38 -0800

    M  5/range_sum_query_immutable.cpp ?? 5/x

--- 5/range_sum_query_immutable.cpp
+++ 5/range_sum_query_immutable.cpp
@@ -18,17 +18,49 @@ Show Similar Problems
 
 #include "header.h"
 
+/* naive approach 
 class NumArray {
+    vector<vector<int> >* sums;
+    int arr_sz = 0;
 public:
     NumArray(vector<int> &nums) {
-        
+        arr_sz = nums.size();
+        sums = new vector<vector<int> >(arr_sz, vector<int>(arr_sz, 0));
+        if(arr_sz>0){
+            for(int i=0;i<arr_sz; i++){
+                (*sums)[i][i] = nums[i];
+                for(int j=i+1; j<arr_sz; j++) (*sums)[i][j] = (*sums)[i][j-1]+nums[j];
+            }
+        }
     }
 
     int sumRange(int i, int j) {
-        
+        if(arr_sz == 0) return 0;        
+        return (*sums)[i][j];
     }
 };
+*/
 
+class NumArray {
+    vector<vector<int> >* sums;
+    int arr_sz = 0;
+public:
+    NumArray(vector<int> &nums) {
+        arr_sz = nums.size();
+        sums = new vector<vector<int> >(arr_sz, vector<int>(arr_sz, 0));
+        if(arr_sz>0){
+            for(int i=0;i<arr_sz; i++){
+                (*sums)[i][i] = nums[i];
+                for(int j=i+1; j<arr_sz; j++) (*sums)[i][j] = (*sums)[i][j-1]+nums[j];
+            }
+        }
+    }
+
+    int sumRange(int i, int j) {
+        if(arr_sz == 0) return 0;        
+        return (*sums)[i][j];
+    }
+};
 
 // Your NumArray object will be instantiated and called as such:
 // NumArray numArray(nums);
@@ -36,9 +68,12 @@ public:
 // numArray.sumRange(1, 2);
 
 int main(){
-    vector<int> nums = {-2, 0, 3, -5, 2, -1};
-    NumArray NumArray(nums);
-    cout<<NumArray.sumRange(0, 1)<<endl;
-    cout<<NumArray.sumRange(1, 2)<<endl;
+    //vector<int> num = {-2, 0, 3, -5, 2, -1};
+    vector<int> num = {-2};
+    NumArray NumArray(num);
+    cout<<NumArray.sumRange(0, 0)<<endl;
+    //cout<<NumArray.sumRange(0, 2)<<endl;
+    //cout<<NumArray.sumRange(2, 5)<<endl;
+    //cout<<NumArray.sumRange(0, 5)<<endl;
     return 0;
 }
