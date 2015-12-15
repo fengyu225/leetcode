aef5295cd3ed8d4f2acac9d4d2dfe5c530cacf3d
Yu Feng <yfeng@boingo.com>
2015-12-14 19:34:43 -0800

    M  5/range_sum_query_2d_immutable.cpp ?? 5/x

--- 5/range_sum_query_2d_immutable.cpp
+++ 5/range_sum_query_2d_immutable.cpp
@@ -22,13 +22,34 @@ You may assume that row1 ≤ row2 and col1 ≤ col2.
 #include "header.h"
 
 class NumMatrix {
+    vector<vector<pair<int,int> > > one_d_sum;
+    vector<vector<int> > two_d_sum;
+    vector<vector<int> >* m;
 public:
     NumMatrix(vector<vector<int> > &matrix) {
-        
+        m = &matrix;
+        if(!(matrix.size() == 0 || matrix[0].size() == 0)){
+            int row = matrix.size(), col = matrix[0].size();
+            one_d_sum.resize(row);
+            two_d_sum.resize(row);
+            for(int i=0; i<row; i++){
+                for(int j=0; j<col; j++){
+                    one_d_sum[i].push_back(make_pair(0, 0));
+                    one_d_sum[i][j].first = (j==0?matrix[i][0]:(one_d_sum[i][j-1].first+matrix[i][j]));
+                    one_d_sum[i][j].second = i==0?matrix[i][j]:(one_d_sum[i-1][j].second+matrix[i][j]);
+                    if(i == 0) two_d_sum[i].push_back(one_d_sum[i][j].first);
+                    else if(j == 0) two_d_sum[i].push_back(one_d_sum[i][j].second);
+                    else two_d_sum[i].push_back(two_d_sum[i-1][j-1]+one_d_sum[i][j].first+one_d_sum[i][j].second-matrix[i][j]);
+                }
+            }
+        }
     }
 
     int sumRegion(int row1, int col1, int row2, int col2) {
-        
+        if(row1 == 0 && col1 == 0) return two_d_sum[row2][col2];
+        else if(row1 == 0) return two_d_sum[row2][col2]-two_d_sum[row2][col1-1];
+        else if(col1 == 0) return two_d_sum[row2][col2]-two_d_sum[row1-1][col2];
+        else return two_d_sum[row2][col2]-two_d_sum[row1-1][col2]-two_d_sum[row2][col1-1]+two_d_sum[row1-1][col1-1];
     }
 };
 
@@ -47,7 +68,7 @@ int main(){
         {1, 0, 3, 0, 5}
     };
     NumMatrix numMatrix(matrix);;
+    cout<<numMatrix.sumRegion(2, 1, 4, 3)<<endl;
     cout<<numMatrix.sumRegion(0, 1, 2, 3)<<endl;
-    cout<<numMatrix.sumRegion(1, 2, 3, 4)<<endl;
     return 0;
 }
