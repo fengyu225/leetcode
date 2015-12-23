1c65e163149d1929a5ba27b2f02d539f6068f8bb
Yu Feng <yfeng@boingo.com>
2015-12-23 15:41:58 -0800

    M  5/minimal_height_trees.cpp

--- 5/minimal_height_trees.cpp
+++ 5/minimal_height_trees.cpp
@@ -39,11 +39,59 @@ Note:
 (2) The height of a rooted tree is the number of edges on the longest downward path between the root and a leaf.
 */
 
-vector<int> findMinHeightTrees(int n, vector<pair<int, int> >& edges) {
+#include "header.h"
 
+vector<int> findMinHeightTrees(int n, vector<pair<int, int> >& edges) {
+    if(edges.size() == 0) return vector<int>(1, 0);
+    vector<int> res;
+    vector<unordered_set<int> > graph(n);
+    vector<int> degree(n, 0);
+    for(pair<int,int> e:edges){
+        graph[e.first].insert(e.second);
+        graph[e.second].insert(e.first);
+    }
+    queue<int> qs[2];
+    int curr = 0;
+    for(int i=0; i<n; i++){
+        degree[i] = graph[i].size();
+        if(degree[i] == 1) qs[curr].push(i);
+    }
+    for(int remains = n; remains>2;){
+        while(!qs[curr%2].empty()){
+            int temp = qs[curr%2].front();
+            qs[curr%2].pop();
+            remains--;
+            for(int i:graph[temp]){
+                degree[i]--;
+                graph[i].erase(graph[i].find(temp));
+                if(degree[i] == 1){
+                    qs[(curr+1)%2].push(i);
+                }
+            }
+        }
+        curr++;
+    }
+    while(!qs[curr%2].empty()){
+        res.push_back(qs[curr%2].front());
+        qs[curr%2].pop();
+    }
+    return res;
 }
 
 int main(){
-    
+    vector<pair<int,int> > edges = {
+        {0, 3},
+        {1, 3},
+        {2, 3},
+        {4, 3},
+        {5, 4}
+    };     
+    vector<int> res = findMinHeightTrees(6, edges);
+//    vector<pair<int,int> > edges = {
+//        {1, 0},
+//        {1, 2},
+//        {1, 3}
+//    vector<int> res = findMinHeightTrees(4, edges);
+    print_vector<int>(res);
     return 0;
 }
