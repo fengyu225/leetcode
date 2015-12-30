a592ea902fbae5fb41d6560dc820d2ebe4790974
Yu Feng <yfeng@boingo.com>
2015-12-30 15:01:35 -0800

    M  5/remove_duplicate_letters.cpp

--- 5/remove_duplicate_letters.cpp
+++ 5/remove_duplicate_letters.cpp
@@ -29,12 +29,57 @@ Return "acdb"
 //    return temp+removeDuplicateLetters(s);
 //}
 
+//    string removeDuplicateLetters(string s) {
+//        unordered_map<char, int> cnts;
+//        string ret;
+//        stack<char> stk;
+//        vector<bool> isVisited(26, false);
+//        for (char each : s) cnts[each] ++;
+//        for (int i = 0; i < s.size(); cnts[s[i]] --, ++ i) {
+//            if (isVisited[s[i] - 'a'] || (!stk.empty() && stk.top() == s[i])) continue;
+//            while (!stk.empty() && stk.top() > s[i] && cnts[stk.top()] > 0) {
+//                isVisited[stk.top() - 'a'] = false;
+//                stk.pop();
+//            }
+//            stk.push(s[i]);
+//            isVisited[s[i] - 'a'] = true;
+//        }
+//        while (!stk.empty()) {
+//            ret.push_back(stk.top());
+//            stk.pop();
+//        }
+//        reverse(ret.begin(), ret.end());
+//        return ret;
+//    }
+
 string removeDuplicateLetters(string s) {
     int sz = s.length();
     if(sz < 2) return s;
+    stack<int> st;
+    string res = "";
+    vector<int> count(26, 0);
+    vector<bool> in_stack(26, false);
+    for(char c:s) count[c-'a']++;
+    for(int i=0; i<sz; count[s[i]-'a']--, i++){
+        if(in_stack[s[i]-'a']) continue;
+        while(!st.empty() && st.top()>s[i] && count[st.top()-'a']>0){
+            in_stack[st.top()-'a'] = false;
+            st.pop();
+        }
+        st.push(s[i]);
+        in_stack[s[i]-'a']=true;
+    } 
+    while(!st.empty()){
+        res.push_back(st.top());
+        st.pop();
+    }
+    reverse(res.begin(), res.end());
+    return res;
 }
 
 int main(){
-    cout<<removeDuplicateLetters("cbacdcbc")<<endl;
+//    cout<<removeDuplicateLetters("cbacdcbc")<<endl;
+    //cout<<removeDuplicateLetters("yioccqiorhtoslwlvfgzycahonecugtatbyphpuunwvaalcpndabyldkdtzfjlgwqk")<<endl;
+    cout<<removeDuplicateLetters("baa")<<endl;
     return 0;
 }
