b1c69f836efa5defd7cb23039e52d1483eea5ca5
Yu Feng <yfeng@boingo.com>
2015-12-30 10:22:17 -0800

    M  5/remove_duplicate_letters.cpp

--- 5/remove_duplicate_letters.cpp
+++ 5/remove_duplicate_letters.cpp
@@ -11,22 +11,27 @@ Return "acdb"
 
 #include "header.h"
 
+//string removeDuplicateLetters(string s) {
+//    // https://leetcode.com/discuss/73761/a-short-o-n-recursive-greedy-solution
+//    int sz = s.length();
+//    if(sz == 0) return "";
+//    vector<int> count(26, 0);
+//    for(int i=0; i<sz; i++) count[s[i]-'a']++;
+//    int pos = 0;
+//    for(int i=0; i<sz; i++){
+//        if(s[i]<s[pos]) pos = i;
+//        count[s[i]-'a']--;
+//        if(count[s[i]-'a'] == 0) break;
+//    }
+//    char temp = s[pos];
+//    s = s.substr(pos+1, sz-pos-1);
+//    s.erase(remove(s.begin(), s.end(), temp), s.end());
+//    return temp+removeDuplicateLetters(s);
+//}
+
 string removeDuplicateLetters(string s) {
-    // https://leetcode.com/discuss/73761/a-short-o-n-recursive-greedy-solution
     int sz = s.length();
-    if(sz == 0) return "";
-    vector<int> count(26, 0);
-    for(int i=0; i<sz; i++) count[s[i]-'a']++;
-    int pos = 0;
-    for(int i=0; i<sz; i++){
-        if(s[i]<s[pos]) pos = i;
-        count[s[i]-'a']--;
-        if(count[s[i]-'a'] == 0) break;
-    }
-    char temp = s[pos];
-    s = s.substr(pos+1, sz-pos-1);
-    s.erase(remove(s.begin(), s.end(), temp), s.end());
-    return temp+removeDuplicateLetters(s);
+    if(sz < 2) return s;
 }
 
 int main(){
