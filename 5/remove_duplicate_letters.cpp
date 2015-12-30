02b0b618c0fa942c5df3ac2038adece7c4d12679
Yu Feng <yfeng@boingo.com>
2015-12-29 23:31:11 -0800

    M  5/header.h M  5/remove_duplicate_letters.cpp

--- 5/remove_duplicate_letters.cpp
+++ 5/remove_duplicate_letters.cpp
@@ -12,10 +12,24 @@ Return "acdb"
 #include "header.h"
 
 string removeDuplicateLetters(string s) {
-
+    // https://leetcode.com/discuss/73761/a-short-o-n-recursive-greedy-solution
+    int sz = s.length();
+    if(sz == 0) return "";
+    vector<int> count(26, 0);
+    for(int i=0; i<sz; i++) count[s[i]-'a']++;
+    int pos = 0;
+    for(int i=0; i<sz; i++){
+        if(s[i]<s[pos]) pos = i;
+        count[s[i]-'a']--;
+        if(count[s[i]-'a'] == 0) break;
+    }
+    char temp = s[pos];
+    s = s.substr(pos+1, sz-pos-1);
+    s.erase(remove(s.begin(), s.end(), temp), s.end());
+    return temp+removeDuplicateLetters(s);
 }
 
 int main(){
-    cout<<removeDuplicateLetters("bcabc")<<endl;
+    cout<<removeDuplicateLetters("cbacdcbc")<<endl;
     return 0;
 }
