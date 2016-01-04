0d72c671d3336a2add3c9bdbb1557cf0008bae03
Yu Feng <yfeng@boingo.com>
2016-01-03 21:13:44 -0800

    M  5/number_of_connected_components_in_undirected_graph.cpp

--- 5/number_of_connected_components_in_undirected_graph.cpp
+++ 5/number_of_connected_components_in_undirected_graph.cpp
@@ -24,8 +24,106 @@ You can assume that no duplicate edges will appear in edges. Since all edges are
 
 #include "header.h"
 
-int countComponents(int n, vector<pair<int, int> >& edges) {
+//void search(unordered_map<int,vector<int> >& edges, int i, vector<bool>& visited){
+//    for(int j:edges[i]){
+//        if(!visited[j]){
+//            visited[j] = true;
+//            search(edges, j, visited);
+//        }
+//    } 
+//}
+//
+//int countComponents(int n, vector<pair<int, int> >& edges) {
+//    //DFS
+//    int res = 0;
+//    vector<bool> visited(n, false);
+//    unordered_map<int,vector<int> > edges_map;
+//    for(auto e:edges){
+//        edges_map[e.first].push_back(e.second);
+//        edges_map[e.second].push_back(e.first);
+//    }
+//    for(int i=0; i<n; i++){
+//        if(!visited[i]){
+//            res++;
+//            visited[i] = true;
+//            search(edges_map, i, visited);
+//        }
+//    }
+//    return res;
+//}
 
+//void search(unordered_map<int,vector<int> >& edges, int i, vector<bool>& visited){
+//    queue<int> q;
+//    q.push(i);
+//    while(!q.empty()){
+//        int temp = q.front();
+//        q.pop();
+//        for(int j:edges[temp]){
+//            if(!visited[j]){
+//                visited[j] = true;
+//                q.push(j);
+//            }
+//        }
+//    }
+//}
+//
+//int countComponents(int n, vector<pair<int, int> >& edges) {
+//    //BFS
+//    int res = 0;
+//    unordered_map<int,vector<int> > edges_map;
+//    for(auto e:edges){
+//        edges_map[e.first].push_back(e.second);
+//        edges_map[e.second].push_back(e.first);
+//    }
+//    vector<bool> visited(n, false);
+//    for(int i=0; i<n; i++){
+//        if(!visited[i]){
+//            res++;
+//            visited[i] = true;
+//            search(edges_map, i, visited);
+//        }
+//    }
+//    return res;
+//}
+
+class Node{
+    public:
+        int x, rank;
+        Node* parent;
+        Node(int x):x(x),rank(1),parent(this){}
+};
+
+Node* f(Node* n){
+    if(n == n->parent) return n;
+    n->parent = f(n->parent);
+    return n->parent;
+}
+
+Node* u(Node* m, Node* n, int& res){
+    Node* p_m = f(m);
+    Node* p_n = f(n);
+    if(p_m == p_n) return p_m;
+    if(p_m->rank<p_n->rank){
+        p_m->parent = p_n;
+        res--;
+        return p_n;
+    }
+    else{
+        p_n->parent = p_m;
+        if(p_m->rank == p_n->rank) p_m->rank++;
+        res--;
+        return p_m;
+    }
+}
+
+int countComponents(int n, vector<pair<int, int> >& edges) {
+    int res = n;
+    vector<Node*> nodes(n); 
+    for(int i=0; i<n; i++) nodes[i] = new Node(i);
+    for(auto e:edges){
+        u(nodes[e.first], nodes[e.second], res); 
+    }
+    return res;
 }
 
 int main(){
