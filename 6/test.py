7e39e2c6e0783cbb52c314482859b35ac5f7c077
Yu Feng <yfeng@boingo.com>
2016-01-04 16:18:36 -0800

    D  6/test.py ?? 5/.ropeproject/ ?? 6/.ropeproject/ ?? 6/revisit

--- 6/test.py
+++
@@ -1,9 +0,0 @@
-def wiggleSort(nums):
-    nums.sort()
-    half = len(nums[::2])
-    nums[::2], nums[1::2] = nums[:half][::-1], nums[half:][::-1]
-
-if __name__ == "__main__":
-    nums = [1,3,2,2,2,1,1,3,1,1,2]
-    wiggleSort(nums)
-    print nums
