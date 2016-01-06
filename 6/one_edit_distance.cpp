48ccf0e5c7219782afba57b704544998d18f24cf
Yu Feng <yfeng@boingo.com>
2016-01-06 15:20:59 -0800

    M  6/one_edit_distance.cpp ?? 5/.ropeproject/ ?? 6/.ropeproject/ ?? 6/file ?? 6/revisit

--- 6/one_edit_distance.cpp
+++ 6/one_edit_distance.cpp
@@ -13,13 +13,29 @@ bool isOneEditDistance(string s, string t){
     }
     int s_sz = s.length();
     int t_sz = t.length();
+    if(abs(s_sz-t_sz)>=2) return false;
+    if(t_sz == 0) return s_sz == 1;
+    int s_curr = 0, t_curr = 0, found = 0;
+    while(s_curr<s_sz && t_curr<t_sz){
+        if(s[s_curr] != t[t_curr]){
+            if(found>=1) return false;
+            found++;
+            if(s_sz == t_sz) t_curr++;
+        }
+        else t_curr++;
+        s_curr++;
+    }
+    return (s_curr == s_sz && t_curr == t_sz && found == 1) || (found == 0 && s_sz-s_curr==1);
 }
 
 int main(){
-    cout<<isOneEditDistance("", "")<<endl;
-    cout<<isOneEditDistance("a", "")<<endl;
-    cout<<isOneEditDistance("abc", "ab")<<endl;
-    cout<<isOneEditDistance("abcd", "abe")<<endl;
+//    cout<<isOneEditDistance("", "")<<endl;
+//    cout<<isOneEditDistance("a", "")<<endl;
+//    cout<<isOneEditDistance("abc", "ab")<<endl;
+//    cout<<isOneEditDistance("abcd", "abe")<<endl;
     cout<<isOneEditDistance("abcd", "abed")<<endl;
+    cout<<isOneEditDistance("a", "A")<<endl;
+    cout<<isOneEditDistance("a", "ba")<<endl;
+    cout<<isOneEditDistance("ab", "acb")<<endl;
     return 0;
 }
