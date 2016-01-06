9bc8cc364233d4d97c6f0f42ca3c99c1997aa847
Yu Feng <yfeng@boingo.com>
2016-01-05 23:17:44 -0800

    M  6/read_n_char_given_read4_ii.cpp ?? 5/.ropeproject/ ?? 6/.ropeproject/ ?? 6/file ?? 6/revisit

--- 6/read_n_char_given_read4_ii.cpp
+++ 6/read_n_char_given_read4_ii.cpp
@@ -22,41 +22,8 @@ int read4(char *buf){
     return 4;
 }
 
-char temp_buf[4];
-char* temp_buf_curr = temp_buf;
-int temp_buf_size = 0;
-
 int read(char *buf, int n) {
-    if(n == 0) return 0;
-    int cnt = 0;
-    while(1){
-        if(temp_buf_curr<temp_buf+temp_buf_size){
-            *(buf++)=*(temp_buf_curr++);
-            cnt++;
-            if(cnt == n){
-                *buf = '\0';
-                return n;
-            }
-            continue;
-        }
-        int temp = read4(buf);
-        if(n-cnt<temp){
-            temp_buf_curr = temp_buf;
-            temp_buf_size = temp-(n-cnt);
-            for(char* x = buf+(n-cnt); x<buf+temp; *(temp_buf_curr++)=*(x++));
-            temp_buf_curr = temp_buf;
-            *(buf+(n-cnt)) = '\0';
-            return n;
-        }
-        if(temp<4){
-            temp_buf_curr = temp_buf;
-            temp_buf_size = 0;
-            *(buf+temp) = '\0';
-            return cnt+temp;
-        }
-        buf += 4;
-        cnt += 4;
-    }
+
 }
 
 int main(){
