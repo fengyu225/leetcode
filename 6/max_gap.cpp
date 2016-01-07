5f3ba999c6a73583646f1ca9a96dd9c4b9fb4dfb
Yu Feng <yfeng@boingo.com>
2016-01-06 20:06:54 -0800

    M  6/max_gap.cpp ?? 5/.ropeproject/ ?? 6/.ropeproject/ ?? 6/file ?? 6/revisit

--- 6/max_gap.cpp
+++ 6/max_gap.cpp
@@ -49,6 +49,31 @@ int maximumGap(vector<int>& nums) {
     return res;
 }
 
+/*
+https://leetcode.com/discuss/34289/pigeon-hole-principle
+int maximumGap(vector<int>& nums) {
+        const int n = nums.size();
+        if(n<=1) return 0;
+        int maxE = *max_element(nums.begin(),nums.end());
+        int minE = *min_element(nums.begin(),nums.end());
+        double len = double(maxE-minE)/double(n-1);
+        vector<int> maxA(n,INT_MIN);
+        vector<int> minA(n,INT_MAX);
+        for(int i=0; i<n; i++) {
+            int index = (nums[i]-minE)/len;
+            maxA[index] = max(maxA[index],nums[i]);
+            minA[index] = min(minA[index],nums[i]);
+        }
+        int gap = 0, prev = maxA[0];
+        for(int i=1; i<n; i++) {
+            if(minA[i]==INT_MAX) continue;
+            gap = max(gap,minA[i]-prev);
+            prev = maxA[i];
+        }
+        return gap;
+    } 
+*/
+
 int main(){
     //vector<int> nums = {8, 1, 5, 9, 2};
     vector<int> nums = {1,1,1,1};
