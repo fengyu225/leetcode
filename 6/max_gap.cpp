b422f0a3fe15222133e0b2a6e94bcd8335e36767
Yu Feng <yfeng@boingo.com>
2016-01-06 18:53:47 -0800

    A  6/max_gap.cpp ?? 5/.ropeproject/ ?? 6/.ropeproject/ ?? 6/file ?? 6/revisit

---
+++ 6/max_gap.cpp
@@ -0,0 +1,21 @@
+/*
+Given an unsorted array, find the maximum difference between the successive elements in its sorted form.
+
+Try to solve it in linear time/space.
+
+Return 0 if the array contains less than 2 elements.
+
+You may assume all elements in the array are non-negative integers and fit in the 32-bit signed integer range.
+*/
+
+#include "header.h"
+
+int maximumGap(vector<int>& nums) {
+
+}
+
+int main(){
+    vector<int> nums = {8, 1, 5, 9, 2};
+    cout<<maximumGap(nums)<<endl;
+    return 0;
+}
