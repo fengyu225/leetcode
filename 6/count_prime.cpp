51b87bfc7e26b82b8b9124d3c6cf213472670bb6
Yu Feng <yfeng@boingo.com>
2016-01-08 00:50:32 -0800

    A  6/count_prime.cpp A  6/happy_number.cpp A  6/remove_linked_list_element.cpp ?? 5/.ropeproject/ ?? 6/.ropeproject/ ?? 6/file ?? 6/revisit

---
+++ 6/count_prime.cpp
@@ -0,0 +1,34 @@
+/*
+Description:
+
+Count the number of prime numbers less than a non-negative number, n.
+
+Credits:
+Special thanks to @mithmatt for adding this problem and creating all test cases.
+
+Hint:
+
+Let's start with a isPrime function. To determine if a number is prime, we need to check if it is not divisible by any number less than n. The runtime complexity of isPrime function would be O(n) and hence counting the total prime numbers up to n would be O(n2). Could we do better?
+*/
+
+#include "header.h"
+
+int countPrimes(int n) {
+    if(n<=2) return 0;
+    vector<bool> arr(n-1, true);
+    for(int i=2; i*i<n; i++){
+        if(!arr[i]) continue;
+        int temp = i;
+        for(int j=2; temp*j<n; j++) arr[temp*j]=false;
+    }
+    int res = 0;
+    for(int i=2; i<n; i++){
+        if(arr[i]) res++;
+    }
+    return res;
+}
+
+int main(){
+    cout<<countPrimes(20)<<endl;
+    return 0;
+}
