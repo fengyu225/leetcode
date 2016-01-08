1a20d19e7a445346cc095bee5532800817a1c7d1
Yu Feng <yfeng@boingo.com>
2016-01-07 20:43:37 -0800

    M  6/repeated_dna_seq.cpp ?? 5/.ropeproject/ ?? 6/.ropeproject/ ?? 6/file ?? 6/revisit

--- 6/repeated_dna_seq.cpp
+++ 6/repeated_dna_seq.cpp
@@ -12,12 +12,49 @@ Return:
 
 #include "header.h"
 
+unordered_map<char,int> m = {
+    {'A',0},
+    {'C',1},
+    {'G',2},
+    {'T',3}
+};
+
+int str_2_int(string s){
+    int res = 0;
+    for(int i=0;i<s.length();i++){
+        res |= m[s[i]];
+        res <<= 2;
+    }
+    res >>= 2;
+    return res;
+}
+
 vector<string> findRepeatedDnaSequences(string s){
     int sz = s.length();
+    unordered_set<int> st;
+    if(sz<10) return vector<string>();
+    int curr = str_2_int(s.substr(0,10));
+    st.insert(curr);
+    unordered_set<int> more_than_twice;
+    vector<string> res;
+    for(int i=1; i<=sz-10; i++){
+        curr <<= 2;
+        curr &= (1<<20)-1;
+        curr |= m[s[i+9]];
+        if(st.find(curr) != st.end()){
+            if(more_than_twice.find(curr) == more_than_twice.end()){
+                res.push_back(s.substr(i,10));
+                more_than_twice.insert(curr);
+            }
+        }
+        else st.insert(curr);
+    }
+    return res;
 }
 
 int main(){
-    string s = "AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT";
+//    string s = "AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT";
+    string s = "AAAAAAAAAAAA";
     vector<string> res = findRepeatedDnaSequences(s);
     for(int i=0; i<res.size(); i++) cout<<res[i]<<" ";
     cout<<endl;
