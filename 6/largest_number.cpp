122f315b89e5fce7b77a525405513c846f39984a
Yu Feng <yfeng@boingo.com>
2016-01-07 17:36:37 -0800

    M  5/largest_number.cpp A  6/largest_number.cpp ?? 5/.ropeproject/ ?? 6/.ropeproject/ ?? 6/file ?? 6/revisit

--- 6/largest_number.cpp
+++ 6/largest_number.cpp
@@ -8,21 +8,24 @@ Note: The result may be very large, so you need to return a string instead of an
 
 #include "header.h"
 
-bool comp(int a, int b){
-    string a_str = to_string(a);
-    string b_str = to_string(b);
-    return (a_str+b_str).compare(b_str+a_str)>0;
-}
+class comp{
+    public:
+        bool operator() (int a, int b){
+            string a_s = to_string(a);
+            string b_s = to_string(b);
+            return (a_s+b_s).compare(b_s+a_s)>0;
+        }
+};
 
 string largestNumber(vector<int>& nums) {
-    sort(nums.begin(), nums.end(), comp);
+    sort(nums.begin(), nums.end(), comp());
     string res = "";
-    for(int i=0; i<nums.size(); i++) res += to_string(nums[i]);
+    for(int i:nums) res += to_string(i);
     return res;
 }
 
 int main(){
-//    int arr[] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+ //   int arr[] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
     int arr[] = {3, 30, 34, 5, 9};
     vector<int> nums(arr,arr+sizeof(arr)/sizeof(arr[0]));
     cout<<largestNumber(nums)<<endl;
