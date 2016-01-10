d519c210a4b3807b67f2c2e2694944d7c84f54f5
Yu Feng <yfeng@boingo.com>
2016-01-09 20:22:37 -0800

    A  6/basic_calculator_ii.cpp ?? 5/.ropeproject/ ?? 6/.ropeproject/ ?? 6/file ?? 6/revisit

---
+++ 6/basic_calculator_ii.cpp
@@ -0,0 +1,28 @@
+/*
+Implement a basic calculator to evaluate a simple expression string.
+
+The expression string contains only non-negative integers, +, -, *, / operators and empty spaces . The integer division should truncate toward zero.
+
+You may assume that the given expression is always valid.
+
+Some examples:
+
+"3+2*2" = 7
+" 3/2 " = 1
+" 3+5 / 2 " = 5
+
+Note: Do not use the eval built-in library function.
+*/
+
+#include "header.h"
+
+
+
+int main(){
+//    cout<<calculate("3+2*2")<<endl;
+//    cout<<calculate("3/2")<<endl;
+//    cout<<calculate("3+5 / 2")<<endl;
+    //cout<<calculate("1*2-3/4+5*6-7*8+9/10")<<endl;
+    cout<<calculate("42")<<endl;
+    return 0;
+}
