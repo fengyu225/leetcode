6d745621da7cab849c33e24e91271136d30dd7c0
Yu Feng <yfeng@boingo.com>
2016-01-10 14:57:52 -0800

    A  6/delete_node_in_a_linked_list.cpp A  6/lca.cpp A  6/lca_bst.cpp ?? 5/.ropeproject/ ?? 6/.ropeproject/ ?? 6/file ?? 6/revisit

---
+++ 6/lca.cpp
@@ -0,0 +1,33 @@
+/*
+Given a binary tree, find the lowest common ancestor (LCA) of two given nodes in the tree.
+
+According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes v and w as the lowest node in T that has both v and w as descendants (where we allow a node to be a descendant of itself).”
+        _______3______
+       /              \
+    ___5__          ___1__
+   /      \        /      \
+   6      _2       0       8
+         /  \
+         7   4
+For example, the lowest common ancestor (LCA) of nodes 5 and 1 is 3. Another example is LCA of nodes 5 and 4 is 5, since a node can be a descendant of itself according to the LCA definition.
+*/
+
+#include "header.h"
+
+TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q){
+    if(!root || !p || !q) return NULL;
+    if(root == p || root == q) return root;
+    TreeNode* l_res = lowestCommonAncestor(root->left, p, q);
+    TreeNode* r_res = lowestCommonAncestor(root->right, p, q);
+    if(l_res && r_res) return root;
+    return l_res?l_res:r_res;
+}
+
+int main(){
+    TreeNode* root = create_tree("3516208##74");
+    TreeNode* res = lowestCommonAncestor(root, root->left, root->left->right->right);
+    if(!res) cout<<"NULL";
+    cout<<res->val;
+    cout<<endl;
+    return 0;
+}
