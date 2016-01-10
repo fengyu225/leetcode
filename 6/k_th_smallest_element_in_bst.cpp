fcedeae20a1713a1f73f13df90d1571cd87712ae
Yu Feng <yfeng@boingo.com>
2016-01-10 11:41:26 -0800

    M  6/k_th_smallest_element_in_bst.cpp ?? 5/.ropeproject/ ?? 6/.ropeproject/ ?? 6/file ?? 6/revisit

--- 6/k_th_smallest_element_in_bst.cpp
+++ 6/k_th_smallest_element_in_bst.cpp
@@ -10,28 +10,51 @@ What if the BST is modified (insert/delete operations) often and you need to fin
 
 #include "header.h"
 
-void search(TreeNode* root, int& k, TreeNode*& res){
-    if(!root || res) return;
-    search(root->left, k, res);
-    k--;
-    if(k == 0){
-        res = root;
-        return;
-    }
-    search(root->right, k, res);
-}
+/* dfs recursive */
+//void search(TreeNode* root, int& k, TreeNode*& res){
+//    if(!root || res) return;
+//    search(root->left, k, res);
+//    k--;
+//    if(k == 0){
+//        res = root;
+//        return;
+//    }
+//    search(root->right, k, res);
+//}
+//
+//int kthSmallest(TreeNode* root, int k) {
+//    if(!root) return 0;
+//    TreeNode* res = NULL;
+//    search(root, k, res);
+//    return res->val;
+//}
 
 int kthSmallest(TreeNode* root, int k) {
-    if(!root) return 0;
-    TreeNode* res = NULL;
-    search(root, k, res);
-    return res->val;
+    stack<TreeNode*> st;
+    st.push(root);
+    while(!st.empty()){
+        TreeNode* curr = st.top();
+        if(curr->left) st.push(curr->left);
+        else{
+            while(!st.empty()){
+                if(k == 1) return st.top()->val;
+                k--;
+                TreeNode* temp = st.top();
+                st.pop();
+                if(temp->right){
+                    st.push(temp->right);
+                    break;
+                }
+            }
+        }
+    }
+    return 0;
 }
 
 int main(){
     //TreeNode* root = create_tree("53724681#");
     //TreeNode* root = create_tree("1#2");
     TreeNode* root = create_tree("21");
-    cout<<kthSmallest(root, 1)<<endl;
+    cout<<kthSmallest(root, 2)<<endl;
     return 0;
 }
