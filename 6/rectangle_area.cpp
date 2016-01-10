38cc92bac9612e4fa9042d00f80a56930c08c833
Yu Feng <yfeng@boingo.com>
2016-01-09 20:20:00 -0800

    M  6/rectangle_area.cpp ?? 5/.ropeproject/ ?? 6/.ropeproject/ ?? 6/file ?? 6/revisit

--- 6/rectangle_area.cpp
+++ 6/rectangle_area.cpp
@@ -13,6 +13,13 @@ Assume that the total area is never beyond the maximum possible value of int.
 int computeArea(int A, int B, int C, int D, int E, int F, int G, int H){
     // rectangle 1: (A,B), (C,D)
     // rectangle 2: (E,F), (G,H)
+    long common = 0, area_0 = 0, area_1 = 0;
+    int r_x = min(C, G), l_x = max(E, A);
+    int t_y = min(D, H), b_y = max(F, B);
+    common = (r_x>=l_x?r_x-l_x:0)*(t_y>=b_y?t_y-b_y:0);
+    area_0 = (long)((C-A)*(D-B));
+    area_1 = (long)((G-E)*(H-F));
+    return area_0+area_1-common;
 }
 
 int main(){
