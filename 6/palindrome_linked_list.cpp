f5b383cfc4964c43fb9ea65c1c7115b2764e051c
Yu Feng <yfeng@boingo.com>
2016-01-10 12:59:13 -0800

    A  6/palindrome_linked_list.cpp ?? 5/.ropeproject/ ?? 6/.ropeproject/ ?? 6/file ?? 6/revisit

---
+++ 6/palindrome_linked_list.cpp
@@ -0,0 +1,25 @@
+/*
+Given a singly linked list, determine if it is a palindrome.
+
+Follow up:
+Could you do it in O(n) time and O(1) space?
+*/
+
+#include "header.h"
+
+bool check(ListNode*& head){
+    
+}
+
+bool isPalindrome(ListNode* head) {
+    if(!head || !head->next) return true;
+    ListNode* curr = head;
+    return check(curr);
+}
+
+int main(){
+    int arr[] = {1,0,1};
+    ListNode* head = create_list(arr, sizeof(arr)/sizeof(arr[0]));
+    cout<<isPalindrome(head)<<endl;
+    return 0;
+}
