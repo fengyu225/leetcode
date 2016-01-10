2bc7610bbae0d66145dcc926ae6e026efc4e2e28
Yu Feng <yfeng@boingo.com>
2016-01-09 17:07:56 -0800

    M  6/contains_duplicate_ii.cpp ?? 5/.ropeproject/ ?? 6/.ropeproject/ ?? 6/file ?? 6/revisit

--- 6/contains_duplicate_ii.cpp
+++ 6/contains_duplicate_ii.cpp
@@ -7,12 +7,26 @@ Given an array of integers and an integer k, find out whether there there are tw
 bool containsNearbyDuplicate(vector<int>& nums, int k){
     int sz = nums.size();
     if(sz < 2 || k == 0) return false;
+    unordered_map<int,int> counts;
+    for(int i=0; i<=min(k, sz-1); i++){
+        counts[nums[i]]++;
+        if(counts[nums[i]] > 1) return true;
+    }
+    if(k>=sz-1) return false;
+    int l = 0, r = k+1;
+    while(r<sz){
+        counts[nums[r]]++;
+        counts[nums[l++]]--;
+        if(counts[nums[r++]]>1) return true;
+    }
+    return false;
 }
 
 int main(){
-    int arr[] = {1,2,3,1,3};
+    //int arr[] = {1,2,3,1,3};
+    int arr[] = {1,2,1};
 //    int arr[] = {99,99};
     vector<int> v_arr(arr, arr+sizeof(arr)/sizeof(arr[0]));
-    cout<<containsNearbyDuplicate(v_arr, 3)<<endl;
+    cout<<containsNearbyDuplicate(v_arr, 1)<<endl;
     return 0;
 }
