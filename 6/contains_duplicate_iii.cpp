07b1dc5742e6808ccddbf5151c50a6c6aee1c648
Yu Feng <yfeng@boingo.com>
2016-01-09 17:08:22 -0800

    A  6/contains_duplicate_iii.cpp ?? 5/.ropeproject/ ?? 6/.ropeproject/ ?? 6/file ?? 6/revisit

---
+++ 6/contains_duplicate_iii.cpp
@@ -0,0 +1,17 @@
+/*
+Given an array of integers, find out whether there are two distinct indices i and j in the array such that the difference between nums[i] and nums[j] is at most t and the difference between i and j is at most k.
+*/
+
+#include "header.h"
+
+bool containsNearbyAlmostDuplicate(vector<int>& nums, int k, int t){
+    int sz = nums.size();
+    if(sz < 2) return false;
+}
+
+int main(){
+    int arr[] = {1,2,3,1,3};
+    vector<int> v_arr(arr, arr+sizeof(arr)/sizeof(arr[0]));
+    cout<<containsNearbyAlmostDuplicate(v_arr,2,1)<<endl;
+    return 0;
+}
