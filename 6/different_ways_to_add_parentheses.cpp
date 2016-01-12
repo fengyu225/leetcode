550a58477fcbba2dcf569b168a959337755f74d3
Yu Feng <yfeng@boingo.com>
2016-01-11 16:10:09 -0800

    M  6/different_ways_to_add_parentheses.cpp ?? 5/.ropeproject/ ?? 6/.ropeproject/ ?? 6/file ?? 6/revisit

--- 6/different_ways_to_add_parentheses.cpp
+++ 6/different_ways_to_add_parentheses.cpp
@@ -20,17 +20,49 @@ Output: [-34, -14, -10, -10, 10]
 #include "header.h"
 
 vector<int> diffWaysToCompute(string input){
+    int sz = input.length();
     vector<int> res;
-    return res;
+    if(sz == 0) return res;
+    vector<string> input_arr;
+    int l=0, r = 0;
+    while(r<sz){
+        if(input[r] == '+' || input[r] == '-' || input[r] == '*'){
+            input_arr.push_back(input.substr(l, r-l));
+            input_arr.push_back(string(1,input[r]));
+            l = r+1;
+        }
+        r++;
+    }
+    input_arr.push_back(input.substr(l, r-l));
+    sz=input_arr.size();
+    vector<vector<vector<int> > > arr(sz, vector<vector<int> >(sz, vector<int>()));
+    for(int len = 1; len<=sz; len+=2){
+        for(int start = 0; start+len<=sz; start+=2){
+            if(len == 1) arr[start][start] = vector<int>(1, stoi(input.substr(start, 1))); 
+            else{
+                vector<int> temp;
+                for(int left_len = 1; left_len<len; left_len+=2){
+                    vector<int> left = arr[start][start+left_len-1];
+                    vector<int> right = arr[start+left_len+1][start+len-1];
+                    for(int l_v:left){
+                        for(int r_v:right){
+                            if(input_arr[start+left_len] == "+") temp.push_back(l_v+r_v);
+                            if(input_arr[start+left_len] == "-") temp.push_back(l_v-r_v);
+                            if(input_arr[start+left_len] == "*") temp.push_back(l_v*r_v);
+                        }
+                    }
+                }  
+                arr[start][start+len-1] = temp;
+            }
+        }
+    } 
+    return arr[0][sz-1];
 }
 
 int main(){
     vector<string> input = {
-		"(2*(3-(4*5)))",
-		"((2*3)-(4*5))",
-		"((2*(3-4))*5)",
-		"(2*((3-4)*5))",
-		"(((2*3)-4)*5)"
+        "2*3-4*5",
+        "2-1-1"
     };
     for(string s:input){
         cout<<s<<endl;
