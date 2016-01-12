4052510c86e7a54b951acf69f25ce9aba8518cf2
Yu Feng <yfeng@boingo.com>
2016-01-11 20:27:28 -0800

    A  6/flatten_2d_matrix.cpp ?? 5/.ropeproject/ ?? 6/.ropeproject/ ?? 6/file ?? 6/revisit

---
+++ 6/flatten_2d_matrix.cpp
@@ -0,0 +1,59 @@
+/*
+Implement an iterator to flatten a 2d vector.
+
+For example,
+Given 2d vector =
+[
+  [1,2],
+  [3],
+  [4,5,6]
+]
+By calling next repeatedly until hasNext returns false, the order of elements returned by next should be: [1,2,3,4,5,6].
+As an added challenge, try to code it using only iterators in C++ or iterators in Java.
+*/
+
+#include "header.h"
+
+class Vector2D {
+public:
+    Vector2D(vector<vector<int>>& vec2d) {
+        
+    }
+
+    int next() {
+        
+    }
+
+    bool hasNext() {
+        
+    }
+};
+
+/**
+ * Your Vector2D object will be instantiated and called as such:
+ * Vector2D i(vec2d);
+ * while (i.hasNext()) cout << i.next();
+ */
+
+int main(){
+//    int arr0[] = {1, 2};
+//    int arr1[] = {3};
+//    int arr2[] = {4,5,6};
+//    vector<int> v_arr0(arr0, arr0+sizeof(arr0)/sizeof(arr0[0]));
+//    vector<int> v_arr1(arr1, arr1+sizeof(arr1)/sizeof(arr1[0]));
+//    vector<int> v_arr2(arr2, arr2+sizeof(arr2)/sizeof(arr2[0]));
+//    vector<vector<int> > vec2d = {v_arr0, v_arr1, v_arr2};
+//
+    int arr0[] = {1};
+    int arr1[] = {};
+    vector<int> v_arr0(arr0, arr0+sizeof(arr0)/sizeof(arr0[0]));
+    vector<int> v_arr1(arr1, arr1);
+    vector<vector<int> > vec2d = {v_arr0, v_arr1};
+
+    Vector2D vec(vec2d);
+    while(vec.hasNext()){
+        cout<<vec.next()<<endl;    
+    }
+    cout<<endl;
+    return 0;
+}
