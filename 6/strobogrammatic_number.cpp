b601d29350ea4b6ee769e23ea4a0255192ab6538
Yu Feng <yfeng@boingo.com>
2016-01-11 18:05:18 -0800

    M  6/strobogrammatic_number.cpp ?? 5/.ropeproject/ ?? 6/.ropeproject/ ?? 6/file ?? 6/revisit

--- 6/strobogrammatic_number.cpp
+++ 6/strobogrammatic_number.cpp
@@ -8,17 +8,31 @@ For example, the numbers "69", "88", and "818" are all strobogrammatic.
 
 #include "header.h"
 
+unordered_map<char,char> m = {
+    {'0', '0'},
+    {'1', '1'},
+    {'6', '9'},
+    {'8', '8'},
+    {'9', '6'}
+};
+
 bool isStrobogrammatic(string num) {
     int sz = num.length();
     if(sz == 0) return true;
-    if(sz == 1) return num[0] == '0' || num[0] == '8' || num[0] == '1';
+    int l = 0, r = sz-1;
+    while(l<=r){
+        if(m[num[l]] != num[r]) return false;
+        l++;
+        r--;
+    }
+    return true;
 }
 
 int main(){
     cout<<isStrobogrammatic("60809")<<endl;
-    //cout<<isStrobogrammatic("101")<<endl;
-    //cout<<isStrobogrammatic("33")<<endl;
-    //cout<<isStrobogrammatic("69")<<endl;
-    //cout<<isStrobogrammatic("818")<<endl;
+    cout<<isStrobogrammatic("101")<<endl;
+    cout<<isStrobogrammatic("33")<<endl;
+    cout<<isStrobogrammatic("69")<<endl;
+    cout<<isStrobogrammatic("818")<<endl;
     return 0;
 }
