b7410fdc123c78702e2d07222b21f187e8653460
Yu Feng <yfeng@boingo.com>
2016-01-12 21:38:17 -0800

    M  6/3_sum_smaller.cpp  D 6/add_digits.cpp ?? 5/.ropeproject/ ?? 6/.ropeproject/ ?? 6/file ?? 6/revisit ?? 6/test

--- 6/3_sum_smaller.cpp
+++ 6/3_sum_smaller.cpp
@@ -11,11 +11,48 @@ Could you solve it in O(n2) runtime?
 
 #include "header.h"
 
+//int threeSumSmaller(vector<int>& nums, int target){
+//    int sz = nums.size();
+//    int res = 0;
+//    sort(nums.begin(), nums.end());
+//    for(int i=0; i+2<sz; i++){
+//        if(nums[i]>=target) break;
+//        int temp = target-nums[i];
+//        int l = i+1, r=sz-1;
+//        while(l<r){
+//            int x = nums[l]+nums[r];
+//            if(x<temp){
+//                cout<<l<<" "<<r<<endl;
+//                res++;
+//                r--;;
+//            }
+//            else l++;
+//        }
+//    }
+//    return res;
+//}
 int threeSumSmaller(vector<int>& nums, int target){
+    int sz = nums.size();
+    if(sz<3) return 0;
+    sort(nums.begin(), nums.end());
+    int res = 0;
+    for(int i=0; i+2<sz; i++){
+        int l = i+1, r=sz-1;
+        while(l<r){
+            int temp = nums[i]+nums[l]+nums[r];
+            if(temp<target){
+                res += (r-l);
+                l++;
+            }
+            else r--;
+        }
+    }
+    return res;
 }
-
 int main(){
+    //vector<int> nums = {-2, 0, 1, 3};
+    //cout<<threeSumSmaller(nums, 2)<<endl;
     vector<int> nums = {-2, 0, 1, 3};
-    cout<<threeSumSmaller(nums, 2)<<endl;
+    cout<<threeSumSmaller(nums, 4)<<endl;
     return 0;
 }
