9141e33c010b7f9e76a1cb3ba09e2ebf2883bd68
Yu Feng <yfeng@boingo.com>
2016-01-12 22:37:46 -0800

    D 6/add_digits.cpp M  6/graph_valid_tree.cpp ?? 5/.ropeproject/ ?? 6/.ropeproject/ ?? 6/file ?? 6/revisit ?? 6/test

--- 6/graph_valid_tree.cpp
+++ 6/graph_valid_tree.cpp
@@ -11,35 +11,39 @@ Note: you can assume that no duplicate edges will appear in edges. Since all edg
 
 #include "header.h"
 
-bool search(unordered_map<int,vector<int> >& graph, vector<int>& color, int curr, int from){
-    for(int i:graph[curr]){
-        if(from>=0 && from == i) continue;
-        if(color[i] == 1) return false;
-        if(color[i] == 2) continue;
-        color[i] = 1;
-        bool res = search(graph, color, i, curr);
-        if(!res) return false;
-    }
-    color[curr] = 2;
-    return true;
-}
+//bool search(unordered_map<int,vector<int> >& graph, vector<int>& color, int curr, int from){
+//    for(int i:graph[curr]){
+//        if(from>=0 && from == i) continue;
+//        if(color[i] == 1) return false;
+//        if(color[i] == 2) continue;
+//        color[i] = 1;
+//        bool res = search(graph, color, i, curr);
+//        if(!res) return false;
+//    }
+//    color[curr] = 2;
+//    return true;
+//}
+//
+//bool validTree(int n, vector<pair<int, int> >& edges) {
+//    //dfs
+//    vector<int> color(n, 0);
+//    if(edges.size() != n-1) return false;
+//    unordered_map<int,vector<int> > m;
+//    for(auto p:edges){
+//        m[p.first].push_back(p.second);
+//        m[p.second].push_back(p.first);
+//    }
+//    for(int i=0; i<n; i++){
+//        if(color[i] != 0) continue;
+//        color[i] = 1;
+//        bool res = search(m, color, i, -1);
+//        if(!res) return false;
+//    }
+//    return true;
+//}
 
 bool validTree(int n, vector<pair<int, int> >& edges) {
-    //dfs
-    vector<int> color(n, 0);
-    if(edges.size() != n-1) return false;
-    unordered_map<int,vector<int> > m;
-    for(auto p:edges){
-        m[p.first].push_back(p.second);
-        m[p.second].push_back(p.first);
-    }
-    for(int i=0; i<n; i++){
-        if(color[i] != 0) continue;
-        color[i] = 1;
-        bool res = search(m, color, i, -1);
-        if(!res) return false;
-    }
-    return true;
+
 }
 
 int main(){
