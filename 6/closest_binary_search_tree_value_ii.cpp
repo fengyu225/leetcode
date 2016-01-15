6dcae8eb038fc727d07a4f495f609f604fda3c35
Yu Feng <yfeng@boingo.com>
2016-01-14 17:11:55 -0800

    M  6/closest_binary_search_tree_value_ii.cpp

--- 6/closest_binary_search_tree_value_ii.cpp
+++ 6/closest_binary_search_tree_value_ii.cpp
@@ -11,15 +11,78 @@ Assume that the BST is balanced, could you solve it in less than O(n) runtime (w
 
 #include "header.h"
 
-vector<int> closestKValues(TreeNode* root, double target, int k) {
+void init_small(TreeNode* root, stack<TreeNode*>& small, int target){
+    //largest smaller than target
+    TreeNode* curr = root;
+    while(curr){
+        if(curr->val<=target){
+            small.push(curr);
+            curr = curr->right;
+        }
+        else curr = curr->left;
+    }
+}
 
+void init_large(TreeNode* root, stack<TreeNode*>& large, int target){
+    //smallest larger than target
+    TreeNode* curr = root;
+    while(curr){
+        if(curr->val>target){
+            large.push(curr);
+            curr = curr->left;
+        }
+        else curr = curr->right;
+    }
+}
+
+void find_next_small(stack<TreeNode*>& small){
+    if(small.empty()) return;
+    TreeNode* curr = small.top()->left;
+    small.pop();
+    while(curr){
+        small.push(curr);
+        curr = curr->right;
+    }
+}
+
+void find_next_large(stack<TreeNode*>& large){
+    if(large.empty()) return;
+    TreeNode* curr = large.top()->right;
+    large.pop();
+    while(curr){
+        large.push(curr);
+        curr = curr->left;
+    }
+}
+
+vector<int> closestKValues(TreeNode* root, double target, int k) {
+    stack<TreeNode*> small, large;
+    vector<int> res;
+    if(!root) return res;
+    init_small(root, small, target);
+    init_large(root, large, target);
+    for(int i=0; i<k; i++){
+        TreeNode* n;
+        if(small.empty() || !large.empty() && target-(double)small.top()->val>(double)large.top()->val-target){
+            n = large.top();
+            find_next_large(large);
+            res.push_back(n->val);
+        }
+        else{
+            n = small.top();
+            find_next_small(small);
+            res.push_back(n->val);
+        }
+    }
+    return res;
 }
 
 int main(){
-    //vector<string> v = {"5","3","10","1","4","7","11","#","#","#","#","6","#","#","12"};
-    //TreeNode* root = create_tree(v);
-    TreeNode* root = create_tree("1");
-    vector<int> res = closestKValues(root, 0.0, 1);
+    vector<string> v = {"5","3","10","1","4","7","11","#","#","#","#","6","#","#","12"};
+    TreeNode* root = create_tree(v);
+    vector<int> res = closestKValues(root, 7.7, 5);
+    //TreeNode* root = create_tree("1");
+    //vector<int> res = closestKValues(root, 0.0, 1);
     for(int i:res) cout<<i<<" ";
     cout<<endl;
     return 0;
