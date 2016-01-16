b7e4b315f84b17d0712a12563f68b71790b23f64
Yu Feng <yfeng@boingo.com>
2016-01-15 16:31:39 -0800

    M  6/find_the_duplicate_number.cpp

--- 6/find_the_duplicate_number.cpp
+++ 6/find_the_duplicate_number.cpp
@@ -9,8 +9,27 @@ Your runtime complexity should be less than O(n2).
 
 #include "header.h"
 
-int findDuplicate(vector<int>& nums) {
+void search(vector<int>& nums, int& lt, int& gt, int& eq, int m){
+    for(int c:nums){
+        if(c == m) eq++;
+        else if(c<m) lt++;
+        else gt++;
+    }
+}
 
+int findDuplicate(vector<int>& nums) {
+    int n = nums.size();    
+    n-=1;
+    int l = 1, r = n;
+    while(l<r){
+        int m = l+(r-l)/2;
+        int lt = 0, gt=0, eq = 0;
+        search(nums, lt, gt, eq, m);
+        if(eq>1) return m;
+        if(lt>m-1) r=m-1;
+        else l=m+1;
+    }
+    return l;
 }
 
 int main(){
