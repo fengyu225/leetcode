152e267ee7d5674ef5714a9f8a5f3a884018ba29
Yu Feng <yfeng@boingo.com>
2016-01-17 16:00:47 -0800

    M  6/pow.cpp

--- 6/pow.cpp
+++ 6/pow.cpp
@@ -4,8 +4,21 @@ Implement pow(x, n).
 
 #include "header.h"
 
-double myPow(double x, int n) {
+double myPow(double x, long n){
+    if(n == 0) return 1;
+    if(n<0) return 1.0/myPow(x,-n);
+    if(n == 1) return x;
+    double res = 1.0, np = x;
+    while(n){
+        if(n&1) res*=np;
+        np*=np;
+        n>>=1;
+    }
+    return res;
+}
 
+double myPow(double x, int n) {
+    return myPow(x,(long)n);
 }
 
 int main(){
