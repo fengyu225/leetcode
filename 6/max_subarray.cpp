2dc55b147ba6d96c5d20f2b6e65548a95d434cc2
Yu Feng <yfeng@boingo.com>
2016-01-17 16:23:12 -0800

    M  6/max_subarray.cpp

--- 6/max_subarray.cpp
+++ 6/max_subarray.cpp
@@ -8,7 +8,16 @@ the contiguous subarray [4,−1,2,1] has the largest sum = 6.
 #include "header.h"
 
 int maxSubArray(vector<int>& nums) {
-
+    int sz = nums.size();
+    if(sz == 0) return 0;
+    if(sz == 1) return nums[0];
+    int res = nums[0], last_max = nums[0];
+    for(int i=1; i<sz; i++){
+        int curr_max = max(last_max+nums[i], nums[i]);
+        res = max(res, curr_max);
+        last_max = curr_max;
+    }
+    return res;
 }
     
 int main(){
