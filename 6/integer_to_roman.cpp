3f4127290b8cc79443dcfd75bb50378eb534971e
Yu Feng <yfeng@boingo.com>
2016-01-21 16:19:08 -0800

    M  6/integer_to_roman.cpp

--- 6/integer_to_roman.cpp
+++ 6/integer_to_roman.cpp
@@ -1,10 +1,50 @@
 #include "header.h"
 
+vector<string> key = {
+    "M",
+    "CM",
+    "D",
+    "CD",
+    "C",
+    "XC",
+    "L",
+    "XL",
+    "X",
+    "IX",
+    "V",
+    "IV",
+    "I",
+};
+
+vector<int> val = {
+    1000,
+    900,
+    500,
+    400,
+    100,
+    90,
+    50,
+    40,
+    10,
+    9,
+    5,
+    4,
+    1
+};
+
 string intToRoman(int num){
-    unordered_map<int,string> m = {
-        {1,"a"} 
-    };
-    return "";
+    string res = "";
+    if(num == 0) return res;
+    int sz = key.size();
+    for(int i=0; i<sz; i++){
+        if(val[i]>num) continue;
+        int r = num/val[i];
+        string temp = "";
+        for(int j=0; j<r; j++) temp+=key[i];
+        res += temp;
+        num -= r*val[i];
+    }
+    return res;
 }
 
 int main(){
