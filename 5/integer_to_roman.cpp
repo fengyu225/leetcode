774944de985f6c9e70a90d4d2208cd3d4cb83d1d
Yu Feng <yfeng@boingo.com>
2016-01-21 16:19:23 -0800

    M  5/integer_to_roman.cpp M  5/roman_to_integer.cpp

--- 5/integer_to_roman.cpp
+++ 5/integer_to_roman.cpp
@@ -1,30 +1,53 @@
 #include "header.h"
 
-/*
-I = 1;
-V = 5;
-X = 10;
-L = 50;
-C = 100;
-D = 500;
-M = 1000;
-*/
+vector<string> key = {
+    "M",
+    "CM",
+    "D",
+    "CD",
+    "C",
+    "XC",
+    "L",
+    "XL",
+    "X",
+    "IX",
+    "V",
+    "IV",
+    "I",
+};
+
+vector<int> val = {
+    1000,
+    900,
+    500,
+    400,
+    100,
+    90,
+    50,
+    40,
+    10,
+    9,
+    5,
+    4,
+    1
+};
 
 string intToRoman(int num){
-    int a[] = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
-    string b[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
-    string res="";
-    int n = sizeof(a)/sizeof(a[0]);
-    for(int i=0; i<n; i++){
-        if(num<a[i]) continue;
-        int r = num/a[i];
-        for(int j=0;j<r;j++) res += b[i];
-        num -= r*a[i];
+    string res = "";
+    if(num == 0) return res;
+    int sz = key.size();
+    for(int i=0; i<sz; i++){
+        if(val[i]>num) continue;
+        int r = num/val[i];
+        string temp = "";
+        for(int j=0; j<r; j++) temp+=key[i];
+        res += temp;
+        num -= r*val[i];
     }
     return res;
 }
 
 int main(){
-    cout<<intToRoman(47)<<endl;
+    cout<<intToRoman(123)<<endl;
     return 0;
 }
--- 5/roman_to_integer.cpp
+++ 5/roman_to_integer.cpp
@@ -4,48 +4,56 @@ Given a roman numeral, convert it to an integer.
 Input is guaranteed to be within the range from 1 to 3999.
 */
 
-/*
-I = 1;
-V = 5;
-X = 10;
-L = 50;
-C = 100;
-D = 500;
-M = 1000;
-*/
 
 #include "header.h"
 
+unordered_map<char,int> m = {
+    {'M',1000},
+    {'D',500},
+    {'C',100},
+    {'L',50},
+    {'X',10},
+    {'V',5},
+    {'I',1}
+};
+
+unordered_map<char,int> limits = {    
+    {'M',4},
+    {'D',2},
+    {'C',4},
+    {'L',2},
+    {'X',4},
+    {'V',2},
+    {'I',4}
+};
+
 int romanToInt(string s){
-    if(s.length() == 0) return 0;
-    unordered_map<char,int> m;
-    m['I'] = 1;
-    m['V'] = 5;
-    m['X'] = 10;
-    m['L'] = 50;
-    m['C'] = 100;
-    m['D'] = 500;
-    m['M'] = 1000;
-    int res = 0, sz = s.length(), pre=0, curr=1;
-    while(curr<sz){
-        if(m[s[curr]]<=m[s[pre]]){
-            res += m[s[pre]];
-            pre = curr;
-            curr++;
+    //check invalid input
+    int sz = s.length();
+    if(sz == 0) return 0;
+    int res = 0;
+    for(int i=0; i<sz; i++){
+        char c = s[i];
+        if(m.find(c) == m.end()) return -1;
+        if(i>0 && m[c]>m[s[i-1]]) res += m[c]-2*m[s[i-1]];
+        else if(i>0 && m[c]==m[s[i-1]] && limits[c]+i-2<sz){
+            bool temp = true;
+            for(int j=i; j<=limits[c]+i-2; j++)
+                if(s[j] != s[j-1]){
+                    temp = false;
+                    break;
+                }
+            if(temp) return -1;
+            res+=m[c];
         }
-        else{
-            res += m[s[curr]]-m[s[pre]];
-            pre = curr+1;
-            curr = pre+1;
-        }
-    } 
-    if(pre<curr)
-        res += m[s[pre]];
+        else res+=m[c];
+    }
     return res;
 }
 
 int main(){
     cout<<romanToInt("CXXIII")<<endl;
     cout<<romanToInt("C")<<endl;
+    cout<<romanToInt("CIIII")<<endl;
     return 0;
 }
