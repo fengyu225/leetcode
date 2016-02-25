8edfbee29cf0931e5aee1284cb2e811526f2ea08
Yu Feng <yfeng@boingo.com>
2016-02-24 19:27:19 -0800

    A  6/sparse_matrix_multiplication.cpp M  6/two_sum_iii_data_structure_design.cpp M  6/valid_number.cpp

--- 6/valid_number.cpp
+++ 6/valid_number.cpp
@@ -16,33 +16,7 @@ The signature of the C++ function had been updated. If you still see your functi
 #include "header.h"
 
 bool isNumber(string s) {
-    int sz = s.length();
-    int l = 0, r = sz-1;
-    for(;s[l] == ' ';l++);
-    for(;s[r] == ' ';r--);
-    if(s[l] == '+' || s[l] == '-') l++;
-    if(l>r) return false;
-    bool dot = false, e = false, num = false, num_after_e = false, num_after_dot=false; 
-    for(int i=l; i<=r; i++){
-        if(s[i] == '.'){
-            if(dot || e) return false;
-            dot = true;
-        } 
-        else if(s[i] == 'e'){
-            if(e || !num) return false;
-            e = true;
-            if(i+1<=r && (s[i+1] == '+' || s[i+1] == '-')) i++;
-        }
-        else if(s[i]<='9' && s[i]>='0'){
-            if(dot) num_after_dot = true;
-            if(e) num_after_e = true;
-            num = true;
-        }
-        else return false;
-    }
-    if(!num && (dot || e)) return false;
-    if(e && !num_after_e) return false;
-    return true;
+
 }
 
 int main(){
