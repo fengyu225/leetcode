e7cef2a0bc269f660315d25e57799ce4df5d8bc4
Yu Feng <yfeng@boingo.com>
2016-02-25 18:44:16 -0800

    M  5/valid_number.cpp M  6/valid_number.cpp ?? 5/Valid_number.jpeg ?? 6/Valid_number.jpeg

--- 5/valid_number.cpp
+++ 5/valid_number.cpp
@@ -46,6 +46,51 @@ bool isNumber(string s) {
     return true;
 }
 
+/* using DFA
+bool isNumber(string s) {
+    int sz = s.length(), l = 0, r = sz-1;
+    for(;l<sz && s[l] == ' ';l++);
+    for(;r>=0 && s[r] == ' ';r--);
+    if(l>r) return false;
+    string zero_to_9 = "0123456789", plus_minus = "+-", dot = ".", e = "e";
+    unordered_map<char, int> labels = {
+        {'0', 0},
+        {'1', 0},
+        {'2', 0},
+        {'3', 0},
+        {'4', 0},
+        {'5', 0},
+        {'6', 0},
+        {'7', 0},
+        {'8', 0},
+        {'9', 0},
+        {'+', 1},
+        {'-', 1},
+        {'.', 2},
+        {'e', 3}
+    };
+    vector<vector<int> > transitions = {
+        {2, 1, 6, -1},
+        {2, -1, 6, -1},
+        {2, -1, 3, 5},
+        {4, -1, -1, 5},
+        {4, -1, -1, 5},
+        {7, 8, -1, -1},
+        {4, -1, -1, -1},
+        {7, -1, -1, -1},
+        {7, -1, -1, -1}
+    };
+    int curr = 0;
+    for(;l<=r;l++){
+        vector<int> temp = transitions[curr];
+        if(labels.find(s[l]) == labels.end()) return false;
+        int next = temp[labels[s[l]]];
+        if(next == -1) return false;
+        curr = next;
+    }
+    return curr == 2 || curr == 4 || curr == 3 || curr == 7;
+}
+*/
 int main(){
     cout<<isNumber("0e")<<endl;
     cout<<isNumber("0")<<endl;
@@ -57,5 +102,12 @@ int main(){
     cout<<isNumber("1 a")<<endl;
     cout<<isNumber("2e10")<<endl;
     cout<<isNumber("2e1.23")<<endl;
+    cout<<isNumber("  00,00.0")<<endl;
+    cout<<isNumber("12.3e2")<<endl;
+    cout<<isNumber("0.e2")<<endl;
+    cout<<isNumber(".1")<<endl;
+    cout<<isNumber("1.")<<endl;
+    cout<<isNumber(".2e10")<<endl;
+    cout<<isNumber("-.1")<<endl;
     return 0;
 }
