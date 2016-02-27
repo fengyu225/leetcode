b7b12704f67edb742da244d44b5a313a75e70f8b
Yu Feng <yfeng@boingo.com>
2016-02-26 19:02:09 -0800

    M  6/shortest_word_distance_iii.cpp

--- 6/shortest_word_distance_iii.cpp
+++ 6/shortest_word_distance_iii.cpp
@@ -18,19 +18,7 @@ You may assume word1 and word2 are both in the list.
 #include "header.h"
 
 int shortestWordDistance(vector<string>& words, string word1, string word2) {
-    int sz = words.size();
-    int w1_index = -1, w2_index = -1;
-    int res = sz+1;
-    for(int i=0; i<sz; i++){
-        string w = words[i];
-        if(w != word1 && w != word2) continue;
-        bool a = (w == word1), b = (w == word2);
-        if(a) res = min(res, w2_index>=0?i-w2_index:sz+1); 
-        if(b) res = min(res, w1_index>=0?i-w1_index:sz+1); 
-        if(a) w1_index = i;
-        if(b) w2_index = i;
-    }
-    return res;
+
 }
 
 int main(){
