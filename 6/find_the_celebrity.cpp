19ed4784127558bd9a7ff3d28f77cf2dcabcaf6d
Yu Feng <yfeng@boingo.com>
2016-02-28 19:29:03 -0800

    M  6/find_the_celebrity.cpp

--- 6/find_the_celebrity.cpp
+++ 6/find_the_celebrity.cpp
@@ -23,18 +23,6 @@ bool knows(int a, int b){
 class Solution {
 public:
     int findCelebrity(int n) {
-        if(n == 0) return 0;
-        if(n == 1) return 1;
-        int candidate = 0;
-        for(int i=1; i<n; i++){
-            //if(knows(candidate, i) || !knows(i,candidate)) candidate = i;
-            if(knows(candidate, i)) candidate = i;
-        }
-        for(int i=0; i<n; i++){
-            if(i == candidate) continue;
-            if(!knows(i,candidate) || knows(candidate, i)) return -1;
-        }
-        return candidate;
     }
 };
 
