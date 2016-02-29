8756223f2fb6fdecc8a4a133dbeec9cd7ce2869b
Yu Feng <yfeng@boingo.com>
2016-02-28 16:19:55 -0800

    M  6/max_product_subarr.cpp

--- 6/max_product_subarr.cpp
+++ 6/max_product_subarr.cpp
@@ -8,6 +8,16 @@ the contiguous subarray [2,3] has the largest product = 6.
 #include "header.h"
 
 int maxProduct(vector<int>& nums){
+    int sz = nums.size();
+    int large = nums[0], small = nums[0], res = nums[0];
+    for(int i=1; i<sz; i++){
+        int new_large = max(nums[i], max(large*nums[i], small*nums[i]));
+        int new_small = min(nums[i], min(large*nums[i], small*nums[i]));
+        res = max(res, new_large);
+        large = new_large;
+        small = new_small;
+    }
+    return res;
 }
 
 int main(){
