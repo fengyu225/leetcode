a4f8830b73f83dac4dc78d5250e15e8820ac4b7d
Yu Feng <fengyu225@gmail.com>
2016-06-08 15:17:07 -0700

    M  5/largest_bst_subtree.cpp  D 5/test.cpp ?? 5/test.pyc

--- 5/largest_bst_subtree.cpp
+++ 5/largest_bst_subtree.cpp
@@ -20,10 +20,40 @@ Can you figure out ways to solve it with O(n) time complexity?
 
 #include "header.h"
 
-int largestBSTSubtree(TreeNode* root) {
+bool find(TreeNode* root, int& min_val, int& max_val, int& cnt, int& res){
+    if(!root){
+        cnt = 0;
+        return true;
+    }
+    int l_min = INT_MAX, l_max = INT_MIN, r_min = INT_MAX, r_max = INT_MIN;
+    int l_cnt = 0, r_cnt = 0, l_res = 0, r_res = 0;
+    bool l = find(root->left, l_min, l_max, l_cnt, l_res);
+    bool r = find(root->right, r_min, r_max, r_cnt, r_res);
+    bool temp = (root->left == NULL || l_max<=root->val) && (root->right == NULL || root->val<=r_min);
+    cnt = l_cnt+r_cnt+1;
+    if(temp && l && r){
+        min_val = root->left?l_min:root->val;
+        max_val = root->right?r_max:root->val;
+        res = cnt;
+        return true;
+    }
+    else{
+        res = max(l_res, r_res);
+        return false;
+    }
+}
 
+int largestBSTSubtree(TreeNode* root) {
+    if(!root) return 0;    
+    int min_val = INT_MAX, max_val = INT_MIN;
+    int res = 0, cnt = 0;
+    find(root, min_val, max_val, cnt, res);
+    return res;
 }
 
 int main(){
+    vector<string> s = {"10","5","15","1","8","#","7"};
+    TreeNode* root = create_tree(s);
+    cout<<largestBSTSubtree(root)<<endl;
     return 0;
 }
