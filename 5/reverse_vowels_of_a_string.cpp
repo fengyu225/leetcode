e012c1f3a8ac776fffd0d6c3376e711463ae558a
Yu Feng <fengyu225@gmail.com>
2016-06-18 17:12:20 -0700

    M  5/reverse_vowels_of_a_string.cpp ?? 5/test.pyc

--- 5/reverse_vowels_of_a_string.cpp
+++ 5/reverse_vowels_of_a_string.cpp
@@ -10,7 +10,20 @@ Given s = "leetcode", return "leotcede".
 
 #include "header.h"
 
+bool is_vowel(char c){
+    return 
+        c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' ||
+        c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U';
+}
+
 string reverseVowels(string s) {
+    int sz = s.length();
+    int l = 0, r=sz-1;
+    while(l<r){
+        if(!is_vowel(s[l])) l++;
+        else if(!is_vowel(s[r])) r--;
+        else swap(s[l++], s[r--]);
+    }
     return s;
 }
 
