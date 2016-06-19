38ea3c68b26ecc1e5dbb79532ed8a180a03212e3
Yu Feng <fengyu225@gmail.com>
2016-06-19 00:11:09 -0700

    M  5/top_k_frequent_elements.cpp ?? 5/test.pyc

--- 5/top_k_frequent_elements.cpp
+++ 5/top_k_frequent_elements.cpp
@@ -11,8 +11,24 @@ Your algorithm's time complexity must be better than O(n log n), where n is the 
 
 #include "header.h"
 
+class comp{
+    public:
+        bool operator() (pair<int,int>& l, pair<int,int>& r){
+            return r.second>l.second;          
+        }
+};
+
 vector<int> topKFrequent(vector<int>& nums, int k) {
-    return nums;
+    vector<int> res;
+    unordered_map<int,int> m;
+    for(int i : nums) m[i]++;
+    priority_queue<pair<int,int>, vector<pair<int,int> >, comp> q;
+    for(auto p : m) q.push(p);
+    for(int i=0; i<k; i++){
+        res.push_back(q.top().first);
+        q.pop();
+    }
+    return res;
 }
 
 int main(){
