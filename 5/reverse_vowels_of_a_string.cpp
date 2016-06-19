cd1bb07dddda201868c728d0c5ec7e7a1d1cbb7b
Yu Feng <fengyu225@gmail.com>
2016-06-18 17:08:15 -0700

    A  5/reverse_vowels_of_a_string.cpp ?? 5/test.pyc

---
+++ 5/reverse_vowels_of_a_string.cpp
@@ -0,0 +1,20 @@
+/*
+Write a function that takes a string as input and reverse only the vowels of a string.
+
+Example 1:
+Given s = "hello", return "holle".
+
+Example 2:
+Given s = "leetcode", return "leotcede".
+*/
+
+#include "header.h"
+
+string reverseVowels(string s) {
+    return s;
+}
+
+int main(){
+    cout<<reverseVowels("leetcode")<<endl;
+    return 0;
+}
