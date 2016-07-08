791e593e5d0c035b5bcbcb9a194a0245c2e459d1
Yu Feng <fengyu225@gmail.com>
2016-07-08 00:30:00 -0700

    M  5/design_twitter.cpp ?? 5/test.pyc

--- 5/design_twitter.cpp
+++ 5/design_twitter.cpp
@@ -34,31 +34,65 @@ twitter.getNewsFeed(1);
 
 #include "header.h"
 
+class comp{
+    public:
+        bool operator() (pair<int,int> a, pair<int,int> b){
+            return b.second<a.second;
+        }
+};
+
 class Twitter {
+    unordered_map<int, unordered_set<int> > followers;
+    unordered_map<int, unordered_set<int> > following;
+    unordered_map<int, vector<pair<int,int> > > posts;
+    int time;
 public:
     /** Initialize your data structure here. */
     Twitter() {
-        
+        this->time = 0;
     }
     
     /** Compose a new tweet. */
     void postTweet(int userId, int tweetId) {
-        
+        this->posts[userId].push_back(make_pair(tweetId, this->time++));    
     }
-    
+
     /** Retrieve the 10 most recent tweet ids in the user's news feed. Each item in the news feed must be posted by users who the user followed or by the user herself. Tweets must be ordered from most recent to least recent. */
     vector<int> getNewsFeed(int userId) {
-        
+        vector<int> res;
+        priority_queue<pair<int,int>, vector<pair<int,int> >, comp> pq;
+        for(int i=(this->posts[userId].size())-1; i>=0 && pq.size()<10; i--){
+            pq.push(this->posts[userId][i]);
+        }
+        for(int followee : this->following[userId]){
+            vector<pair<int,int> > temp_posts = this->posts[followee];
+            int p = temp_posts.size()-1;
+            while(p>=0 && (pq.size()<10 || temp_posts[p].second>pq.top().second)){
+                if(pq.size()>=10) pq.pop();
+                pq.push(temp_posts[p--]);
+            }
+        } 
+        while(!pq.empty()){
+            res.push_back(pq.top().first);
+            pq.pop();
+        }
+        reverse(res.begin(), res.end());
+        return res;
     }
     
     /** Follower follows a followee. If the operation is invalid, it should be a no-op. */
     void follow(int followerId, int followeeId) {
-        
+        if(followerId == followeeId) return;
+        this->followers[followeeId].insert(followerId);
+        this->following[followerId].insert(followeeId);
     }
     
     /** Follower unfollows a followee. If the operation is invalid, it should be a no-op. */
     void unfollow(int followerId, int followeeId) {
-        
+        if(this->followers[followeeId].find(followerId) != this->followers[followeeId].end()){
+            this->followers[followeeId].erase(this->followers[followeeId].find(followerId)); 
+            this->following[followerId].erase(this->following[followerId].find(followeeId));
+        }
     }
 };
 
@@ -72,5 +106,13 @@ public:
  */
 
 int main(){
+    Twitter* twitter = new Twitter();
+    twitter->postTweet(1, 5);
+    twitter->follow(1, 2);
+    twitter->follow(2, 1);
+    print_vector<int>(twitter->getNewsFeed(2));
+    twitter->postTweet(2, 6);
+    print_vector<int>(twitter->getNewsFeed(1));
+    print_vector<int>(twitter->getNewsFeed(2));
     return 0;
 }
