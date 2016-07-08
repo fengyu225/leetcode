c5d1df65d0caa0412687ff8812189aef826e68dd
Yu Feng <fengyu225@gmail.com>
2016-07-08 12:53:47 -0700

    M  5/sort_transformed_array.cpp ?? 5/test.pyc

--- 5/sort_transformed_array.cpp
+++ 5/sort_transformed_array.cpp
@@ -19,12 +19,28 @@ Result: [-23, -5, 1, 7]
 
 vector<int> sortTransformedArray(vector<int>& nums, int a, int b, int c) {
     vector<int> res;
+    double mid = -1*b/a/2.0;
+    int sz = nums.size();
+    int l = 0, r = sz-1;
+    while(l<=r){
+        double l_dis = abs(nums[l]-mid);
+        double r_dis = abs(nums[r]-mid);
+        if(l_dis>=r_dis){
+            res.push_back(a*nums[l]*nums[l]+b*nums[l]+c);
+            l++;
+        }
+        else{
+            res.push_back(a*nums[r]*nums[r]+b*nums[r]+c);
+            r--;
+        }
+    }
+    if(a>0) reverse(res.begin(), res.end());
     return res;
 }
 
 int main(){
     vector<int> nums = {-4, -2, 2, 4};
-    vector<int> res = sortTransformedArray(nums, -1, 3, 5);
+    vector<int> res = sortTransformedArray(nums, 1, 3, 5);
     print_vector<int>(res);
     return 0;
 }
