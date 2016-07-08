72370678e7ba2e549540684ef613e852586b43bc
Yu Feng <fengyu225@gmail.com>
2016-07-08 13:25:13 -0700

    M  5/sort_transformed_array.cpp ?? 5/test.pyc

--- 5/sort_transformed_array.cpp
+++ 5/sort_transformed_array.cpp
@@ -19,17 +19,20 @@ Result: [-23, -5, 1, 7]
 
 vector<int> sortTransformedArray(vector<int>& nums, int a, int b, int c) {
     vector<int> res;
-    double mid = -1*b/a/2.0;
     int sz = nums.size();
+    if(sz == 0) return res;
+    if(sz == 1) return vector<int>(nums[0]*nums[0]*a+nums[0]*b+c);
+    if(a == 0 && b == 0) return vector<int>(sz, c);
+    double mid = a!=0?-1*b/(double)a/2.0:(nums[0]+nums[sz-1])/2.0;
     int l = 0, r = sz-1;
     while(l<=r){
-        double l_dis = abs(nums[l]-mid);
-        double r_dis = abs(nums[r]-mid);
-        if(l_dis>=r_dis){
+        double l_dis = fabs((double)nums[l]-mid);
+        double r_dis = fabs((double)nums[r]-mid);
+        if((a!= 0 && l_dis>=r_dis) || (a==0 && b>0)){
             res.push_back(a*nums[l]*nums[l]+b*nums[l]+c);
             l++;
         }
-        else{
+        else if((a != 0 && l_dis<r_dis) || (a==0 && b<0)){
             res.push_back(a*nums[r]*nums[r]+b*nums[r]+c);
             r--;
         }
@@ -39,8 +42,8 @@ vector<int> sortTransformedArray(vector<int>& nums, int a, int b, int c) {
 }
 
 int main(){
-    vector<int> nums = {-4, -2, 2, 4};
-    vector<int> res = sortTransformedArray(nums, 1, 3, 5);
+    vector<int> nums = {-95,-95,-93,-92,-89,-89,-88,-82,-81,-79,-71,-71,-66,-66,-65,-65,-62,-61,-60,-54,-54,-51,-50,-48,-47,-45,-43,-37,-35,-35,-32,-32,-29,-24,-24,-22,-20,-20,-17,-14,-13,-12,-12,-4,1,8,11,14,16,16,22,24,26,28,28,28,29,30,31,36,44,46,49,60,60,60,62,64,65,73,76,86,87,89,91,92,93,94,94,96,96,97};
+    vector<int> res = sortTransformedArray(nums, 41, 45, -89);
     print_vector<int>(res);
     return 0;
 }
