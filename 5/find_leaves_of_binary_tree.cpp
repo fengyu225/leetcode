1055a71f0b6abddbb90fe8ec7231a3c55ce88e3e
Yu Feng <fengyu225@gmail.com>
2016-07-08 23:13:06 -0700

    M  5/find_leaves_of_binary_tree.cpp

--- 5/find_leaves_of_binary_tree.cpp
+++ 5/find_leaves_of_binary_tree.cpp
@@ -24,28 +24,49 @@ Returns [4, 5, 3], [2], [1].
 
 #include "header.h"
 
-vector<int> merge(vector<int>& a, vector<int>& b){
-    vector<int> res;
-    for(int i:a) res.push_back(i);
-    for(int i:b) res.push_back(i);
-    return res;
+//vector<int> merge(vector<int>& a, vector<int>& b){
+//    vector<int> res;
+//    for(int i:a) res.push_back(i);
+//    for(int i:b) res.push_back(i);
+//    return res;
+//}
+//
+//vector<vector<int>> findLeaves(TreeNode* root) {
+//    vector<vector<int> > res;
+//    if(!root) return res;
+//    vector<vector<int> > left = findLeaves(root->left);
+//    vector<vector<int> > right = findLeaves(root->right);
+//    for(int l = 0, r = 0; l<left.size() || r<right.size(); ){
+//        if(l<left.size() && r<right.size()){
+//            res.push_back(merge(left[l], right[r]));
+//            l++;
+//            r++;
+//        }
+//        else if(l == left.size()) res.push_back(right[r++]);
+//        else res.push_back(left[l++]); 
+//    }
+//    res.push_back(vector<int>(1, root->val));
+//    return res;
+//}
+
+int findLeaves(TreeNode* root, vector<vector<int> >& res){
+    if(!root) return -1;
+    if(root->left == NULL && root->right == NULL){
+        if(res.size() == 0) res.push_back(vector<int>(1, root->val));
+        else res[0].push_back(root->val);
+        return 0;
+    }
+    int l = findLeaves(root->left, res);
+    int r = findLeaves(root->right, res);
+    int level = max(l, r)+1;
+    if(level >= res.size()) res.push_back(vector<int>(1, root->val));
+    else res[level].push_back(root->val);
+    return level;
 }
 
 vector<vector<int>> findLeaves(TreeNode* root) {
     vector<vector<int> > res;
-    if(!root) return res;
-    vector<vector<int> > left = findLeaves(root->left);
-    vector<vector<int> > right = findLeaves(root->right);
-    for(int l = 0, r = 0; l<left.size() || r<right.size(); ){
-        if(l<left.size() && r<right.size()){
-            res.push_back(merge(left[l], right[r]));
-            l++;
-            r++;
-        }
-        else if(l == left.size()) res.push_back(right[r++]);
-        else res.push_back(left[l++]); 
-    }
-    res.push_back(vector<int>(1, root->val));
+    findLeaves(root, res);
     return res;
 }
 
