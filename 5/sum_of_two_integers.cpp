984ccb352514fded6b3619109b11fdf616f572ef
Yu Feng <fengyu225@gmail.com>
2016-07-10 01:16:15 -0700

    A  5/sum_of_two_integers.cpp

---
+++ 5/sum_of_two_integers.cpp
@@ -0,0 +1,17 @@
+/*
+Calculate the sum of two integers a and b, but you are not allowed to use the operator + and -.
+
+Example:
+Given a = 1 and b = 2, return 3.
+*/
+
+#include "header.h"
+
+int getSum(int a, int b) {
+
+}
+
+int main(){
+    cout<<getSum(1, 2)<<endl;
+    return 0;
+}
