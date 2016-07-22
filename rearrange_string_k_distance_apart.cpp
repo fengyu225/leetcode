c8dda7aa6a753596ff0c2f9bba035c72f960082b
Yu Feng <yufeng@linkedin.com>
2016-07-22 15:45:25 -0700

    M  rearrange_string_k_distance_apart.cpp

--- rearrange_string_k_distance_apart.cpp
+++ rearrange_string_k_distance_apart.cpp
@@ -29,42 +29,37 @@ The same letters are at least distance 2 from each other.
 
 #include "header.h"
 
-class comp{
-    public:
-        bool operator() (pair<char,int>& a, pair<char,int>& b){
-            return a.second<b.second;
-        }
-};
-
 string rearrangeString(string str, int k) {
     int len = str.length();
     vector<int> last_pos(256, -1);
     vector<int> m(256, 0); 
     for(char c : str) m[c]++;
-    priority_queue<pair<char, int>, vector<pair<char,int> >, comp> pq;
+    priority_queue<pair<int, char>, vector<pair<int, char> > > pq;
     for(int i=0; i<256; i++)
-        if(m[i]>0) pq.push(make_pair(i, m[i]));
+        if(m[i]>0) pq.push(make_pair(m[i], i));
     string res = "";
     for(int i=0; i<len; i++){
-        pair<char,int> t = pq.top();
-        if(last_pos[t.first] == -1 || i-last_pos[t.first]>=k){
-            res.push_back(t.first);
+        pair<int, char> t = pq.top();
+        if(last_pos[t.second] == -1 || i-last_pos[t.second]>=k){
+            res.push_back(t.second);
             pq.pop();
-            if(t.second-1>0) pq.push(make_pair(t.first, t.second-1));
-            last_pos[t.first] = i;
+            t.first--;
+            if(t.first>0) pq.push(t);
+            last_pos[t.second] = i;
         }
         else{
-            vector<pair<char,int> > temp_v;
-            while(!pq.empty() && last_pos[pq.top().first]>=0 && i-last_pos[pq.top().first]<k){
+            vector<pair<int, char> > temp_v;
+            while(!pq.empty() && last_pos[pq.top().second]>=0 && i-last_pos[pq.top().second]<k){
                 temp_v.push_back(pq.top());
                 pq.pop();
             } 
             if(pq.empty()) return "";
-            res.push_back(pq.top().first);
-            pair<char,int> temp_p = pq.top();
+            res.push_back(pq.top().second);
+            pair<int, char> temp_p = pq.top();
             pq.pop();
-            if(temp_p.second-1>0) pq.push(make_pair(temp_p.first, temp_p.second-1));
-            last_pos[temp_p.first] = i;
+            temp_p.first--;
+            if(temp_p.first>0) pq.push(temp_p);
+            last_pos[temp_p.second] = i;
             while(!temp_v.empty()){
                 pq.push(temp_v.back());
                 temp_v.pop_back();
