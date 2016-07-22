de4ace696d497c452e9e8fab524d6c3b7f9e64e2
Yu Feng <yufeng@linkedin.com>
2016-07-22 16:13:15 -0700

    M  russian_doll_envelopes.cpp

--- russian_doll_envelopes.cpp
+++ russian_doll_envelopes.cpp
@@ -9,10 +9,37 @@ Given envelopes = [[5,4],[6,4],[6,7],[2,3]], the maximum number of envelopes you
 
 #include "header.h"
 
-int maxEnvelopes(vector<pair<int, int>>& envelopes) {
+class comp{
+    public:
+        bool operator() (pair<int,int>& a, pair<int,int>& b){
+            return a.first<b.first || a.first == b.first && a.second<b.second;
+        }
+};
 
+int maxEnvelopes(vector<pair<int, int> >& envelopes) {
+    int sz = envelopes.size();
+    if(sz<2) return sz;
+    //sort(envelopes.begin(), envelopes.end(), comp());
+    sort(envelopes.begin(), envelopes.end());
+    vector<int> arr(sz, 1);
+    int res = 1;
+    for(int i=1; i<sz; i++){
+        int m = 1;
+        for(int j=0; j<i; j++)
+            if(envelopes[j].first<envelopes[i].first && envelopes[j].second<envelopes[i].second) m = max(m, arr[j]+1);
+        arr[i] = m;
+        res = max(arr[i], res);
+    }
+    return res;
 }
 
 int main(){
+    vector<pair<int,int> > envelopes = {
+        {5, 4},
+        {6, 4},
+        {6, 7},
+        {2, 3}
+    };
+    cout<<maxEnvelopes(envelopes)<<endl;
     return 0;
 }
