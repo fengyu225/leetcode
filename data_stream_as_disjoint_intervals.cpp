7187a1324b0c7e638f699e86047bd4219ee7c4df
Yu Feng <yufeng@linkedin.com>
2016-07-31 17:10:37 -0700

    M  data_stream_as_disjoint_intervals.cpp

--- data_stream_as_disjoint_intervals.cpp
+++ data_stream_as_disjoint_intervals.cpp
@@ -23,22 +23,54 @@ What if there are lots of merges and the number of disjoint intervals are small 
  *     Interval(int s, int e) : start(s), end(e) {}
  * };
  */
+class comp{
+    public:
+        bool operator() (Interval a, Interval b){
+            return a.start<b.start;
+        }
+};
+
 class SummaryRanges {
+    set<Interval, comp> intervals;
+    unordered_map<int,int> parents;
+    unordered_map<int,Interval*> p_intervals;
+    int f(int curr){
+        if(parents[curr] == curr) return curr;
+        parents[curr] = f(parents[curr]);
+        return parents[curr];
+    }
+    void u(int a, int b){
+        if(parents.find(b) != parents.end()){
+            int a_p = f(a);
+            int b_p = f(b);
+            if(a_p == b_p) return;
+            parents[a_p] = b_p;
+            intervals.erase(intervals.find(*(p_intervals[a_p])));
+            intervals.erase(intervals.find(*(p_intervals[b_p])));
+            int start = min(p_intervals[b_p]->start, p_intervals[a_p]->start);
+            int end = max(p_intervals[b_p]->end, p_intervals[a_p]->end);
+            p_intervals[b_p]->start = start;
+            p_intervals[b_p]->end = end;
+            intervals.insert(*(p_intervals[b_p]));
+        }
+    }
 public:
     /** Initialize your data structure here. */
-    SummaryRanges() {
-        
-    }
+    SummaryRanges() {}
     
     void addNum(int val) {
-        
+        if(parents.find(val) == parents.end()){
+            parents[val] = val;
+            p_intervals[val] = new Interval(val, val);
+            intervals.insert(*(p_intervals[val]));
+        }
+        if(val != INT_MAX) u(val, val+1);
+        if(val != INT_MIN) u(val, val-1);
     }
     
     vector<Interval> getIntervals() {
-        vector<Interval> res = {
-            {0, 1},
-            {2, 3}
-        }; 
+        vector<Interval> res;
+        for(Interval i : this->intervals) res.push_back(i);
         return res;
     }
 };
