23c86496166baeb2d6ad0d12d2482162b1bf5f75
Yu Feng <yufeng@linkedin.com>
2016-08-03 00:42:59 -0700

    M  design_phone_directory.cpp ?? test

--- design_phone_directory.cpp
+++ design_phone_directory.cpp
@@ -35,35 +35,49 @@ directory.check(2);
 
 
 class PhoneDirectory {
-    unordered_set<int> s;
+    vector<bool> v;
+    vector<int> l;
     int maxNumbers;
+    int l_bound;
+    int r_bound;
 public:
     /** Initialize your data structure here
         @param maxNumbers - The maximum numbers that can be stored in the phone directory. */
     PhoneDirectory(int maxNumbers) {
         this->maxNumbers = maxNumbers;
-        for(int i=0; i<maxNumbers; i++) s.insert(i);
+        this->v = vector<bool>(maxNumbers, true);
+        this->l = vector<int>(maxNumbers);
+        this->l_bound = 0;
+        this->r_bound = 0;
+        for(int i=0; i<maxNumbers; i++){
+            this->l[r_bound] = i; 
+            r_bound = (r_bound+1)%(this->maxNumbers+1);
+        }
     }
     
     /** Provide a number which is not assigned to anyone.
         @return - Return an available number. Return -1 if none is available. */
     int get() {
-        if(s.size() == 0) return -1;
-        int res = *(s.begin());
-        s.erase(s.begin());
+        if((this->r_bound - this->l_bound)%(this->maxNumbers+1) == 0) return -1;
+        int res = this->l[this->l_bound];
+        this->l_bound = (this->l_bound+1)%(this->maxNumbers+1);
+        v[res] = false;
         return res;
     }
     
     /** Check if a number is available or not. */
     bool check(int number) {
-        return s.find(number) != s.end(); 
+        if(number<0 || number>=this->maxNumbers) return false;
+        return v[number];
     }
     
     /** Recycle or release a number. */
     void release(int number) {
-        int sz = s.size();
-        if(sz+1<=this->maxNumbers){
-            s.insert(number);    
+        int sz = (this->r_bound - this->l_bound)%(this->maxNumbers+1);
+        if(sz+1<=this->maxNumbers && number<this->maxNumbers && number>=0 && v[number]==false){
+            this->l[this->r_bound] = number;
+            this->r_bound = (this->r_bound+1)%(this->maxNumbers+1);
+            this->v[number] = true;
         }
     }
 };
